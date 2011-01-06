55e86cc2a3f8ed1ae27075946e1187db0c50b810
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-01-06 12:10:36 +0000

    [qjug-2011] Flesh out UI.

--- qjug-2011/src/main/java/biz/freshcode/qjug2011/ui/MainFrame.java
+++ qjug-2011/src/main/java/biz/freshcode/qjug2011/ui/MainFrame.java
@@ -1,24 +1,68 @@
 package biz.freshcode.qjug2011.ui;
 
 import biz.freshcode.qjug2011.logging.Logging;
+import com.jgoodies.forms.builder.PanelBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
 import org.slf4j.Logger;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 import javax.swing.*;
+import javax.swing.text.DefaultCaret;
+import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+
+import static java.awt.Font.MONOSPACED;
+import static javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED;
+import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER;
 
 @Component
 public class MainFrame extends JFrame {
     @Inject private ConfigurableApplicationContext ctx;
     @Logging private Logger log;
+    private JButton btn = new JButton("Go");
+    private JTextArea area = new JTextArea(1,1);
 
     public MainFrame() {
         setTitle("QJUG 2011 - Peter Leong");
         setupClose();
         setSize(800, 600);
+        populateUi();
+    }
+
+    private void populateUi() {
+        JScrollPane textPane = setupTextArea();
+
+        FormLayout layout = new FormLayout(
+                "right:pref, 3dlu, pref, pref:grow", // cols
+                "p, 3dlu, p, 3dlu, p, fill:pref:grow, 3dlu"); // rows
+        PanelBuilder builder = new PanelBuilder(layout);
+        builder.setDefaultDialogBorder();
+        CellConstraints c = new CellConstraints();
+        int fullWidth = layout.getColumnCount() + 1;
+        int row = 0;
+        int col;
+        builder.addSeparator("Complicated", c.xyw(col = 1, row += 1, fullWidth - col));
+        builder.addLabel("Button:", c.xy(1, row += 2));
+        builder.add(btn, c.xy(3, row));
+        builder.addLabel("Output:", c.xy(1, row += 2));
+        builder.add(textPane, c.xyw(col = 1, row += 1, fullWidth - col));
+
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
+        cp.add(builder.getPanel(), BorderLayout.CENTER);
+    }
+
+    private JScrollPane setupTextArea() {
+        area.setEditable(false);
+        Font f = Font.decode(MONOSPACED);
+        area.setFont(f);
+        DefaultCaret c = (DefaultCaret) area.getCaret();
+        c.setUpdatePolicy(DefaultCaret.NEVER_UPDATE);
+        return new JScrollPane(area, VERTICAL_SCROLLBAR_NEVER, HORIZONTAL_SCROLLBAR_AS_NEEDED);
     }
 
     public void launch() {
