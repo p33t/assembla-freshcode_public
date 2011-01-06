c1b7446caa0f8c44376a9b97ce6f0141bcd33e45
pwl <1179301+p33t@users.noreply.github.com>
2011-01-06 11:06:35 +0000

    [qjug-2011] Test interface invocation capture.

--- qjug-2011/src/test/java/biz/freshcode/qjug2011/util/AppReflectUtilTest.java
+++ qjug-2011/src/test/java/biz/freshcode/qjug2011/util/AppReflectUtilTest.java
@@ -15,21 +15,32 @@ import static org.testng.Assert.assertNotNull;
 
 public class AppReflectUtilTest {
     @Test
-    public void testCaptureInvocation() throws Exception {
+    public void testCaptureInvocationClass() throws Exception {
         final Ref<Invocation> r = ref();
-        InvocationHandler h = new InvocationHandler() {
-            public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
-                r.val = new Invocation(method, objects);
-                return defVal(method);
-            }
-        };
-
+        InvocationHandler h = new RefInvocationHandler(r);
         MyClass myClass = captureInvocation(MyClass.class, h);
         myClass.pop("bruce");
         myClass.noise();
         assertNotNull(r.val);
         assertEquals(r.val.method, MyClass.class.getMethod("pop", arr(String.class)));
         assertEquals(r.val.args, arr("bruce"));
+    }
+    
+    @Test
+    public void testCaptureInvocationInterface() throws Exception {
+        final Ref<Invocation> r = ref();
+        InvocationHandler h = new RefInvocationHandler(r);
+        MyInterface myInterface = captureInvocation(MyInterface.class, h);
+        myInterface.bruce("lee");
+        myInterface.noise();
+        assertNotNull(r.val);
+        assertEquals(r.val.method, MyInterface.class.getMethod("bruce", arr(String.class)));
+        assertEquals(r.val.args, arr("lee"));
+    }
+
+    public interface MyInterface {
+        void bruce(String surname);
+        void noise();
     }
 
     public static class MyClass {
@@ -43,5 +54,18 @@ public class AppReflectUtilTest {
         }
 
         // TODO: Primitive return type
+    }
+
+    private static class RefInvocationHandler implements InvocationHandler {
+        private final Ref<Invocation> r;
+
+        public RefInvocationHandler(Ref<Invocation> r) {
+            this.r = r;
+        }
+
+        public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+            r.val = new Invocation(method, objects);
+            return defVal(method);
+        }
     }
 }
