4143f7ea744bdd284f5e795d17fdf30388ca808f
pwl <1179301+p33t@users.noreply.github.com>
2011-03-28 05:06:30 +0000

    [learn] Closures - function literal

--- learn_scala/syntax/src/pkg/Closures.scala
+++ learn_scala/syntax/src/pkg/Closures.scala
@@ -1,34 +1,38 @@
 package pkg
 
 object Closures {
-  def main(args: Array[String]) {
-    service(noArgsToUnit)
-    service2(noArgsToString)
-    service3(dblToString)
-    service4(intIntToString)
-  }
-
   def noArgsToUnit() = println("noArgsToUnit called")
   def noArgsToString() = {"noArgsToString called"}
   def dblToString(d: Double) = {"dblToString called with arg:" + d}
   def intIntToString(i: Int,j: Int) = {i + " + " + j + " = " + (i + j)}
 
-  def service(clr: () => Unit) = {
+  def main(args: Array[String]) {
+    service(noArgsToUnit)
+    service2(noArgsToString)
+    service3(dblToString)
+    service4(intIntToString)
+    service3((d: Double) => "Function Literal... " + d)
+  }
+
+
+  def service(clr: () => Unit) {
     clr()
   }
 
-  def service2(clr: () => String) = {
+  def service2(clr: () => String) {
     val s = clr()
     println(s)
   }
 
-  def service3(clr: Double => String) = {
+  def service3(clr: Double => String) {
     val s = clr(java.lang.Math.random())
     println(s)
   }
   
-  def service4(clr: (Int,Int) => String) = {
+  def service4(clr: (Int,Int) => String) {
     val s = clr(3, 4)
+//    val s = clr2(4)
+//    val clr2 = Function.curried(clr, 3) _
     println(s)
   }
 }
