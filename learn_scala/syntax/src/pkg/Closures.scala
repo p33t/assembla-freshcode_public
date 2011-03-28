5125bdc7384d9bb905b0ef2e035acf84f30836d7
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-03-28 02:15:57 +0000

    [learn] Closures

--- learn_scala/syntax/src/pkg/Closures.scala
+++ learn_scala/syntax/src/pkg/Closures.scala
@@ -5,11 +5,13 @@ object Closures {
     service(noArgsToUnit)
     service2(noArgsToString)
     service3(dblToString)
+    service4(intIntToString)
   }
 
   def noArgsToUnit() = println("noArgsToUnit called")
   def noArgsToString() = {"noArgsToString called"}
   def dblToString(d: Double) = {"dblToString called with arg:" + d}
+  def intIntToString(i: Int,j: Int) = {i + " + " + j + " = " + (i + j)}
 
   def service(clr: () => Unit) = {
     clr()
@@ -22,6 +24,11 @@ object Closures {
 
   def service3(clr: Double => String) = {
     val s = clr(java.lang.Math.random())
+    println(s)
+  }
+  
+  def service4(clr: (Int,Int) => String) = {
+    val s = clr(3, 4)
     println(s)
   }
 }
