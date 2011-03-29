39373480515ccb5dfb5dc4c136e7a27bc8467b48
pwl <1179301+p33t@users.noreply.github.com>
2011-03-29 03:07:51 +0000

    [learn] FunctionLiterals: parameter lists and placeholder syntax

--- learn_scala/syntax/src/pkg/FunctionLiteral.scala
+++ learn_scala/syntax/src/pkg/FunctionLiteral.scala
@@ -10,16 +10,18 @@ object FunctionLiteral {
     targetTyping
     placeHolderSyntax
     partialApplication
+    parameterLists
+    parameterListsAlt
   }
 
-  private def targetTyping: Unit = {
+  private def targetTyping() {
     var intToString: Int => String = null
     // type of args is inferred and no need for redundant parens
     intToString = x => "Minimal Syntax " + x
     println(intToString(23))
   }
 
-  private def placeHolderSyntax: Unit = {
+  private def placeHolderSyntax {
     var intToString: Int => String = null
     // can skip the parameter declaration and use an implicit '_'
     intToString = "Place holder syntax " + _
@@ -31,7 +33,7 @@ object FunctionLiteral {
     println(intx3ToInt(2, 7, 9))
   }
 
-  private def partialApplication: Unit = {
+  private def partialApplication {
     def myFunct(i: Int): String = {
       "A nested function " + i
     }
@@ -39,6 +41,8 @@ object FunctionLiteral {
     // need the place holder to convert a function into a function value, which can then be passed around
     val myFunctionValue = myFunct _
     myFunctionValue(20)
+    val refToValue = myFunctionValue
+    refToValue(30)
 
     def intIntToString(i: Int, j: Int): String = {
       "Sum of " + i + " and " + j + " is " + (i + j)
@@ -47,9 +51,39 @@ object FunctionLiteral {
 
     // only need 1 place holder (with no parens) to signify entire arg list
     val argListed = intIntToString _
-    
+
     // is this that fabled currying?
-    val partial = argListed((_:Int), 1)
+    val partial = argListed((_: Int), 1)
     println(partial(9))
+  }
+
+  private def parameterLists() {
+    def funct(i: Int)(j: Int) {
+      println(i + " + " + j + " = " + (i + j))
+    }
+    funct(1)(2)
+
+    val f2 = funct(1)(_)
+    f2(3)
+    // Hmm.. this needs a type(?!)
+    val f3 = funct(_: Int)(2)
+    f3(0)
+    val f4 = funct(_)
+    f4(4)(3)
+    // combining param lists?
+    val f5 = funct(_: Int)(_: Int)
+    f5(7, 8)
+    val f6 = funct _
+    f6(9)(10)
+  }
+
+  private def parameterListsAlt() {
+    def funct(i: Int, j: Int) {
+      println(i + " + " + j + " = " + (i + j))
+    }
+
+    // splitting parameter lists?
+    def f2(i: Int)(j: Int) = funct(i, j)
+    f2(1)(3)
   }
 }
