771433bb5ebad64e8b90c8e8bb1b7950477d72b5
pwl <1179301+p33t@users.noreply.github.com>
2011-03-29 00:15:23 +0000

    [learn] Closures, Functions, Free Variables, Bound Variables

--- learn_scala/syntax/src/pkg/Closures.scala
+++ learn_scala/syntax/src/pkg/Closures.scala
@@ -6,10 +6,12 @@ object Closures {
     var freeVariable = 10
 
     // a function only has 'bound variables'
+    // it is a 'closed term'... meaning the section of code (term) is self contained
     val funct = (x: Int) => x * 10
     println(funct(1))
 
     // a closure has at least one 'free variable'
+    // is is an 'open term' requiring a contextual binding
     val closure = (x: Int) => x * freeVariable
     println(closure(5))
     freeVariable = 9
