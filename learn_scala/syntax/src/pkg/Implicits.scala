305e5f205314606b2be0a110ba53858c9d28078d
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-03-30 01:03:52 +0000

    [learn] Implicits: Arglist

--- learn_scala/syntax/src/pkg/Implicits.scala
+++ learn_scala/syntax/src/pkg/Implicits.scala
@@ -3,9 +3,15 @@ package pkg
 
 object Implicits {
   private implicit def intToString(i: Int) = "" + i
+
   private implicit def boolToInt(b: Boolean) = if (b) 1 else 0
+
   private implicit def makeLessBoring(b: Boring) = new LessBoring()
 
+  class Unordered(sortVal: Int) {
+    // TODO not sure why this is necessary
+    def getSortVal = sortVal
+  }
 
   def main(args: Array[String]) {
     // Implicit is applied if it is a singular (non-composite) identifier.  EG. Not SomeClass.intToString
@@ -27,6 +33,23 @@ object Implicits {
     // its a method on an implicit that takes any type
     val t = 3 -> "three"
     println(t)
+
+    // implicit args are typically used to adorn normally supplied args
+    // and have a very specific type to prevent collisions
+    def myMax[A](elements: List[A])(implicit orderer: A => Int): A =
+      elements match {
+        case List() =>
+          throw new IllegalArgumentException("empty list!")
+        case List(x) => x
+        case x :: rest =>
+          val maxRest = myMax(rest)(orderer)
+          if (orderer(x) > orderer(maxRest)) x
+          else maxRest
+      }
+    // define the implicit transformer method
+    implicit val myOrderer: Unordered => Int = un => un.getSortVal
+    val max = myMax(List(new Unordered(3), new Unordered(2), new Unordered(5)))
+    println("Should be 5... " + max.getSortVal)
   }
 
   def printString(s: String) = println(s)
@@ -39,3 +62,7 @@ class LessBoring {
     println("veryUniqueName called")
   }
 }
+
+
+
+
