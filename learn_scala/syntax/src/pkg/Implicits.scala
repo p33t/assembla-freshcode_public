ed7ff84601ccfb2596520db67dcd38299acdde32
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-03-30 01:07:41 +0000

    [learn] Implicits: Arglist

--- learn_scala/syntax/src/pkg/Implicits.scala
+++ learn_scala/syntax/src/pkg/Implicits.scala
@@ -8,12 +8,22 @@ object Implicits {
 
   private implicit def makeLessBoring(b: Boring) = new LessBoring()
 
-  class Unordered(sortVal: Int) {
-    // TODO not sure why this is necessary
-    def getSortVal = sortVal
+  def main(args: Array[String]) {
+    typeConversion
+    receiverConversion
+    pseudoSyntax
+    implicitArgList
   }
 
-  def main(args: Array[String]) {
+  def receiverConversion: Unit = {
+    // can grapht a method onto a different receiver
+    val lb = new LessBoring()
+    lb.veryUniqueName()
+    val b = new Boring()
+    b.veryUniqueName()
+  }
+
+  def typeConversion: Unit = {
     // Implicit is applied if it is a singular (non-composite) identifier.  EG. Not SomeClass.intToString
     // By convention one might import all members from a 'Preamble' object for a package EG. import pkg.Preamble._
     // Methods defined on a participating class's 'companion object' are also considered
@@ -22,18 +32,16 @@ object Implicits {
     // Will not apply two implicits at a time
     printString(boolToInt(true))
     // Doesn't work...printString(true)
+  }
 
-    // can grapht a method onto a different receiver
-    val lb = new LessBoring()
-    lb.veryUniqueName()
-    val b = new Boring()
-    b.veryUniqueName()
-
+  def pseudoSyntax: Unit = {
     // the -> map building syntax is not actually syntax
     // its a method on an implicit that takes any type
     val t = 3 -> "three"
     println(t)
+  }
 
+  def implicitArgList: Unit = {
     // implicit args are typically used to adorn normally supplied args
     // and have a very specific type to prevent collisions
     def myMax[A](elements: List[A])(implicit orderer: A => Int): A =
@@ -50,6 +58,11 @@ object Implicits {
     implicit val myOrderer: Unordered => Int = un => un.getSortVal
     val max = myMax(List(new Unordered(3), new Unordered(2), new Unordered(5)))
     println("Should be 5... " + max.getSortVal)
+  }
+
+  class Unordered(sortVal: Int) {
+    // TODO not sure why this is necessary
+    def getSortVal = sortVal
   }
 
   def printString(s: String) = println(s)
