32e191d4b6ffbea132f59954d40dfe2f01eee59e
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-04-04 05:10:03 +0000

    [learn] Poi:  Refactor.

--- learn/poi/src/main/java/pkg/PoiApp.java
+++ learn/poi/src/main/java/pkg/PoiApp.java
@@ -2,8 +2,6 @@ package pkg;
 
 import org.apache.poi.hwpf.extractor.WordExtractor;
 import org.apache.poi.poifs.filesystem.DirectoryNode;
-import org.apache.poi.poifs.filesystem.DocumentInputStream;
-import org.apache.poi.poifs.filesystem.Entry;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -11,51 +9,47 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Iterator;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class PoiApp {
     public static void main(String[] args) throws Exception {
         PoiApp p = new PoiApp();
-        p.doc("minimal.doc");
-        p.docX("minimal.docx");
-        p.doc("tables.doc");
-        p.docX("tables.docx");
+        p.outDoc("minimal.doc");
+        p.outDocX("minimal.docx");
+        p.outDoc("tables.doc");
+        p.outDocX("tables.docx");
     }
 
-    private void doc(String fileName) throws Exception {
-        // need an open InputStream; for a file-based system, this would be appropriate:
-        // InputStream stream = new FileInputStream(fileName);
-        DirectoryNode root = getRoot(fileName);
-        String s = root.getName() + " with " + root.getEntryCount() + " entries...";
-        out(s);
-        Iterator<Entry> entries = root.getEntries();
-        while (entries.hasNext()) {
-            Entry next = entries.next();
-            out(next.getName());
-        }
+    private void outDoc(String fileName) throws Exception {
+        File file = file(fileName);
+        List<String> list = doc(file);
+        outParagraphs(fileName, list);
+    }
 
-        out("");
-        out("Obtaining document: WordDocument");
-        DocumentInputStream doc = root.createDocumentInputStream("WordDocument");
-        byte[] bytes = new byte[doc.available()];
-        out(bytes.length + " bytes allocated.");
-        int readCout = doc.read(bytes);
-        out("Read " + readCout + " bytes.");
+    private void outDocX(String fileName) throws Exception {
+        File myFile = file(fileName);
+        List<String> list = docX(myFile);
+        outParagraphs(fileName, list);
+    }
 
-        out("");
-        WordExtractor extractor = new WordExtractor(getStream(fileName));
+    private List<String> doc(File file) throws IOException {
+        WordExtractor extractor = new WordExtractor(stream(file));
         String[] lines = extractor.getParagraphText();
-//        TODO: Make doc vs docx transparent.
-        System.out.println("<" + fileName + ">.getText()...");
+        List<String> list = new ArrayList<String>();
+        for (String line : lines) list.add(cleanString(line));
+        return list;
+    }
+
+    private void outParagraphs(String fileName, List<String> lines) {
+        System.out.println("<" + fileName + "> paragraphs...");
         for (String line : lines) {
-            out(cleanString(line));
+            out(line);
         }
-        out("---------------End of getText()");
+        out("---------------End of paragraphs");
     }
 
     private String cleanString(String line) {
@@ -69,42 +63,31 @@ public class PoiApp {
         return line;
     }
 
-    private void docX(String fileName) throws Exception {
-        File myFile = file(fileName);
-        ZipFile docxFile = new ZipFile(myFile);
-        ZipEntry documentXML = docxFile.getEntry("word/document.xml");
-        InputStream documentXMLIS = docxFile.getInputStream(documentXML);
+    private List<String> docX(File myFile) throws Exception {
+        ZipFile zip = new ZipFile(myFile);
+        ZipEntry documentXML = zip.getEntry("word/document.xml");
+        InputStream stream = zip.getInputStream(documentXML);
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        Document doc = dbf.newDocumentBuilder().parse(documentXMLIS);
+        Document doc = dbf.newDocumentBuilder().parse(stream);
 
-        Element tElement = doc.getDocumentElement();
-        NodeList n = tElement.getElementsByTagName("w:p");
+        Element elem = doc.getDocumentElement();
+        NodeList n = elem.getElementsByTagName("w:p");
 
-        out("<" + fileName + ">.lines...");
+        List<String> list = new ArrayList<String>();
         for (int j = 0; j < n.getLength(); j++) {
             Node child = n.item(j);
             String line = child.getTextContent();
-
-            out(cleanString(line));
-//            if (isNotEmpty(line)) {
-//                out(line);
-//            }
+            list.add(cleanString(line));
         }
-        out("---------------- end of lines.");
+        return list;
     }
 
     private File file(String fileName) {
         return new File("src/main/resources/" + fileName);
     }
 
-    private DirectoryNode getRoot(String fileName) throws Exception {
-        InputStream inputStream = getStream(fileName);
-        POIFSFileSystem fs = new POIFSFileSystem(inputStream);
-        return fs.getRoot();
-    }
-
-    private InputStream getStream(String fileName) throws Exception {
-        return new FileInputStream(file(fileName));
+    private FileInputStream stream(File file) throws FileNotFoundException {
+        return new FileInputStream(file);
     }
 
     private void out(Object s) {
