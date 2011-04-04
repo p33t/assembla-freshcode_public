f0a00b72e825b0d0a5da850657fc217638e1a979
pwl <1179301+p33t@users.noreply.github.com>
2011-04-04 00:59:39 +0000

    [learn] Poi project boot

---
+++ learn/poi/src/main/java/pkg/Minimal.java
@@ -0,0 +1,40 @@
+package pkg;
+
+import org.apache.poi.poifs.filesystem.DirectoryNode;
+import org.apache.poi.poifs.filesystem.Entry;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
+public class Minimal {
+    public static void main(String[] args) throws Exception {
+        new Minimal().run();
+    }
+
+    private void run() throws Exception {
+        // need an open InputStream; for a file-based system, this would be appropriate:
+        // InputStream stream = new FileInputStream(fileName);
+        DirectoryNode root = getRoot("minimal.doc");
+        String s = root.getName() + " " + root.getEntryCount();
+        out(s);
+        Iterator<Entry> entries = root.getEntries();
+        while (entries.hasNext()) {
+            Entry next = entries.next();
+            out(next.getName());
+        }
+    }
+
+    private DirectoryNode getRoot(String fileName) throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
+        POIFSFileSystem fs = new POIFSFileSystem(inputStream);
+        return fs.getRoot();
+    }
+
+    private void out(Object s) {
+        System.out.println(s);
+    }
+
+
+}
