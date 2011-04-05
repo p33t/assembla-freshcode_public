50b9a43d95325156a86f0371f525f6299e2dee0e
pwl <1179301+p33t@users.noreply.github.com>
2011-04-05 23:35:47 +0000

    [learn] Loops

---
+++ learn_scala/syntax/src/pkg/Loops.scala
@@ -0,0 +1,26 @@
+package pkg
+
+
+object Loops {
+  def main(args: Array[String]) {
+    val elems = List(2, 4, 6, 8, 0)
+
+    if (elems.isEmpty) println("bad") else println("good")
+    println(if (elems.isEmpty) "bad" else "good")
+
+    var i = 0
+    while (i < elems.size) {
+      print(" " + elems(i))
+      i = i + 1
+    }
+    println
+
+    // hmmm this doesn't work
+//    elems.foreach(print(" " + _))
+    elems.foreach((elem) => print(" " + elem))
+    println
+
+    for (elem <- elems) print(" " + elem)
+    println
+  }
+}
