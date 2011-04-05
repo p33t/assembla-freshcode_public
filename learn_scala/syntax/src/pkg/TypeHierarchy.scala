9e8a92a3eb27b35c31fb677e0083dd2a769bc25b
pwl <1179301+p33t@users.noreply.github.com>
2011-04-05 07:13:07 +0000

    [learn] Type Hierachy

---
+++ learn_scala/syntax/src/pkg/TypeHierarchy.scala
@@ -0,0 +1,28 @@
+package pkg
+
+
+object TypeHierarchy {
+  def main(args: Array[String]) {
+    // null has only one instance and is a subclass of all AnyRef
+    val nul: Null = null
+    val anyRef: AnyRef = nul
+    // Unit has only one instance and is a subclass of all AnyVal
+    val unit: Unit = ()
+    val anyVal: AnyVal = unit
+    // Any is the parent class of AnyVal and AnyRef
+    var any: Any = anyVal
+    any = anyRef
+    val str = {
+      if (any == null) "any is null"
+      else pop("any is supposed to be null")
+    }
+    println(str)
+  }
+
+  // Nothing has NO instances and is a subclass of all Any
+  // It is used as the return type for methods that don't return normally
+  // and will not interfere with type inferencing
+  def pop(msg: String): Nothing = {
+    throw new Exception(msg)
+  }
+}
