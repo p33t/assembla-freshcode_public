50d4859ee70f3103127e8b81335047671dea4704
pwl <1179301+p33t@users.noreply.github.com>
2011-04-05 03:56:36 +0000

    [learn] Constructor accessibility

---
+++ learn_scala/syntax/src/pkg/Constructors.scala
@@ -0,0 +1,38 @@
+package pkg
+
+
+object Constructors {
+  def main(args: Array[String]) {
+
+//    Not allowed...
+//    val cip = new ClassIsPrivate("bruce")
+    val cip = new ClassIsPrivate(4)
+    println("Should be 4: " + cip.s)
+
+    val aip = new FieldIsPrivate("bruce")
+//    Not allowed...
+//    println("Should be 'bruce':" + aip.s)
+
+//    Doesn't work...
+//    val pub0 = IsPublic("lee")
+    val pub = new IsPublic("lee")
+    println("Should be lee: " + pub.s)
+
+    val b = new Boring
+    println(b)
+  }
+
+  class ClassIsPrivate private(val s:String) {
+    def this(i:Int) = this(i.toString)
+  }
+
+  class FieldIsPrivate (private val s:String) {
+    def this(j: Int) = this(j.toString)
+  }
+
+  class IsPublic(val s:String) {
+    def this(j:Int) = this(j.toString)
+  }
+
+  class Boring
+}
