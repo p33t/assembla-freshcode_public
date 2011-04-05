8e0bdc8006b80736dd9b839d2f97f633253ff755
pwl <1179301+p33t@users.noreply.github.com>
2011-04-05 04:08:27 +0000

    [learn] Companion object

--- learn_scala/syntax/src/pkg/CompanionCheck.scala
+++ learn_scala/syntax/src/pkg/CompanionCheck.scala
@@ -3,6 +3,7 @@ package pkg
 
 object CompanionCheck {
   def main(args: Array[String]) {
+//    Looks like a factory method...
     val c: Companion = Companion(10)
     println("Should be 10: " + Companion.getS(c))
     //    println("Shoud not be able to access: " + c.s)
