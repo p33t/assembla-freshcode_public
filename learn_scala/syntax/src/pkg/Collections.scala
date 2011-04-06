dc06a9d745af5232dd8ee7dcba93c698c36ed65a
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-04-06 01:45:48 +0000

    [learn] Maps

--- learn_scala/syntax/src/pkg/Collections.scala
+++ learn_scala/syntax/src/pkg/Collections.scala
@@ -3,13 +3,35 @@ package pkg
 
 object Collections {
   def main(args: Array[String]) {
-    var immutableSet = Set("bruce", "lee", "springsteen")
-    immutableSet += "willis"
-    println(immutableSet)
+    sets
+    maps
+  }
+
+  def maps: Unit = {
+    var immut = Map(1 -> "one", 2 -> "two")
+    println(immut + (3 -> "three"))
+    println(immut)
+    immut += (3 -> "three")
+    println(immut)
+//    Hmmm... this doesn't work.  Is this a failure of the type system?  Should this work like List and Stack?
+//    var immut2 = immut + ("bruce" -> "lee")
+//    println(immut2)
+
+    val mut = scala.collection.mutable.Map(1 -> "i", 2 -> "ii")
+    mut + (3 -> "iii")
+    println(mut)
+    // won't work because type parameters of map have been decided
+//    mut + ("bruce" -> "willis")
+  }
+
+  def sets: Unit = {
+    var immut = Set("bruce", "lee", "springsteen")
+    immut += "willis"
+    println(immut)
 
     // mutable variants have same (?) methods... but don't need 'var'
-    val mutableSet = scala.collection.mutable.Set("raphael", "donatello", "leonardo")
-    mutableSet += "michelangelo"
-    println(mutableSet)
+    val mut = scala.collection.mutable.Set("raphael", "donatello", "leonardo")
+    mut += "michelangelo"
+    println(mut)
   }
 }
