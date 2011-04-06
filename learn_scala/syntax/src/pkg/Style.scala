f35d239cbe146dc5c0d9950075ec5e1e674c4a5e
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-04-06 07:10:42 +0000

    [learn] No param methods style

---
+++ learn_scala/syntax/src/pkg/Style.scala
@@ -0,0 +1,17 @@
+package pkg
+
+
+class Style {
+  private val x = "ex"
+
+  def emptyParensMethod() {
+    // anything with a side affect should have parens to give the clue that something is happening
+    println()
+  }
+
+  def noParensMethod:String = {
+    // benign methods that have no sideaffects don't need parens and could easily be substituted with a field
+    // as far as client code is concerned: 'uniform access principal'
+    x
+  }
+}
