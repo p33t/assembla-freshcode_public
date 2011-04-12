7afa67f82e2be47bcfebbbc03c69067888861af2
pwl <1179301+p33t@users.noreply.github.com>
2011-04-12 06:55:36 +0000

    [learn] Structual Types

---
+++ learn_scala/syntax/src/pkg/StructuralType.scala
@@ -0,0 +1,37 @@
+package pkg
+
+object StructuralType {
+
+  class A
+
+  class B extends A {
+    def method() {
+      println("B.method called")
+    }
+  }
+
+  class C extends A {
+    def method() {
+      println("C.method called")
+    }
+  }
+
+  // a structural type requiring an object that has a 'method' method
+  def callMethod(m: {def method(): Unit}) {
+    m.method
+  }
+
+  // a structural type that has a common ancestor
+  def callAMethod(m: A {def method(): Unit}) {
+    m.method
+  }
+
+  def main(args: Array[String]) {
+    //    Doesn't compile...
+    //    callMethod(new A)
+    callMethod(new B)
+    callMethod(new C)
+    callAMethod(new B)
+    callAMethod(new C)
+  }
+}
