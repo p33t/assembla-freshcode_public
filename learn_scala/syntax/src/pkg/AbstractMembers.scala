1c83abc0752a4eff9fbfd2b0dfc85e5491c21db4
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-04-12 04:56:52 +0000

    [learn] Abstract Members: Overrides

--- learn_scala/syntax/src/pkg/AbstractMembers.scala
+++ learn_scala/syntax/src/pkg/AbstractMembers.scala
@@ -7,10 +7,21 @@ object AbstractMembers {
     // An 'abstract type' member
     type T
     var value: T = _
+    val valueString: String = ""
+    def valueToString = value.toString
   }
 
   class StringCell extends Cell {
     type T = String
+    // cannot override a 'var'
+//    override def value = "Bruce"
+//    override var value: String = "Bruce Willis"
+
+    // method cannot override a val
+//    override def valueString = value.toString
+
+    // implementing a method as a val is supported
+    override val valueToString = "Not Supported"
   }
 
   def main(args: Array[String]) {
