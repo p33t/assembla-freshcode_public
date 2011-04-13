6bd4470ff60e3b994c2fc46894c48410808e1dba
pwl <1179301+p33t@users.noreply.github.com>
2011-04-13 04:44:55 +0000

    [learn] Regular Expressions (heavier)

---
+++ learn_scala/syntax/src/pkg/Regexpr2.scala
@@ -0,0 +1,44 @@
+package pkg
+
+import collection.mutable.ListBuffer
+
+
+object Regexpr2 {
+  //  Task: Successfully identify parts of this text...
+  //    <header>....<body-elem-1>.....<body-elem-2>...  <header>...<body-elem-1>.....<body-elem-2>...
+
+  def matches(s: String): List[(String, String, String)] = {
+    val expr = """<header>(.*?)<body-elem-1>(.*?)<body-elem-2>(.*?)(<header>|\z)""".r
+    val accum = new ListBuffer[(String,String,String)]
+
+    def findMatches(str: String) {
+      val first = expr.findPrefixMatchOf(str)
+      first match {
+        case None => //println("no match on " + str)
+        case Some(m) => {
+          require(m.groupCount == 4)
+          val found = (m.group(1), m.group(2), m.group(3))
+          accum += found
+          val rest = str.substring(m.matched.length)
+          if (!rest.isEmpty) findMatches(m.group(4) + rest)
+        }
+      }
+    }
+    findMatches(s)
+    accum.toList
+  }
+
+  def test(s: String, expected: (String, String, String)*) {
+    val actual = matches(s)
+    val exp = List(expected: _*)
+    require(actual == exp, "expected: " + exp + "\n actual: " + actual)
+  }
+
+  def main(args: Array[String]) {
+    val in1 = "<header>bruce lee<body-elem-1>was here<body-elem-2>and kicked azz"
+    val out1 = ("bruce lee", "was here", "and kicked azz")
+    test(in1, out1)
+    test(in1 * 2, out1, out1)
+  }
+
+}
