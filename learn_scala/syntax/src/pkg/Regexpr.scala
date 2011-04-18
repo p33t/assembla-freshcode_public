95d18d02ad64760a634806f04cc22b4a4a07adec
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-04-18 05:57:48 +0000

    [learn] Regex learning.

--- learn_scala/syntax/src/pkg/Regexpr.scala
+++ learn_scala/syntax/src/pkg/Regexpr.scala
@@ -2,11 +2,35 @@ package pkg
 
 
 object Regexpr {
-  def main(args: Array[String]) {
+  def simple {
     val bruceLeeRegex = """[B|b]ruce [L|l]ee""".r
     val first = bruceLeeRegex.findFirstIn("bruce willis bruce Lee bruce springsteen")
     first match {
       case Some(s) => println(s)
     }
+  }
+
+  def strangeChars {
+    val parens = """\(""".r
+    val str = "mary had a (little) lamb"
+    val first = parens.findFirstMatchIn(str)
+    first match {
+      case Some(s) => println("Found: " + str.substring(s.start))
+    }
+  }
+
+  def numbers {
+    val matchStr = """\d+"""
+    require("1234".matches(matchStr))
+    require(!"abc".matches(matchStr))
+    require(!"12 abc".matches(matchStr))
+    require(!"abc 1234".matches(matchStr))
+    println("Numbers is fine")
+  }
+
+  def main(args: Array[String]) {
+    simple
+    strangeChars
+    numbers
   }
 }
