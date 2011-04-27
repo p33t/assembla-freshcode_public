36897ba55777ee98f688edfc6244cdb40261414e
pwl <1179301+p33t@users.noreply.github.com>
2011-04-27 07:12:10 +0000

    [learn] Actors: self / exit

--- learn_scala/syntax/src/pkg/ActorDemo.scala
+++ learn_scala/syntax/src/pkg/ActorDemo.scala
@@ -3,11 +3,12 @@ package pkg
 import scala.actors.Actor._
 
 object ActorDemo {
-  def main(args: Array[String]) {
+  def basics: Unit = {
     val act1 = actor {
       while (true) {
         receive {
           case x: Int => println("Integer " + x)
+          case "exit" => exit
         }
       }
     }
@@ -15,5 +16,21 @@ object ActorDemo {
     act1 ! 99
     act1 ! "bruce lee" // ignored
     act1 ! 100
+    act1 ! "exit"
+  }
+
+  def useCurrentThread {
+    var result:String = null
+    self ! "Message in a bottle"
+    // safer to use 'receiveWithin' when using current thread
+    self.receiveWithin(200) {
+      case x: String => result = x
+    }
+    println(result)
+  }
+
+  def main(args: Array[String]) {
+    basics
+    useCurrentThread
   }
 }
