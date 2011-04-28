b4951350025dd24f1cab822ffa06bea35d843bc8
pwl <1179301+p33t@users.noreply.github.com>
2011-04-28 07:45:43 +0000

    [learn-scala-lift] Menus / config (WIP)

--- learn_scala/lift/src/main/scala/bootstrap/liftweb/Boot.scala
+++ learn_scala/lift/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,11 +1,8 @@
 package bootstrap.liftweb
 
 import net.liftweb._
-import common._
 import http._
-import sitemap._
-import Loc._
-
+import sitemap.{SiteMap, Menu}
 
 /**
  * A class that's instantiated early and run.  It allows the application
@@ -13,36 +10,46 @@ import Loc._
  */
 class Boot {
   def boot {
-    // where to search snippet
-    LiftRules.addToPackages("code")
-    
-//
-//    // Build SiteMap
-//    val entries = List(
-//      Menu.i("Home") / "index", // the simple way to declare a menu
-//
-//      // more complex because this menu allows anything in the
-//      // /static path to be visible
-//      Menu(Loc("Static", Link(List("static"), true, "/static/index"),
-//	       "Static Content")))
-//
-//    // set the sitemap.  Note if you don't want access control for
-//    // each page, just comment this line out.
-//    LiftRules.setSiteMap(SiteMap(entries:_*))
-//
-//    //Show the spinny image when an Ajax call starts
-//    LiftRules.ajaxStart =
-//      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-//
-//    // Make the spinny image go away when it ends
-//    LiftRules.ajaxEnd =
-//      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
-//
-//    // Use jQuery 1.4
-//    LiftRules.jsArtifacts = net.liftweb.http.js.jquery.JQuery14Artifacts
-//
-//    // Force the request to be UTF-8
-//    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+    // LiftRules singleton provides configuration but is only mutable at boot time.
+    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
 
+    // where to search for snippet
+    LiftRules.addToPackages("code")
+
+    // Use HTML5 for rendering (instead of the default xhtml)
+    LiftRules.htmlProperties.default.set((r: Req) => new Html5Properties(r.userAgent))
+
+    // Build SiteMap
+    //    def sitemap(): SiteMap = SiteMap(Menu.i("Home") / "index")
+    def siteMap(): SiteMap = SiteMap(
+      Menu.i("Home") / "index" // the simple way to declare a menu
+      // TODO: Finish sub menus
+      //      , Menu.i("Experiments") / "experiments" submenus (
+//        Menu.i("My first snippet") / "first_snippet"
+//        )
+    )
+
+    //      // more complex because this menu allows anything in the
+    //      // /static path to be visible
+    //      Menu(Loc("Static", Link(List("static"), true, "/static/index"),
+    //	       "Static Content")))
+    //
+    // set the sitemap.  Note if you don't want access control for
+    // each page, just comment this line out.
+    //        LiftRules.setSiteMap(siteMap)
+    LiftRules.setSiteMapFunc(() => siteMap()) // TODO: Try siteMap _
+    //
+    //    //Show the spinny image when an Ajax call starts
+    //    LiftRules.ajaxStart =
+    //      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+    //
+    //    // Make the spinny image go away when it ends
+    //    LiftRules.ajaxEnd =
+    //      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+    //
+    //    // Use jQuery 1.4
+    //    LiftRules.jsArtifacts = net.liftweb.http.js.jquery.JQuery14Artifacts
+    //
+    // Force the request to be UTF-8
   }
 }
