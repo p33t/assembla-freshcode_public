73b8a05a52d1d32b48b2c69bbbe52463a4a042d6
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-05-04 00:30:35 +0000

    [learn-scala-lift] Snippet Recursion.

---
+++ learn_scala/lift/src/main/scala/code/snippet/Recursive.scala
@@ -0,0 +1,29 @@
+package code.snippet
+
+import xml.NodeSeq
+
+
+object Recursive {
+  val Finished = "Finished"
+
+  def render(in: NodeSeq) = {
+    val newText = in.text.trim match {
+      case "3" => "2"
+      case "2" => "1"
+      case "1" => "0"
+      case "0" => Finished
+    }
+    // TODO: Figure out how to construct line-break sensitive XML.  Intellij formatting inserts them automatically.
+    if (newText != Finished)
+      <span>
+        &lt; <span class="lift:Recursive">
+        {newText}
+      </span> &gt;
+      </span>
+    else {
+      <span>
+        {newText}
+      </span>
+    }
+  }
+}
