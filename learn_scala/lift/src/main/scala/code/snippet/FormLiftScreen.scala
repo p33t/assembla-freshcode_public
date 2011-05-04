b9b36d625eba9e44e609e7e0143dbb475c8e9eee
pwl <1179301+p33t@users.noreply.github.com>
2011-05-04 23:40:44 +0000

    [learn-scala-lift] Forms: LiftScreen

--- learn_scala/lift/src/main/scala/code/snippet/FormLiftScreen.scala
+++ learn_scala/lift/src/main/scala/code/snippet/FormLiftScreen.scala
@@ -1,11 +1,22 @@
 package code.snippet
 
+import net.liftweb._
+import util._
 import net.liftweb.http.{S, LiftScreen}
+import net.liftweb.util.FieldError
+import Helpers._
 
 object FormLiftScreen extends LiftScreen {
   // Need only declare the field and any restrictions
-  val message = field("Message", "", valMinLen(5, "Must be >= 5 chars"))
+  val message = field("Message", "", validateMessage _)
+
   def finish() {
     S.notice("Message is: " + message.is)
+  }
+
+  private def validateMessage(s: String): List[FieldError] = {
+    var result = valMinLen(5, "Must be >= 5 chars")(s)
+    if (asInt(s).isDefined) result = FieldError(message, "Message cannot be a number") :: result
+    result
   }
 }
