0bee1677851fe861ed5a0cb92bd170197bb5fd0f
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-05-04 01:26:28 +0000

    [learn-scala-lift] Redirection.

---
+++ learn_scala/lift/src/main/scala/code/snippet/RedirectNow.scala
@@ -0,0 +1,13 @@
+package code.snippet
+
+import xml.NodeSeq
+import net.liftweb.http.S
+
+
+object RedirectNow {
+  def render(in: NodeSeq) = {
+    S.notice("You've been redirected.")
+    S.redirectTo("/experiments/embed.html")
+    NodeSeq.Empty
+  }
+}
