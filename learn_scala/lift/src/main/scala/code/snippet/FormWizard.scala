cf7dcd92976e6cc5670545b39f81f8e04e812d44
pwl <1179301+p33t@users.noreply.github.com>
2011-05-04 23:57:08 +0000

    [learn-scala-lift] Forms: Wizards.

---
+++ learn_scala/lift/src/main/scala/code/snippet/FormWizard.scala
@@ -0,0 +1,18 @@
+package code.snippet
+
+import net.liftweb._
+import http.S
+import wizard._
+object FormWizard extends Wizard {
+  val screen1 = new Screen {
+    val message1 = field("Message One:", "<one>", valMinLen(6, "Must be >= 6 chars"))
+  }
+  val screen2 = new Screen {
+    val message2 = field("Message Two:", "<two>", valMinLen(6, "Must be >= 6 chars"))
+  }
+
+  def finish() = {
+    S.notice("1: " + screen1.message1.is)
+    S.notice("2: " + screen2.message2.is)
+  }
+}
