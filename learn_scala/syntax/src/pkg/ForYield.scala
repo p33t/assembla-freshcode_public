9581663b4d2574a6e36676123f066d55a34e45b1
pwl <1179301+p33t@users.noreply.github.com>
2011-05-05 05:31:19 +0000

    [learn] Misc: for-yield (options)

--- learn_scala/syntax/src/pkg/ForYield.scala
+++ learn_scala/syntax/src/pkg/ForYield.scala
@@ -4,11 +4,26 @@ package pkg
 object ForYield {
   val data = List(1, 2, 3, 4) :: List(5, 6, 7, 8) :: List(9, 10) :: Nil
 
-  def main(args: Array[String]) {
+  def multiClauseComprehension {
     val evens = for {list <- data if list.size > 3
                      num <- list if num % 2 == 0}
-      // Note: The yield statement must come immediately after the clauses
+    // Note: The yield statement must come immediately after the clauses
     yield "Even number: " + num
-    println("Hopefully 2,4,6,8:" + evens)
+    require(List(2, 4, 6, 8).map("Even number: " + _) == evens)
+  }
+
+  def optionType {
+    def check(o: Option[String]) = {
+      for (s <- o) yield s + s
+    }
+
+    require(Some("abcabc") == check(Some("abc")))
+    require(None == check(None))
+  }
+
+  def main(args: Array[String]) {
+    multiClauseComprehension
+    optionType
+    println("No errors: " + this)
   }
 }
