dd8854b08f28d08ab17813506ae206fc800b1f8f
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-05-05 03:32:13 +0000

    [learn-scala-lift] Forms:  simple URL Parameters.

---
+++ learn_scala/lift/src/main/scala/code/snippet/FormUrlParam.scala
@@ -0,0 +1,17 @@
+package code.snippet
+
+import xml.NodeSeq
+import net.liftweb.http.S
+import net.liftweb.common.Full
+
+
+object FormUrlParam {
+  def render = {
+    S.param("message") match {
+      case Full(message) => S.notice("Message was: " + message)
+      case _ => // nothing
+    }
+
+    (in: NodeSeq) => in
+  }
+}
