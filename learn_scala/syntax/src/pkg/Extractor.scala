cb5a8f17d168ab9cc435bc58b018293949a658fd
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-05-05 06:53:31 +0000

    [learn] Extractor: strange infix notation

--- learn_scala/syntax/src/pkg/Extractor.scala
+++ learn_scala/syntax/src/pkg/Extractor.scala
@@ -3,17 +3,26 @@ package pkg
 
 object Extractor {
   def main(args: Array[String]) {
-    val sp = "bruce-lee"
-    sp match {
-      case StringPair(Array("bruce", "willis")) => println("weird")
-      case StringPair(Array("bruce", "lee")) => println("normal")
+    def matchThis(s: String) {
+      s match {
+        case StringPair("bruce", "willis") => println("weird")
+        case StringPair("bruce", "lee") => println("normal")
+          // Apparently this is called 'infix notation'.
+          // See https://github.com/dpp/simply_lift/blob/master/samples/http_rest/src/main/scala/code/lib/BasicWithHelper.scala#L46
+        case "bruce" StringPair "springsteen" => println("strange syntax")
+      }
     }
+
+
+    matchThis("bruce-lee")
+    matchThis("bruce-springsteen")
   }
 
 
   object StringPair {
-    def unapply(sp: String): Option[Array[String]] = {
-      if (sp.contains("-")) Some(sp.split("-"))
+    def unapply(sp: String): Option[(String,String)] = {
+      val ix = sp.indexOf('-')
+      if (ix >= 0) Some((sp.substring(0, ix), sp.substring(ix + 1)))
       else None
     }
   }
