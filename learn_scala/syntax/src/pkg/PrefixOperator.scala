51277f3fe5ea4b6f8a36c449f9a1a60b16d6b3be
pwl <1179301+p33t@users.noreply.github.com>
2011-05-05 06:06:58 +0000

    [learn] Misc: prefix operator

--- learn_scala/syntax/src/pkg/PrefixOperator.scala
+++ learn_scala/syntax/src/pkg/PrefixOperator.scala
@@ -7,12 +7,22 @@ object PrefixOperator {
     def unary_! = {
       new Rich(v.reverse)
     }
+
+    def unary_up = {
+      new Rich(v toUpperCase)
+    }
   }
 
   def main(args: Array[String]) {
     val r = new Rich("hello")
     val r2 = !r
     require(r2.v == "olleh")
+
+    // Does not compile..... only ! ~ + - are allowed as unary prefix
+//    val r3 = up r
+    val r3 = r.unary_up
+    require(r3.v == "HELLO")
+
     println("No error " + this)
   }
 }
