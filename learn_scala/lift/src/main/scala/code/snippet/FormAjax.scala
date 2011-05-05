9f45821156036e91a4ed57262b38b3b22b65a289
pwl <1179301+p33t@users.noreply.github.com>
2011-05-05 02:09:12 +0000

    [learn-scala-lift] Forms:  Ajax... spinner not working yet.

---
+++ learn_scala/lift/src/main/scala/code/snippet/FormAjax.scala
@@ -0,0 +1,29 @@
+package code.snippet
+
+import net.liftweb._
+import http._
+import js._
+import JsCmds._
+import util._
+import Helpers._
+
+
+object FormAjax {
+  def render = {
+    var message = ""
+    val referer = S.referer openOr "/"
+    def process: JsCmd = {
+
+      // pause for effect
+      Thread.sleep(500)
+
+      if (message.length < 5) {
+        S.error("Message must be >= 5 chars: " + message)
+        Noop
+      }
+      else RedirectTo(referer, () => S.notice("Message: " + message))
+    }
+
+    "name=message" #> (SHtml.text(message, message = _) ++ SHtml.hidden(process _))
+  }
+}
