837cca0b550bb05a7f3986a37f2b2380e99d0298
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-05-11 06:41:19 +0000

    [learn-scala-lift] Menu: Center the menu.

--- learn_scala/lift/src/main/scala/code/snippet/CustomMenu.scala
+++ learn_scala/lift/src/main/scala/code/snippet/CustomMenu.scala
@@ -1,16 +1,26 @@
 package code.snippet
 
 import net.liftweb.widgets.menu.MenuWidget
-import xml.NodeSeq
-
+import xml.{Text, NodeSeq}
 
 object CustomMenu {
+
+  private def styleElem = {
+    <style type="text/css">
+      {Text("/*float center effect*/\n" +
+      ".menu-wrapper{float: right; position: relative; left: -50%; text-align: left; margin-bottom: 2em;}\n" +
+      "ul.sf-menu{left: 50%; position: relative;}")}
+    </style>
+  }
+
   def render(in: NodeSeq) = {
     // Need to get MenuWidget to provide the plumbing
     // We render menu ourselves because MenuWidget doesn't seem to do it properly.
-    MenuWidget(List("No Group")) ++
-        <lift:Menu.builder top:class="sf-menu" linkToSelf="true" expandAll="true"/> ++
-    // This div terminates the floating effects properly.
+    MenuWidget(List("No Group")) ++ styleElem ++
+      <div class="menu-wrapper">
+          <lift:Menu.builder top:class="sf-menu" linkToSelf="true" expandAll="true"/>
+      </div> ++
+      // This div terminates the floating effects properly.
       <div style="height:1px; overflow:hidden; margin-top:-1px; clear:both;"></div>
   }
 }
