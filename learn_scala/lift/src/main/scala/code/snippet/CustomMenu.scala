2cc42740443a19f93be12c0618265fe579a935d4
pwl <1179301+p33t@users.noreply.github.com>
2011-05-11 06:29:24 +0000

    [learn-scala-lift] Menu: Only partially use MenuWidget...ouch.

--- learn_scala/lift/src/main/scala/code/snippet/CustomMenu.scala
+++ learn_scala/lift/src/main/scala/code/snippet/CustomMenu.scala
@@ -1,46 +1,16 @@
 package code.snippet
 
 import net.liftweb.widgets.menu.MenuWidget
-import xml.{Text, NodeSeq}
+import xml.NodeSeq
 
 
 object CustomMenu {
-  private val styleBody = "/* Customisations to get menu working smoothly */\n" +
-    ".sf-menu span {\n" +
-    "  display: block;\n" +
-    "  position: relative;\n" +
-    "  border-left: 1px solid #fff;\n" +
-    "  border-top: 1px solid #CFDEFF;\n" +
-    "  padding: .75em 1em;\n" +
-    "  text-decoration: none;\n" +
-    "}\n" +
-    "ul.sf-menu {\n" +
-    "  left: 50%;\n" +
-    "  position: relative;\n" +
-    "}\n" +
-    ".menu-wrapper { /* Causes center floating-ish*/ \n" +
-    "  float: right;\n" +
-    "  position: relative;\n" +
-    "  left: -50%;\n" +
-    "  text-align: left;\n" +
-    "  margin-bottom: 2em;\n" +
-    "}\n" +
-    ".menu-chaser { /* clears out the floating effects */\n" +
-    "  height:1px;\n" +
-    "  overflow:hidden;\n" +
-    "  margin-top:-1px;\n" +
-    "  clear:both;\n" +
-    "}"
-
-  private def style = {
-    <style type="text/css">
-      {Text(styleBody)}
-    </style>
-  }
-
   def render(in: NodeSeq) = {
-    style ++ <div class="menu-wrapper">
-      {MenuWidget()}
-    </div> ++ <div class="menu-chaser"></div>
+    // Need to get MenuWidget to provide the plumbing
+    // We render menu ourselves because MenuWidget doesn't seem to do it properly.
+    MenuWidget(List("No Group")) ++
+        <lift:Menu.builder top:class="sf-menu" linkToSelf="true" expandAll="true"/> ++
+    // This div terminates the floating effects properly.
+      <div style="height:1px; overflow:hidden; margin-top:-1px; clear:both;"></div>
   }
 }
