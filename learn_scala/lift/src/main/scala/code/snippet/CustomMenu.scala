da75e11d5814885db91d79585d17e41fe891689f
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-05-12 02:02:20 +0000

    [learn-scala-lift] Menu: Refactor and fix Firefox issue.

--- learn_scala/lift/src/main/scala/code/snippet/CustomMenu.scala
+++ learn_scala/lift/src/main/scala/code/snippet/CustomMenu.scala
@@ -7,20 +7,35 @@ object CustomMenu {
 
   private def styleElem = {
     <style type="text/css">
-      {Text("/*float center effect*/\n" +
-      ".menu-wrapper{float: right; position: relative; left: -50%; text-align: left; margin-bottom: 2em;}\n" +
-      "ul.sf-menu{left: 50%; position: relative;}")}
+      {Text(
+      ".float-center-item {\n" +
+        "  left: 50%;\n" +
+        "  position: relative;\n" +
+        "}\n\n" +
+        ".float-center-wrapper {\n" +
+        "  float: right;\n" +
+        "  position: relative;\n" +
+        "  left: -50%;\n" +
+        "}\n\n" +
+        ".float-clear {\n" +
+        "  clear:both;\n" +
+        "}\n" +
+        "ul.sf-menu {margin-bottom: 0}"
+    )}
     </style>
   }
 
   def render(in: NodeSeq) = {
     // Need to get MenuWidget to provide the plumbing
     // We render menu ourselves because MenuWidget doesn't seem to do it properly.
-    MenuWidget(List("No Group")) ++ styleElem ++
-      <div class="menu-wrapper">
-          <lift:Menu.builder top:class="sf-menu" linkToSelf="true" expandAll="true"/>
+    MenuWidget(List("No Group")) ++
+      <head>
+        {styleElem}
+      </head> ++
+      <div class="float-center-wrapper">
+          <lift:Menu.builder top:class="sf-menu float-center-item" linkToSelf="true" expandAll="true"/>
       </div> ++
       // This div terminates the floating effects properly.
-      <div style="height:1px; overflow:hidden; margin-top:-1px; clear:both;"></div>
+      <div class="float-clear"></div>
   }
 }
