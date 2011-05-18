6dae366206f61505cadddc3c9051dfb56efc26aa
pwl <1179301+p33t@users.noreply.github.com>
2011-05-18 07:25:33 +0000

    [learn-scala-lift] CssSelect Demo.

--- learn_scala/lift/src/main/scala/code/snippet/CssSelectDemo.scala
+++ learn_scala/lift/src/main/scala/code/snippet/CssSelectDemo.scala
@@ -44,18 +44,41 @@ object CssSelectDemo {
         <b2 attr="b2attr"/>
     </a>
     test(in, "*", <c/>, "Retain top attributes only") ::
-    test(in, "* *", <c/>, "Replace children") ::
-    test(in, "* ^*", Text("Doesn't matter"), "Elevate children (arg ignored)") ::
-    test(in, "* ^^", Text("Doesn't matter"), "Noop (arg ignored)") ::
+      test(in, "* *", <c/>, "Replace children") ::
+      test(in, "* ^*", Text("Doesn't matter"), "Elevate children (arg ignored)") ::
+      test(in, "* ^^", Text("Doesn't matter"), "Noop (arg ignored)") ::
       Nil
   }
 
+  def run(in: NodeSeq, selector: String, arg: NodeSeq): NodeSeq = {
+    (selector #> arg)(in)
+  }
+
   private def test(in: NodeSeq, selector: String, arg: NodeSeq, desc: String) = {
-    val result = (selector #> arg)(in)
+    val result = run(in, selector, arg)
     TestItem(in, selector, arg, result, desc)
   }
 
   def main(args: Array[String]) {
     println(tests)
+
+    println("Compound?")
+    val in = <a1 a1-attr="a1-attr">
+        <a1b1 a1b1-attr="a1b1-attr"/>
+    </a1> ++ 
+      <a2 a2-attr="a2-attr">
+          <a2b1 a2b1-attr="a2b1-attr"/>
+      </a2>
+
+    // Hmm... this appears to use only the first element's children
+    // Basically it assumes only one child and mangles if it is not
+    def bonusAttribOnChildren(in: NodeSeq): NodeSeq = {
+      val children = ("* ^*" #> NodeSeq.Empty)(in)
+      val altChildren = ("* [class+]" #> "bonus-class")(children)
+      ("* *" #> altChildren)(in)
+    }
+
+    val out = bonusAttribOnChildren(in)
+    println(pretty.formatNodes(out))
   }
 }
