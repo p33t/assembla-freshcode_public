50775cc506a4075c3aefa62332ccef099570fbca
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-05-24 03:55:06 +0000

    [learn-scala-lift] Ajax comms.

--- learn_scala/lift/src/main/scala/code/snippet/AjaxDemo.scala
+++ learn_scala/lift/src/main/scala/code/snippet/AjaxDemo.scala
@@ -1,12 +1,17 @@
 package code.snippet
 
 import net.liftweb._
+import http.js.jquery.JqWiringSupport
+import http.{WiringUI, SHtml}
 import util._
 import Helpers._
+import xml.Text
 
 object AjaxDemo {
+  val feedbackCell = ValueCell("default message");
+
   def feedback() = {
-    "* ^^" #> "ignored"
+    "*" #> WiringUI.toNode(feedbackCell, JqWiringSupport.fade)((str, in) => <p>{Text(str)}</p>)
   }
 
   def basic() = {
