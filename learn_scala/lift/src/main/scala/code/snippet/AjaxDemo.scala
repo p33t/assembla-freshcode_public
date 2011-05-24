cf25aaaca5443f2b212805298f46e81ee1b41a5f
pwl <1179301+p33t@users.noreply.github.com>
2011-05-24 05:37:37 +0000

    [learn-scala-lift] Ajax: basic.

--- learn_scala/lift/src/main/scala/code/snippet/AjaxDemo.scala
+++ learn_scala/lift/src/main/scala/code/snippet/AjaxDemo.scala
@@ -1,7 +1,12 @@
 package code.snippet
 
 import net.liftweb._
+import common.Empty
+import http.js.JE.JsRaw
 import http.js.jquery.JqWiringSupport
+import http.js.JsCmds.CmdPair
+import http.js.{JsCmd, JsCmds}
+import net.liftweb.http.js.JsCmds._
 import http.{WiringUI, SHtml}
 import util._
 import Helpers._
@@ -11,10 +16,12 @@ object AjaxDemo {
   val feedbackCell = ValueCell("default message");
 
   def feedback() = {
-    "*" #> WiringUI.toNode(feedbackCell, JqWiringSupport.fade)((str, in) => <p>{Text(str)}</p>)
+    "*" #> WiringUI.toNode(feedbackCell, JqWiringSupport.fade)((str, in) => <p>
+      {Text(str)}
+    </p>)
   }
 
   def basic() = {
-    "* ^^" #> "ignored"
+    "href=# [onclick]" #>  List(Alert("Yay!"), JsReturn(false)).toJsCmd
   }
 }
