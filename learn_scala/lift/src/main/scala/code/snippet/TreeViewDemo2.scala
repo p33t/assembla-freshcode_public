8c2365e2aeca7d465690a3a4678eb8adf059eed1
pwl <1179301+p33t@users.noreply.github.com>
2011-05-24 01:56:10 +0000

    [learn-scala-lift] Treeview: Trying to figure out better separation.

---
+++ learn_scala/lift/src/main/scala/code/snippet/TreeViewDemo2.scala
@@ -0,0 +1,10 @@
+package code.snippet
+
+import xml.NodeSeq
+
+object TreeViewDemo2 {
+  def render(in: NodeSeq): NodeSeq = {
+    // TODO: Extract mini template for [A File] and [A Folder] and render accordingly.
+    in
+  }
+}
