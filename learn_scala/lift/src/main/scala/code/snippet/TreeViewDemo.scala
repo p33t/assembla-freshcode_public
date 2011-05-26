d1d38c11d3cc58b66bee9156ab54e6c8995939a3
pwl <1179301+p33t@users.noreply.github.com>
2011-05-26 05:36:15 +0000

    [learn-scala-lift] Treeview

--- learn_scala/lift/src/main/scala/code/snippet/TreeViewDemo.scala
+++ learn_scala/lift/src/main/scala/code/snippet/TreeViewDemo.scala
@@ -1,17 +1,10 @@
 package code.snippet
 
 import xml.NodeSeq
-import net.liftweb._
-import http.js._
-import http.S
-import JE._
-import widgets.tree.TreeView
-
 
 object TreeViewDemo {
-  def standard(in: NodeSeq): NodeSeq = {
-    val ul_id = S.attr("ul_id") openOr error("The ul_id attribute is required.")
-    // See http://docs.jquery.com/Plugins/Treeview/treeview#options
-    TreeView(ul_id, JsObj(("animated" -> "slow"), ("collapsed" -> true))) ++ in
+  def render(in: NodeSeq): NodeSeq = {
+    // TODO: Extract mini template for [A File] and [A Folder] and render accordingly.
+    in
   }
 }
