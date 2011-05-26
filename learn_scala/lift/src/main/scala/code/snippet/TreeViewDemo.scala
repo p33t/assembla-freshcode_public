24cd0060f54fe96cc5efb95e576ef9f90a69dc9c
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-05-26 06:31:39 +0000

    [learn-scala-lift] Treeview

--- learn_scala/lift/src/main/scala/code/snippet/TreeViewDemo.scala
+++ learn_scala/lift/src/main/scala/code/snippet/TreeViewDemo.scala
@@ -5,11 +5,20 @@ import Helpers._
 import xml._
 
 object TreeViewDemo {
+
   def render(in: NodeSeq): NodeSeq = {
     val sampleFile = (".sampleFile ^^" #> "")(in)
     val sampleFolder = (".sampleFolder ^^" #> "" & "ul" #> NodeSeq.Empty)(in) // clear out nested 'ul's
-    // TODO: Change this to the real thing.  It only puts in one file an one folder.
-    val funct = "ul *" #> (sampleFolder ++ sampleFile)
+    def fileNode(name: String) = {
+      (".sampleText" #> name)(sampleFile)
+    }
+    // TODO: Add folders and nesting.
+    def folderNode(name: String, contents: NodeSeq) {
+      (".sampleText" #> name & "* *+" #> contents)(sampleFolder)
+    }
+    val funct = "ul *" #> (
+      fileNode("file 1")
+      )
     funct(in)
   }
 }
