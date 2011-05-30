78511933f580c5c698df92eeeebff59e7352cb0f
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-05-30 04:13:56 +0000

    [scala-testing] ScalaTests Basics.

---
+++ learn_scala/testing/src/test/scala/pkg/PureScalaFunctionTest.scala
@@ -0,0 +1,10 @@
+package pkg
+
+import org.scalatest.FunSuite
+
+class PureScalaFunctionTest extends FunSuite {
+  test("Some test description") {
+    // the function being supplied
+    println("test in a function")
+  }
+}
