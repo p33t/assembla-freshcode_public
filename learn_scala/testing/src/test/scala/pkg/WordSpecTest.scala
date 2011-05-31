9120e263349223dcf71502a326b6a9d0e50c52e8
pwl <1179301+p33t@users.noreply.github.com>
2011-05-31 23:17:31 +0000

    [scala-testing] ScalaTests WordSpec.

--- learn_scala/testing/src/test/scala/pkg/WordSpecTest.scala
+++ learn_scala/testing/src/test/scala/pkg/WordSpecTest.scala
@@ -4,14 +4,16 @@ import org.scalatest.WordSpec
 import org.scalatest.matchers.MustMatchers
 
 class WordSpecTest extends WordSpec with MustMatchers {
+  val never = afterWord("never") // custom word reuse
+
   "A file path" when {
     "has multiple elements" must {
       "have a / separating the elements" in {}
       "cannot have 2 / in a row" in {}
     }
-    "has only one element" must {
-      "not have any / chars" in {}
-      "cannot be blank" in {}
+    "has only one element" must never {
+      "have any / chars" in {}
+      "be blank" in {}
     }
   }
 }
