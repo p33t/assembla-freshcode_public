8db85f113bc432442e2aa5f80c25e5a9a2326fa4
pwl <1179301+p33t@users.noreply.github.com>
2011-06-01 01:43:07 +0000

    [learn-scala-lift] Treeview

--- learn_scala/lift/src/main/scala/code/snippet/TreeViewDemo.scala
+++ learn_scala/lift/src/main/scala/code/snippet/TreeViewDemo.scala
@@ -15,11 +15,14 @@ object TreeViewDemo {
     val sampleFolder = (".sampleFolder ^^" #> "" & // lift template
       ".sampleFolder [class]" #> blank & // erase 'sampleFolder'
       "ul" #> NodeSeq.Empty)(in) // clear out nested 'ul's
+    def onclick(name: String) = "href=# [onclick]" #> ("console.log('lift clicked on: " + name + "'); return false;")
     def fileNode(name: String) = {
-      (".sampleText" #> name)(sampleFile)
+      (".sampleText" #> name &
+        onclick(name))(sampleFile)
     }
     def folderNode(name: String, contents: NodeSeq = NodeSeq.Empty) = {
-      var out = (".sampleText" #> name)(sampleFolder)
+      var out = (".sampleText" #> name &
+        onclick(name))(sampleFolder)
       if (!contents.isEmpty) out = ("* *+" #> <ul>
         {contents}
       </ul>)(out)
