eadd00b9810ab79c361ceb990187065f4ad8bd59
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-06-07 12:19:56 +0000

    [swing_shots] Hourglass thread checks.

---
+++ swing_shots/src/test/java/biz/freshcode/swing_shots/ui/util/HourglassTest.java
@@ -0,0 +1,24 @@
+package biz.freshcode.swing_shots.ui.util;
+
+import org.testng.annotations.Test;
+
+public class HourglassTest {
+
+    @Test
+    public void testThreadCheck() {
+        Hourglass h = new Hourglass();
+        try {
+        h.surround(new Hourglass.Worker() {
+            public void doInBackground() {
+                // nothing
+            }
+
+            public void done() {
+                // nothing
+            }
+        });
+        } catch (IllegalStateException ex) {
+            // expected
+        }
+    }
+}
