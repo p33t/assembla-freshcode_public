df13f40e28aa104847ffedcc1559a6fca4af56fc
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-06-09 03:40:36 +0000

    [learn-squeryl] Getting started.

---
+++ learn_scala/squeryl/src/test/scala/pkg/FirstTest.scala
@@ -0,0 +1,11 @@
+package pkg
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class FirstTest extends FlatSpec with ShouldMatchers {
+  "void subject" should "operate just fine" in {
+    // nothing
+  }
+
+}
