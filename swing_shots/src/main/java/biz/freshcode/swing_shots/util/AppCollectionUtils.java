c2aac7f150e8725495e8b20a2cb61ee4a130ba2f
pwl <1179301+p33t@users.noreply.github.com>
2011-06-10 11:33:12 +0000

    [swing_shots] H2Database.update,query,execute implemented.

--- swing_shots/src/main/java/biz/freshcode/swing_shots/util/AppCollectionUtils.java
+++ swing_shots/src/main/java/biz/freshcode/swing_shots/util/AppCollectionUtils.java
@@ -1,10 +1,13 @@
 package biz.freshcode.swing_shots.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class AppCollectionUtils {
-    public static <T> List<T> newList() {
-        return new ArrayList<T>();
+    public static <T> List<T> newList(T... elems) {
+        ArrayList<T> l = new ArrayList<T>();
+        Collections.addAll(l, elems);
+        return l;
     }
 }
