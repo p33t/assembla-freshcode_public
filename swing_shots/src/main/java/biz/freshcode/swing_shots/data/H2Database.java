a296f2a5717a98a79924b837e5ff761c05a09140
pwl <1179301+p33t@users.noreply.github.com>
2011-06-10 06:07:38 +0000

    [swing_shots] Introduce JdbcTemplate.

--- swing_shots/src/main/java/biz/freshcode/swing_shots/data/H2Database.java
+++ swing_shots/src/main/java/biz/freshcode/swing_shots/data/H2Database.java
@@ -4,6 +4,8 @@ import biz.freshcode.swing_shots.logging.Logging;
 import org.h2.jdbcx.JdbcConnectionPool;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.DisposableBean;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import static biz.freshcode.swing_shots.config.Bootstrap.APP_HOME;
@@ -12,31 +14,40 @@ import static java.io.File.separator;
 @Component
 public class H2Database implements DisposableBean {
     public static final String DB_PATH = APP_HOME.get() + separator + "data";
-    private JdbcConnectionPool poolOrNull;
     @Logging private Logger log;
+    private JdbcOperations ops = null;
 
     public void openExisting() {
         log.info("Opening database...\n   NOTE: If this doesn't work remember the stack traces are saved in " + DB_PATH);
         String s = "jdbc:h2:" + DB_PATH + separator + "db";
-        poolOrNull = JdbcConnectionPool.create(s, "", "");
+        JdbcConnectionPool pool = JdbcConnectionPool.create(s, "", "");
+        ops = new JdbcTemplate(pool);
         log.info("Database successfully opened.");
     }
 
     public boolean isOpen() {
-        return poolOrNull != null;
+        return ops != null;
     }
 
     public void close() {
         log.info("Closing database.");
         try {
-            poolOrNull.dispose();
+            pool().dispose();
         } finally {
-            poolOrNull = null;
+            ops = null;
         }
     }
 
     @Override
     public void destroy() throws Exception {
         if (isOpen()) close();
+    }
+
+    private JdbcTemplate template() {
+        return (JdbcTemplate) ops;
+    }
+
+    private JdbcConnectionPool pool() {
+        return (JdbcConnectionPool) template().getDataSource();
     }
 }
