fdea3b5b68a1021d1880d9d348c4c1b74ce33a34
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-07-04 07:30:29 +0000

    [scala-lift] On way to charting

---
+++ learn_scala/lift/src/main/scala/code/snippet/ChartDemo.scala
@@ -0,0 +1,18 @@
+package code.snippet
+
+import net.liftweb._
+import http.js.jquery.JqWiringSupport
+import http.js.{JsCmd, JsCmds}
+import JsCmds._
+import http.{WiringUI, SHtml}
+import util._
+import Helpers._
+import java.util.Date
+import java.lang.Thread
+import xml.{Node, Elem, Text, NodeSeq}
+
+object ChartDemo {
+  def defaultData(in: NodeSeq): NodeSeq = {
+    NodeSeq.Empty
+  }
+}
