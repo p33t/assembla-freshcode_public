753ab314d7b09c6989095c59c24a50cb23bafffc
pwl <1179301+p33t@users.noreply.github.com>
2011-07-05 00:54:29 +0000

    [learn-scala-lift] Flot demo - Data defined from scala.

--- learn_scala/lift/src/main/scala/code/snippet/ChartDemo.scala
+++ learn_scala/lift/src/main/scala/code/snippet/ChartDemo.scala
@@ -6,14 +6,32 @@ import http.js.jquery.JqWiringSupport
 import http.js.{JsCmd, JsCmds}
 import JsCmds._
 import http.{WiringUI, SHtml}
+import json.JsonAST.JValue
+import json.{JsonAST, Serialization, Extraction, DefaultFormats}
 import util._
 import Helpers._
 import java.util.Date
 import java.lang.Thread
 import xml.{Node, Elem, Text, NodeSeq}
+import java.io.StringWriter
 
 object ChartDemo {
+  private implicit val formats = DefaultFormats // for Json conversion
   def defaultData(in: NodeSeq): NodeSeq = {
-    Script(JsRaw("dataD = [[0.0, 3.0], [0.25, 3.0], [0.5, 1.0], [0.75, 3.0], [1.0, 3.0]]"))
+    Script(JsRaw("dataD = " + dataDString()))
+  }
+
+  def dataDString(): String = {
+    //causes an exception... Serialization.write(dataD())
+    val doc = JsonAST.render(dataD())
+    val writer = new StringWriter()
+    doc.format(80, writer)
+    writer.flush()
+    writer.toString
+  }
+
+  def dataD(): JValue = {
+    val v = List(List(0.0, 3.0), List(0.25, 3.0), List(0.5, 1.0), List(0.75, 3.0), List(1.0, 3.0))
+    Extraction.decompose(v)
   }
 }
