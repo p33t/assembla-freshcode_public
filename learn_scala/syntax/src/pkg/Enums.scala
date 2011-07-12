4d1de02102a7efd820b5f73cc6fe5245ebfff02f
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-07-12 05:35:37 +0000

    [learn] More var args.

--- learn_scala/syntax/src/pkg/Enums.scala
+++ learn_scala/syntax/src/pkg/Enums.scala
@@ -7,12 +7,19 @@ object Enums {
     val North, South = Value
   }
 
+  def varArgs(dirs: Direction.Value*) = {
+    dirs.length
+  }
+
+
   def main(args: Array[String]) {
     println(Direction.values)
     println(Direction.North)
     // Integer id (0-based)
     println(Direction.North.id)
     val n = Direction(Direction.North.id)
-    println("Hopfully north: " + n)
+    println("Hopefully north: " + n)
+
+    println("Should be three: " + varArgs(Direction.North, Direction.South, Direction.North))
   }
 }
