18560b41b878a1792f136fa479406fb7deb59bbb
pwl <1179301+p33t@users.noreply.github.com>
2011-08-01 00:07:23 +0000

    [learn-lift] Json serialising.

--- learn_scala/lift/src/test/scala/pkg/JsonTest.scala
+++ learn_scala/lift/src/test/scala/pkg/JsonTest.scala
@@ -8,7 +8,7 @@ import net.liftweb.json.Printer._
 class JsonTest extends Suite {
   implicit val formats = net.liftweb.json.DefaultFormats
 
-  def testJsonSerialize() {
+  def testSerializeMap() {
 
     val m = Map("one" -> 1, "two" -> 2, "three" -> List(3, 3, 3))
     expect("""{"one":1,"two":2,"three":[3,3,3]}""") {compact(render(decompose(m)))}
