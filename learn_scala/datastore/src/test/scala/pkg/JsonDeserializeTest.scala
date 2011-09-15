18169ac94568563797d621369d6708f33604f918
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-09-15 04:04:03 +0000

    [scala-datastore] Json rep of complex types.

--- learn_scala/datastore/src/test/scala/pkg/JsonDeserializeTest.scala
+++ learn_scala/datastore/src/test/scala/pkg/JsonDeserializeTest.scala
@@ -2,10 +2,7 @@ package pkg
 
 import org.scalatest.Suite
 import org.testng.annotations.Test
-import net.liftweb.json.{Extraction, TypeInfo, Formats, DefaultFormats}
-import net.liftweb.json.Extraction._
-import net.liftweb.json.Printer._
-import net.liftweb.json.JsonAST._
+import net.liftweb.json._
 
 @Test
 class JsonDeserializeTest extends Suite {
@@ -13,8 +10,9 @@ class JsonDeserializeTest extends Suite {
 
   def testSerialize() {
     implicit val JsonFormats = DefaultFormats // + Parent.Serializer // for Json conversion
-    val str = compact(render(decompose(TestParent)))
-    println(str)
+    val str = Printer.compact(JsonAST.render(Extraction.decompose(TestParent)))
+    val actual = JsonParser.parse(str).extract[Parent]
+    expect(TestParent)(actual)
   }
 
 }
@@ -23,14 +21,14 @@ case class Parent(f1: String, f2: String, f3: Child)
 
 case class Child(f: String)
 
-object Parent {
-
-  object Serializer extends net.liftweb.json.Serializer[Parent] {
-    def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), Parent] = Map.empty
-
-    def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
-      case p: Parent => Extraction.decompose(List(p.f2, p.f1))(format)
-    }
-  }
-
-}
+//object Parent {
+//
+//  object Serializer extends net.liftweb.json.Serializer[Parent] {
+//    def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), Parent] = Map.empty
+//
+//    def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
+//      case p: Parent => Extraction.decompose(List(p.f2, p.f1))(format)
+//    }
+//  }
+//
+//}
