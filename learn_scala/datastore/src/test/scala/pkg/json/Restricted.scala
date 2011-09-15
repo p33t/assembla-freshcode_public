88843ced2c55b4f186b4bf4822129e74bc1b4364
pwl <1179301+p33t@users.noreply.github.com>
2011-09-15 05:10:04 +0000

    [scala-datastore] Json rep of complex types.

--- learn_scala/datastore/src/test/scala/pkg/json/Restricted.scala
+++ learn_scala/datastore/src/test/scala/pkg/json/Restricted.scala
@@ -5,6 +5,7 @@ sealed trait Restricted
 
 case class StringChild(str: String) extends Restricted
 
+// TODO: Why is it when this is changed to a 'case object' it can't be used as a type arg?
 class VoidChild extends Restricted {
   override def hashCode() = 42
 
