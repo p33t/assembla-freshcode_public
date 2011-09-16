54d2a47b95a7d1779f0d2a8827f9af79395ab4ec
pwl <1179301+p33t@users.noreply.github.com>
2011-09-16 00:58:06 +0000

    [scala-datastore] Deserializing json subclasses

--- learn_scala/datastore/src/test/scala/pkg/json/JsonDeserializeTest.scala
+++ learn_scala/datastore/src/test/scala/pkg/json/JsonDeserializeTest.scala
@@ -6,18 +6,26 @@ import net.liftweb.json._
 
 @Test
 class JsonDeserializeTest extends Suite {
-  val TestParents = List(
-    Parent("one", "two", Child("three"), new VoidChild),
-    Parent("three", "four", Child("five"), new StringChild("six"))
-  )
+  implicit val JsonFormats = DefaultFormats + RestrictedSerializer // for Json conversion
+  val StringParent = Parent("three", "four", Child("five"), new StringChild("six"))
+  val VoidParent = Parent("one", "two", Child("three"), new VoidChild)
+
 
   def testSerialize() {
-    implicit val JsonFormats = DefaultFormats + RestrictedSerializer // for Json conversion
-    for (p <- TestParents) {
+    def checkParent(p: Parent) {
       val str = Printer.compact(render(Extraction.decompose(p)))
       val actual = parse(str).extract[Parent]
       expect(p)(actual)
     }
+    checkParent(StringParent)
+    checkParent(VoidParent)
   }
 
+  def testIgnoreExtraData() {
+    val ast = Extraction.decompose(StringParent).asInstanceOf[JObject]
+    val astExtra = JObject(JField("extraField", JString("ignored data")) :: ast.obj)
+    expect(StringParent) {
+      astExtra.extract[Parent]
+    }
+  }
 }
