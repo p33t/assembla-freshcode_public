c83faf8294836d3b4a737fb471fce1c4f0433167
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-10-11 05:08:50 +0000

    [scala-datastore] Javascript return types.

--- learn_scala/datastore/src/test/scala/pkg/script/ScriptTypesTest.scala
+++ learn_scala/datastore/src/test/scala/pkg/script/ScriptTypesTest.scala
@@ -4,6 +4,7 @@ import org.scalatest.Suite
 import org.testng.annotations.Test
 import net.liftweb.json.{JsonParser, JsonDSL, JsonAST}
 import javax.script.ScriptEngineManager
+import sun.org.mozilla.javascript.internal.NativeArray
 
 @Test
 class ScriptTypesTest extends Suite {
@@ -12,11 +13,17 @@ class ScriptTypesTest extends Suite {
   def testTypes() {
     checkReturnVal("'hello';", "hello")
     checkReturnVal("99.9;", 99.9)
+    checkReturnVal("99;", 99.0)
+//    checkReturnVal("[1,2];", new NativeArray(
+//      Array[AnyRef](1.0.asInstanceOf[AnyRef], 2.0.asInstanceOf[AnyRef])
+//    ))
+//    checkReturnVal("[1,2,3,4];", Array[java.lang.Object](1, 2, 3, 4))
   }
 
   private def checkReturnVal(script: String, expected: Any) {
+    val result = js.eval(script)
     expect(expected) {
-      js.eval(script)
+      result
     }
   }
 }
