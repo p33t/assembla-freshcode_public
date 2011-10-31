34def6a8b3847580516f7e2fc02ef474e6d9806c
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-10-31 06:33:11 +0000

    [scala-testing] Dependency Injection.

---
+++ learn_scala/testing/src/test/scala/pkg/di/DiOnlyTest.scala
@@ -0,0 +1,75 @@
+package pkg.di
+
+import org.scalatest.Suite
+import org.testng.annotations.Test
+import org.scala_tools.subcut.inject.{Injectable, NewBindingModule, BindingModule}
+import pkg.di.DiOnlyTest._
+
+@Test
+class DiOnlyTest extends Suite {
+  implicit val bm = TheConfig
+  val app = new TheApp
+
+  def testBasic() {
+    val service = app.service
+    expect("hello world") {service.operation()}
+  }
+
+  def testObjectRetention() {
+    // Looks like 'injectIfBound' does not retaing the dynamically created instance for reuse
+    require(app.service.extras != app.altService.extras)
+  }
+
+  def testAltComponent() {
+    // 'modifyBindings' doesn't do what you'd think.  It doesn't return an altered config.
+    // Instead all the test code needs to go into the closure.
+    TheConfig.modifyBindings({
+      m =>
+        m.bind[TheExtra].toInstance(new TheExtra {
+          override def supportMethod() = "bruce"
+        })
+
+        val testApp = new TheApp()(m)
+        expect("hello bruce") {
+          testApp.service.operation()
+        }
+        expect("around the bruce") {
+          testApp.altService.operation()
+        }
+    })
+  }
+}
+
+object DiOnlyTest {
+
+  class TheExtra {
+    def supportMethod() = "world"
+  }
+
+  class TheService(implicit val bindingModule: BindingModule) extends Injectable {
+    val extras = injectIfBound[TheExtra](new TheExtra)
+
+    def operation() = {
+      "hello " + extras.supportMethod()
+    }
+  }
+
+  class AltService(implicit val bindingModule: BindingModule) extends Injectable {
+    val extras = injectIfBound[TheExtra](new TheExtra)
+
+    def operation() = {
+      "around the " + extras.supportMethod
+    }
+  }
+
+  class TheApp(implicit val bindingModule: BindingModule) extends Injectable {
+    val service = injectIfBound[TheService](new TheService)
+    val altService = injectIfBound[AltService](new AltService)
+  }
+
+  object TheConfig extends NewBindingModule({
+    m =>
+    //    m.bind[TheExtra].toInstance(new TheExtra)
+  })
+
+}
