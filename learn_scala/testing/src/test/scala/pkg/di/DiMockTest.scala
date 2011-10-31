7ef00d54b9954abf63d19f58c3781a1e524d12a9
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-10-31 07:03:19 +0000

    [scala-testing] Dependency Injection.

---
+++ learn_scala/testing/src/test/scala/pkg/di/DiMockTest.scala
@@ -0,0 +1,38 @@
+package pkg.di
+
+import com.borachio.scalatest.MockFactory
+import org.scalatest.Suite
+import org.scala_tools.subcut.inject.{NewBindingModule, Injectable, BindingModule}
+import DiMockTest._
+
+class DiMockTest extends Suite with MockFactory {
+  def testConfig() {
+    val app = Config.inject(classOf[App], None)
+    expect("hello world"){app.service.operation()}
+  }
+}
+
+object DiMockTest {
+
+  object Config extends NewBindingModule({
+    m =>
+      m.bind[Deep].toInstance(new Deep()(m))
+      m.bind[Service].toInstance(new Service()(m))
+      m.bind[App].toInstance(new App()(m))
+  })
+
+  class App(implicit val bindingModule: BindingModule) extends Injectable {
+    val service = inject[Service]
+  }
+
+  class Service(implicit val bindingModule: BindingModule) extends Injectable {
+    private val deep = inject[Deep]
+
+    def operation() = "hello " + deep.operation()
+  }
+
+  class Deep(implicit val bindingModule: BindingModule) extends Injectable {
+    def operation() = "world"
+  }
+
+}
