751fdded0973b299f6c44be846f833e0dbec7a2f
pwl <1179301+p33t@users.noreply.github.com>
2011-11-02 01:49:18 +0000

    [learn-scala] Manifests

--- learn_scala/syntax/src/pkg/Manifests.scala
+++ learn_scala/syntax/src/pkg/Manifests.scala
@@ -9,6 +9,16 @@ object Manifests {
 
     val cls = classOf[Bruce]
     classOp(ClassManifest.fromClass(cls))
+
+    val l = List[Class[_]](cls)
+
+    l match {
+      case typelessClass :: _ =>
+        // Not really sure what is going on here.
+        // Thought I'd need a go between method like... assignType[T](cls: Class[T], t: AnyRef): T
+        val mf = ClassManifest.classType(cls)
+        classOp(mf)
+    }
   }
 
   def classOp[T](implicit mf: ClassManifest[T]) {
