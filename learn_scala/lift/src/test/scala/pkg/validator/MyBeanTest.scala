76f2d5ef3600b8103818aa69b87104dcd4a04aa9
pwl <1179301+p33t@users.noreply.github.com>
2011-11-07 05:07:05 +0000

    [learn-lift]Remove testng.

--- learn_scala/lift/src/test/scala/pkg/validator/MyBeanTest.scala
+++ learn_scala/lift/src/test/scala/pkg/validator/MyBeanTest.scala
@@ -1,12 +1,10 @@
 package pkg.validator
 
 import org.scalatest.Suite
-import org.testng.annotations.Test
 import javax.validation.Validation
 // NOTE: This helps java interop
 import collection.JavaConversions._
 
-@Test
 class MyBeanTest extends Suite {
   val validatorFactory = Validation.buildDefaultValidatorFactory();
   val validator = validatorFactory.getValidator;
