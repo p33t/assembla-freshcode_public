e80e5157559570d5f1ef575379981ad1dd8e72f7
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-11-16 01:51:06 +0000

    [scala-datastore] Daylight Savings Illustration

--- learn_scala/datastore/src/test/scala/pkg/dates/DaylightSavingsTest.scala
+++ learn_scala/datastore/src/test/scala/pkg/dates/DaylightSavingsTest.scala
@@ -11,19 +11,16 @@ class DaylightSavingsTest extends Suite {
   val zone = DateTimeZone.forID("Australia/Sydney") // a time zone that has daylight savings
   val chron = ISOChronology.getInstance(zone) // internationally accepted, slightly different to Gregorian.
   val epoch = new DateTime(0L, chron)
-  val begin = epoch.withYear(2011).withMonthOfYear(10).withDayOfMonth(2).withHourOfDay(1).withMinuteOfHour(59) // 2 not allowed
+  val begin = epoch.withYear(2011).withMonthOfYear(10).withDayOfMonth(2).withHourOfDay(3) // 2 not allowed
   val end = epoch.withYear(2012).withMonthOfYear(4).withDayOfMonth(1).withHourOfDay(2)
   val min1 = Period.minutes(1)
 
-  def testBegin() {
-    println("Begin:         " + begin)
-    println("Begin + 1 min: " + begin.plus(min1))
-    println("Begin - 1 min: " + begin.minus(min1))
-  }
-
-  def testEnd() {
-    println("End:         " + end)
-    println("End + 1 min: " + end.plus(min1))
-    println("End - 1 min: " + end.minus(min1))
+  def testBeginEnd() {
+    println("\nBegin:")
+    println(begin.minus(min1))
+    println(begin + " <-- Notice both time zone and hr increment at trans'n")
+    println("\nEnd:")
+    println(end.minus(min1))
+    println(end + " <-- Notice both time zone and hr decrement at trans'n")
   }
 }
