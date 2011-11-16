652b83b2c7313fff58a11a7fffed4ef34e0622ef
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-11-16 02:26:04 +0000

    [scala-datastore] Daylight Savings Illustration

--- learn_scala/datastore/src/test/scala/pkg/dates/DaylightSavingsTest.scala
+++ learn_scala/datastore/src/test/scala/pkg/dates/DaylightSavingsTest.scala
@@ -12,21 +12,24 @@ class DaylightSavingsTest extends Suite {
   val chron = ISOChronology.getInstance(zone) // internationally accepted, slightly different to Gregorian.
   val epoch = new DateTime(0L, chron)
   val begin1am = epoch.withYear(2011).withMonthOfYear(10).withDayOfMonth(2).withHourOfDay(1)
-  val begin3am = epoch.withYear(2011).withMonthOfYear(10).withDayOfMonth(2).withHourOfDay(3) // 2 not allowed
-  val end1am = epoch.withYear(2012).withMonthOfYear(4).withDayOfMonth(1).withHourOfDay(1)
+  val begin3amDst = begin1am.withHourOfDay(3) // 2 not allowed
+  val end1amDst = epoch.withYear(2012).withMonthOfYear(4).withDayOfMonth(1).withHourOfDay(1)
+  // NOTE the ambiguity!
+  val end2amDst = end1amDst.withHourOfDay(2)
   val end2am = epoch.withYear(2012).withMonthOfYear(4).withDayOfMonth(1).withHourOfDay(2)
   val min1 = Period.minutes(1)
 
   def testBegin() {
-    require(begin1am.plusHours(1) == begin3am)
     println("\nBegin:")
-    println(begin3am.minus(min1))
-    println(begin3am + " <-- Notice both time zone and hr increment at trans'n")
+    require(begin1am.plusHours(1) == begin3amDst)
+    println(begin3amDst.minus(min1))
+    println(begin3amDst + " <-- Notice both time zone and hr increment at trans'n")
   }
 
   def testEnd() {
-    require(end1am.plusHours(2) == end2am)
     println("\nEnd:")
+    require(end1amDst.plusHours(2) == end2am)
+    require(end2amDst.plusHours(1) == end2am)
     println(end2am.minus(min1))
     println(end2am + " <-- Notice both time zone and hr decrement at trans'n")
   }
