ffc39e5b9ee37e01c27895df7f9e1bd3f46d015a
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-11-24 05:59:00 +0000

    [learn-extjs] Getting started.

---
+++ learn/extjs/web/app.js
@@ -0,0 +1,18 @@
+// supposed to improve load times and prevent warning regarding synchronous loads
+// it's like an import
+Ext.require('Ext.container.Viewport');
+
+Ext.application({
+    name: 'HelloExt',
+    launch: function() {
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: [
+                {
+                    title: 'Hello Ext',
+                    html : 'Hello! Welcome to Ext JS.'
+                }
+            ]
+        });
+    }
+});
