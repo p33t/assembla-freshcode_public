6399b54c4d0ba8857a9af89e884747d232f930a2
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-11-28 01:08:26 +0000

    [learn-extjs] Layouts & Containers

--- learn/extjs/web/app.js
+++ learn/extjs/web/app.js
@@ -6,11 +6,15 @@ Ext.application({
     name: 'HelloExt',
     launch: function() {
         Ext.create('Ext.container.Viewport', {
-            layout: 'fit',
+//            layout: 'fit',
             items: [
                 {
                     title: 'Hello Ext',
                     html : 'Hello! Welcome to Ext JS.'
+                },
+                {
+                    title: 'Hello 2',
+                    html : 'Hello... again'
                 }
             ]
         });
