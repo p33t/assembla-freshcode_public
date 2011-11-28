055f1aa32a9b81bee7a77a4d1b6a9a88c04c8167
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-11-28 03:33:07 +0000

    [learn-extjs] MVC: Integrating a custom view.

---
+++ learn/extjs/web/app/view/user/List.js
@@ -0,0 +1,23 @@
+Ext.define('LE.view.user.List' ,{
+    extend: 'Ext.grid.Panel',
+    alias : 'widget.userlist', // xtype is 'userlist'
+
+    title : 'All Users',
+
+    initComponent: function() {
+        this.store = {
+            fields: ['name', 'email'],
+            data  : [
+                {name: 'Ed',    email: 'ed@sencha.com'},
+                {name: 'Tommy', email: 'tommy@sencha.com'}
+            ]
+        };
+
+        this.columns = [
+            {header: 'Name',  dataIndex: 'name',  flex: 1},
+            {header: 'Email', dataIndex: 'email', flex: 1}
+        ];
+
+        this.callParent(arguments);
+    }
+});
