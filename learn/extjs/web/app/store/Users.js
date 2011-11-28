d8b8678c5dde3b4754c9ba551cf5626935ecc649
pwl <1179301+p33t@users.noreply.github.com>
2011-11-28 04:07:47 +0000

    [learn-extjs] MVC: edit a grid, intro a 'store'

---
+++ learn/extjs/web/app/store/Users.js
@@ -0,0 +1,8 @@
+Ext.define('LE.store.Users', {
+    extend: 'Ext.data.Store',
+    fields: ['name', 'email'],
+    data: [
+        {name: 'Ed',    email: 'ed@sencha.com'},
+        {name: 'Tommy', email: 'tommy@sencha.com'}
+    ]
+});
