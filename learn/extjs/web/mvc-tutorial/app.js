0c556f5475e7dbab5b75dbda42b8d36730919176
pwl <1179301+p33t@users.noreply.github.com>
2011-12-09 01:02:55 +0000

    [learn-extjs] Application Bootstrapping: Refactor.

--- learn/extjs/web/mvc-tutorial/app.js
+++ learn/extjs/web/mvc-tutorial/app.js
@@ -14,18 +14,15 @@ Ext.application({
         Ext.create('Ext.container.Viewport', {
             layout: 'fit',
             items: [
-                {
-                    xtype: 'userlist'
-                }
 // Wrapping in a panel causes the scroll bars to stop working.
 // Sencha are aware I think... http://www.sencha.com/forum/showthread.php?142473
 //                {
 //                    xtype: 'panel',
 //                    layout: 'fit',
 //                    items: [
-//                        {
-//                            xtype: 'userlist'
-//                        }
+                        {
+                            xtype: 'userlist'
+                        }
 //                    ]
 //                }
             ]
