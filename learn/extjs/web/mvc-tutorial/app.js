a21b0c2b94af65bb6618c938eb05ffd704322b6f
pwl <1179301+p33t@users.noreply.github.com>
2011-12-09 00:59:54 +0000

    [learn-extjs] Application Bootstrapping: Scroll bug.

--- learn/extjs/web/mvc-tutorial/app.js
+++ learn/extjs/web/mvc-tutorial/app.js
@@ -17,6 +17,17 @@ Ext.application({
                 {
                     xtype: 'userlist'
                 }
+// Wrapping in a panel causes the scroll bars to stop working.
+// Sencha are aware I think... http://www.sencha.com/forum/showthread.php?142473
+//                {
+//                    xtype: 'panel',
+//                    layout: 'fit',
+//                    items: [
+//                        {
+//                            xtype: 'userlist'
+//                        }
+//                    ]
+//                }
             ]
         });
     }
