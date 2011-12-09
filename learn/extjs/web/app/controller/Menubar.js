f6ab74c26bb587e9eb6652d37518e006ff893da7
pwl <1179301+p33t@users.noreply.github.com>
2011-12-09 05:59:44 +0000

    [learn-extjs] Get xtype: 'appmenubar' working.

---
+++ learn/extjs/web/app/controller/Menubar.js
@@ -0,0 +1,5 @@
+Ext.define('LE.controller.Menubar', {
+    extend: 'Ext.app.Controller',
+    // NOTE: We need a controller make sure the view id loaded and the xtype works
+    views: [ 'Menubar']
+});
