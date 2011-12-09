fd0fd7d593bc05c3844b426bec4ea1525c1e35bd
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-12-09 05:34:04 +0000

    [learn-extjs] Introduce LE.view.Menubar.

--- learn/extjs/web/app.js
+++ learn/extjs/web/app.js
@@ -5,136 +5,30 @@ Ext.require('Ext.panel.Panel');
 Ext.require('Ext.app.Application');
 
 function createApp(pathToRoot, controllers, centerConfig) {
-    var viewportConfig = {
+    var result = Ext.create('Ext.app.Application', {
+        name: 'LE',
+        appFolder: pathToRoot + '/app' ,
+        controllers: controllers
+//        ,
+//        views: ['LE.view.Menubar']
+    });
+    // TODO: mymodule/good not working.  xtype not recognised.
+    console.log("Created app: ", result);
+    Ext.create('Ext.container.Viewport', {
         layout: 'border',
         items:[
             {
                 region: 'north',
-                items: menutoolbar(pathToRoot),
+                items: Ext.create('LE.view.Menubar',{
+                    pathToRoot: pathToRoot
+                }),
                 autoHeight: true
             },
-            Ext.merge({ region: 'center' }, centerConfig)
+            Ext.apply(centerConfig, { region: 'center' })
         ]
-    };
-    return Ext.create('Ext.app.Application', {
-        name: 'LE',
-        appFolder: pathToRoot + '/app',
-        controllers: controllers,
-        launch: function() {
-            Ext.create('Ext.container.Viewport', viewportConfig);
-        }
     });
-}
 
-function menutoolbar(pathToRoot) {
-    var pathPrefix = pathToRoot + '/';
-    return Ext.create('Ext.toolbar.Toolbar',
-        {
-            style: {
-                background: 'inherit',
-                border: 0
-            },
-            items:[
-                {xtype:'image', src: pathPrefix + 'image/logo-med.jpg', height: 50, width: 80},
-                {xtype: 'tbfill'},
-                {
-                    text:'Experiments',
-                    menu:{
-                        plain: true,
-                        items: [
-                            {
-                                text: 'Menubar',
-                                href: pathPrefix + 'experiment/menubar.html'
-                            },
-                            {
-                                text: 'Whole Page',
-                                href: pathPrefix + 'experiment/wholepage'
-                            },
-                            {
-                                text: 'HTML Inside',
-                                href: pathPrefix + 'experiment/htmlinside'
-                            },
-                            {
-                                text: 'Modules',
-                                menu: {
-                                    plain: true,
-                                    items: [
-                                        {
-                                            text: 'Good',
-                                            href: pathPrefix + 'experiment/mymodule/good.html'
-                                        },
-                                        {
-                                            text: 'Bad',
-                                            href: pathPrefix + 'experiment/mymodule/bad.html'
-                                        }
-                                    ]
-                                }
-                            }
-                        ]
-                    }
-                },
-                {
-                    xtype: 'button',
-                    text: 'MVC Tutorial',
-                    handler: function() {
-                        window.location = pathPrefix + 'mvc-tutorial';
-                    }
-                },
-                {
-                    xtype: 'button',
-                    text: 'Debug',
-                    handler: function() {
-                        alert('None');
-                    }
-                },
-                {
-                    xtype: 'splitbutton',
-                    text:'Ajax Loader',
-                    handler: function(btn) {
-                        var unchecked = btn.menu.query('menucheckitem[checked=false]')[0];
-                        // NOTE: A little dodgy because we're invoking other parts of the UI (?)
-                        unchecked.setChecked(true);
-                    },
-                    menu:{
-                        plain: true,
-                        items: [
-                            // stick any markup in a menu
-                            '<b class="menu-title">Visible</b>',
-                            {
-                                text: 'Yes',
-                                checked: false,
-                                group: 'ajaxLoaderVisible',
-                                checkHandler: function(item, checked) {
-                                    if (checked) ajaxLoader().show();
-                                }
-                            }, {
-                                text: 'No',
-                                checked: true,
-                                group: 'ajaxLoaderVisible',
-                                checkHandler: function(item, checked) {
-                                    if (checked) ajaxLoader().hide();
-                                }
-                            }
-                        ]
-                    }
-                },
-                {xtype: 'tbfill'},
-                {
-                    xtype: 'container',
-                    height: 50,
-                    width: 80,
-                    items:[
-                        {
-                            xtype: 'image',
-                            id: 'ajax-loader',
-                            src: pathPrefix + 'image/ajax-loader.gif',
-                            hidden: true
-                        }
-                    ]
-                }
-            ]
-        }
-    );
+    return result;
 }
 
 function ajaxLoader() {
