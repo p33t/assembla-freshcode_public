010ee03949bd053c56138d525768e95efcf69060
pwl <1179301+p33t@users.noreply.github.com>
2011-12-19 01:49:42 +0000

    [learn-scala] Java bug.

--- learn_scala/syntax/src/pkg/NegativeZero.java
+++ learn_scala/syntax/src/pkg/NegativeZero.java
@@ -3,6 +3,7 @@ package pkg;
 public class NegativeZero {
     public static void main(String[] args) {
         // looks like a bug to me
+        // long standing... http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6603276
         System.out.println(0.0 == -0.0); // prints 'true'
         System.out.println(new Double(0.0).compareTo(-0.0)); // prints '1'
     }
