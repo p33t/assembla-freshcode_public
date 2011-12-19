fd7ae85da5958feb8efb1fd8c4aaced5bd19e906
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2011-12-19 01:15:19 +0000

    [learn-scala] Refactor.

--- learn_scala/syntax/src/pkg/StringFormatting.scala
+++ learn_scala/syntax/src/pkg/StringFormatting.scala
@@ -4,12 +4,18 @@ package pkg
 object StringFormatting {
   def main(args: Array[String]) {
     // Hmmm.... scientific vs decimal is vague: See java.util.Formatter doco
-    println("A Double rendered to 17 digit precision in scientific or decimal format (depending): " + "%1$.17g".format(100.00000000000001))
-    println("A Double rendered to 17 digit precision in scientific or decimal format (depending): " + "%1$.17g".format(Double.MinValue))
-    println("A Double rendered to 17 digit precision in scientific or decimal format (depending): " + "%1$.17g".format(Double.MaxValue))
-    println("A Double rendered to hexadecimal floating point: " + "%1$a".format(1.000000001))
-    println("A Double rendered to hexadecimal floating point: " + "%1$a".format(Double.MinValue))
-    println("A Double rendered to hexadecimal floating point: " + "%1$a".format(Double.MaxValue))
+    val decSci = "%1$.17g"
+    println("\nDecimal / Scientific: " + decSci)
+    println(decSci.format(100.00000000000001))
+    println(decSci.format(Double.MaxValue))
+    println(decSci.format(Double.MinPositiveValue))
+    println(decSci.format(Double.MinPositiveValue * 2))
 
+    val hex = "%1$a"
+    println("\nHexadecimal: " + hex)
+    println(hex.format(1.000000001))
+    println(hex.format(Double.MaxValue))
+    println(hex.format(Double.MinPositiveValue))
+    println(hex.format(Double.MinPositiveValue * 2))
   }
 }
