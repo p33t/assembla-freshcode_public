373a373cf596db0538827a6b25d937ae8c975c21
pwl <1179301+p33t@users.noreply.github.com>
2011-12-21 06:09:56 +0000

    [scala-datastore] Implementing ScriptUtil.astToJs().

--- learn_scala/datastore/src/test/scala/pkg/script/AstToJsTest.scala
+++ learn_scala/datastore/src/test/scala/pkg/script/AstToJsTest.scala
@@ -4,27 +4,52 @@ import org.scalatest.Suite
 import net.liftweb.json.JsonAST._
 import ScriptingUtil._
 import util.Random
+import net.liftweb.json.JsonParser
 
 class AstToJsTest extends Suite {
   val js = newEngine()
   val scope = obtainScope(js)
 
   def testSimple() {
-    check(JString("String" + Random.nextInt()))
-    check(JInt(Random.nextInt()))
-    check(JDouble(Random.nextDouble()))
-    check(JBool(Random.nextBoolean()))
-    check(JNull) // not sure about this one
-    check(JArray(List(JString("hello"), JInt(99))))
-    check(JObject(List(JField("one", JInt(1)), JField("two", JString("due")))))
+    checkSymmetry(JString("String" + Random.nextInt()))
+    checkSymmetry(JInt(Random.nextInt()))
+    checkSymmetry(JDouble(Random.nextDouble()))
+    checkSymmetry(JBool(Random.nextBoolean()))
+    checkSymmetry(JNull) // not sure about this one
+    checkSymmetry(JArray(List(JString("hello"), JInt(99))))
+    checkSymmetry(JObject(List(JField("one", JInt(1)), JField("two", JString("due")))))
   }
 
-  private def check(jv: JValue) {
+  def testComplex() {
+    val json = JsonParser.parse("""[
+      {"bruce": "lee", "springsteen": 99},
+      99,
+      [
+        "hi how are you",
+        3.14
+      ]
+    ]""")
+    checkSymmetry(json)
+    check(json, "jv[2][0]", "hi how are you")
+    check(json, "jv[0].springsteen", 99)
+  }
+
+  private def eval(jv: JValue, expr: String): AnyRef = {
+    val binds = js.createBindings()
+    js.getContext
+    binds.put("jv", astToJs(jv, scope))
+    js.eval( expr + ";", binds)
+  }
+
+  private def check(jv: JValue, expr: String, expected: Any) {
+    expect(expected) {
+      eval(jv, expr)
+    }
+  }
+
+  private def checkSymmetry(jv: JValue) {
     expect(jv) {
-      val binds = js.createBindings()
-      js.getContext
-      binds.put("myVar", astToJs(jv, scope))
-      val output = js.eval("myVar;", binds)
+      val output: AnyRef = eval(jv, "jv")
       jsToAst(output)
     }
   }
