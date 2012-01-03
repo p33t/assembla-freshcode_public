a8ae0a29e7fe0dfb95351a8e134571c2fdb763f1
pwl <1179301+p33t@users.noreply.github.com>
2012-01-03 23:42:58 +0000

    [learn-lift] Creating a simple Restful service.

--- learn_scala/lift/src/main/scala/code/lib/SimpleRest.scala
+++ learn_scala/lift/src/main/scala/code/lib/SimpleRest.scala
@@ -2,7 +2,7 @@ package code.lib
 
 import net.liftweb._
 import http.rest.RestHelper
-import http.{BadResponse, NotFoundResponse, OkResponse}
+import http.{LiftResponse, BadResponse, NotFoundResponse, OkResponse}
 import json._
 import common._
 import util._
@@ -18,6 +18,7 @@ object SimpleRest extends RestHelper {
 
   // NOTE: Horribly non-thread safe
   var prevId = 1
+
   def nextId() = {
     prevId = prevId + 1
     prevId
@@ -28,38 +29,48 @@ object SimpleRest extends RestHelper {
     case Nil JsonGet _ => decompose(elems.values)
 
     // /api/item/item_id gets the specified item (or a 404)
-    case AsInt(id) :: Nil JsonGet _ => {
-      val opt = elems.get(id)
-      if (opt.isDefined) decompose(opt.get)
-      else NotFoundResponse("Unable to locate id " + id)
-    }
+    case AsInt(id) :: Nil JsonGet _ => retrieveAndProcess(id, decompose(_))
 
     // DELETE the item in question
     case AsInt(id) :: Nil JsonDelete _ =>
-      // TODO: Remove dupe with id lookup (?!)
-      val opt = elems.get(id)
-      if (opt.isDefined) {
-        elems = elems - id
-        decompose(opt.get)
-      }
-      else NotFoundResponse("Unable to locate id " + id)
+      retrieveAndProcess(id, {
+        elem =>
+          elems = elems - id
+          decompose(elem)
+      })
 
     // PUT adds the item if the JSON is parsable
-    case Nil JsonPut jv -> _ => {
-      // ID is supplied by server
-      val json = mergeJson(JObject(List(JField("id", JInt(nextId())))), jv)
+    case Nil JsonPut jv -> _ =>
+      // ID is supplied by server and need to happen before extract in case it is not supplied
+      val json = mergeJson(jv, JObject(List(JField("id", JInt(nextId())))))
       val opt = extractOpt[RestElem](json)
       opt match {
         case None =>
           BadResponse()
         case Some(elem) =>
-          val t2 = elem.id -> elem
-          elems = elems + t2
+          // TODO: What it it exists already?
+          putElem(elem)
           decompose(elem)
       }
-    }
 
-    // TODO: Post for updates
+
+    // POST if we find the item, merge the fields from the
+    // the POST body and update the item
+    case AsInt(id) :: Nil JsonPost jv -> _ =>
+      retrieveAndProcess(id, {
+        elem =>
+        val orig = decompose(elem)
+        val newJv = mergeJson(orig, jv)
+        val opt = extractOpt[RestElem](newJv)
+        opt match {
+          case Some(newElem) =>
+            val correctId = newElem.copy(id = id) // make sure ID has not changed
+            putElem(correctId)
+            OkResponse()
+          case None =>
+            BadResponse()
+        }
+      })
 
     /*
     // /api/item returns all the items
@@ -75,19 +86,25 @@ object SimpleRest extends RestHelper {
             item <- Item.search(searchString)
           } yield item).distinct: JValue
     ​    ​
-
-        // POST if we find the item, merge the fields from the
-        // the POST body and update the item
-        case Item(item) :: Nil JsonPost json -> _ =>
-          Item(mergeJson(item, json)).map(Item.add(_): JValue)
-
     */
   })
 
+  private def putElem(elem: SimpleRest.RestElem) {
+    val t2 = elem.id -> elem
+    elems = elems + t2
+  }
+
+  private def retrieveAndProcess(id: Int, fn: RestElem => LiftResponse): LiftResponse = {
+    val opt = elems.get(id)
+    if (opt.isDefined) fn(opt.get)
+    else NotFoundResponse("Unable to locate id " + id)
+  }
+
   case class RestElem(id: Int, num: Int, str: String)
-//  object RestElem {
-//    val Blank = RestElem(0, 0, "")
-//    val BlankJson = decompose(Blank)
-//  }
+
+  //  object RestElem {
+  //    val Blank = RestElem(0, 0, "")
+  //    val BlankJson = decompose(Blank)
+  //  }
 
 }
