69c8815e970580a932b77d32249cabd40365d92d
pwl <1179301+p33t@users.noreply.github.com>
2012-01-05 06:26:33 +0000

    [learn-lift] Refactor.

---
+++ learn_scala/lift/src/test/javascript/crud/NumStrMapTest.js
@@ -0,0 +1,23 @@
+
+function numStrMap() {
+    var EI = testApp();
+    log("Created EI ", EI);
+    return Ext.create('EI.store.NumStrMap');
+}
+
+describe("Crud infrastructure", function() {
+    it("can create a store with jsonp proxy", function() {
+        var store = numStrMap();
+        expect(store).toBeDefined();
+    });
+    it("can load at least one record that has a num and a str", function() {
+        var store = numStrMap();
+        store.load(function(records, operation, success) {
+            expect(store.getTotalCount()).toBeGreaterThan(0);
+            var first = records[0];
+            expect(first.get('num')).toBeDefined();
+            expect(first.get('str')).toBeDefined();
+        });
+    });
+});
+
