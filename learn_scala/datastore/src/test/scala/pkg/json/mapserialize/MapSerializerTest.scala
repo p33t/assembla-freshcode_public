af1ba7cd12a1b71f100e3f6302dd027cbdf621ad
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-01-09 04:46:36 +0000

    [scala-datastore] Map serialization.

---
+++ learn_scala/datastore/src/test/scala/pkg/json/mapserialize/MapSerializerTest.scala
@@ -0,0 +1,44 @@
+package pkg.json.mapserialize
+
+import org.scalatest.Suite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import net.liftweb.json._
+
+@RunWith(classOf[JUnitRunner])
+class MapSerializerTest extends Suite {
+  implicit val formats = DefaultFormats // ++ List(new MapSerializer[Int])
+
+  def testDefaultMapNowPreservesInsertOrder() {
+    val tuples = List("one" -> 1, "two" -> 2, "four" -> 4, "three" -> 3)
+    expect(tuples) {
+      Map(tuples: _*).toList
+    }
+  }
+
+  /**
+   * Looks like json parsing retains ordering of fields.  No need for fancy stuff!
+   */
+  def testJsonOrderRetained() {
+    val json = JsonParser.parse("""{"one": 1, "two": 2, "three": 3, "four": 4}""")
+    expect(json) {
+      val m = Extraction.extract[Map[String, Int]](json)
+      Extraction.decompose(m)
+    }
+  }
+
+}
+
+//object MapSerializerTest {
+//
+//  class MapSerializer[T <: Any : Manifest] extends Serializer[Map[String, T]] {
+//    def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), Map[String,T]] = {
+//      Map.empty
+//    }
+//
+//    def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
+//      Map.empty
+//    }
+//  }
+//
+//}
