96bb496c5c6af87aa4ea0050a58bf33d627867a2
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-01-12 04:35:02 +0000

    [learn-lift] More elaborate form usage.

--- learn_scala/lift/src/main/scala/code/snippet/form/FormLiftScreen.scala
+++ learn_scala/lift/src/main/scala/code/snippet/form/FormLiftScreen.scala
@@ -7,16 +7,38 @@ import net.liftweb.util.FieldError
 import Helpers._
 
 object FormLiftScreen extends LiftScreen {
+  // TODO: Don't know how to do a ScreenVar field. (?)
+  val key = field("Key", "")
   // Need only declare the field and any restrictions
   val message = field("Message", "", validateMessage _)
 
   def finish() {
-    S.notice("Message is: " + message.is)
+    S.notice("Message is: " + message.is + ", key:" + key.is)
+  }
+
+  override protected def localSetup() {
+    key.set(readKey())
+    super.localSetup()
   }
 
   private def validateMessage(s: String): List[FieldError] = {
     var result = valMinLen(5, "Must be >= 5 chars")(s)
     if (asInt(s).isDefined) result = FieldError(message, "Message cannot be a number") :: result
     result
+  }
+
+  private def readKey() = {
+    println("Reading the key...")
+    val box = S.param("key")
+    if (box.isEmpty) {
+      println("no key found")
+      S.notice("Key has not been supplied.")
+      redirectTo("/form")
+      "no-key"
+    }
+    else {
+      println("key found: " + box.get)
+      box.get
+    }
   }
 }
