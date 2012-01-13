ac2060bf22089c5bf9bcc51697dda3a0361db069
pwl <1179301+p33t@users.noreply.github.com>
2012-01-13 06:32:14 +0000

    [scala-datastore] Comment regarding Serializer implementation.

--- learn_scala/datastore/src/test/scala/pkg/json/RestrictedSerializer.scala
+++ learn_scala/datastore/src/test/scala/pkg/json/RestrictedSerializer.scala
@@ -6,6 +6,7 @@ object RestrictedSerializer extends Serializer[Restricted] {
   private val StrRep = "VoidChild"
 
   def deserialize(implicit format: Formats): PartialFunction[(TypeInfo, JValue), Restricted] = {
+    // NOTE: For generified types the ti.parameterizedType must be examined too.
     case (ti, jval) if (ti.clazz == classOf[Restricted] && jval == JString(StrRep)) => new VoidChild
     case (ti, jval) if (ti.clazz == classOf[Restricted]) => jval.extract[StringChild]
   }
