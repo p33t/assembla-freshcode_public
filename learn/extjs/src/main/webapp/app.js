a2962525a31ca9e90563570fcbfd1e671345b818
pwl <1179301+p33t@users.noreply.github.com>
2012-01-17 00:49:14 +0000

    [learn-extjs] Primitive Html render tool.

--- learn/extjs/src/main/webapp/app.js
+++ learn/extjs/src/main/webapp/app.js
@@ -53,3 +53,40 @@ function ajaxLoader() {
     var arr = Ext.ComponentQuery.query('#ajax-loader');
     return arr[0];
 }
+
+var Html = {
+    attrib: function(name, value) {
+        return name + '="' + value + '"';
+    },
+    attribs: function(obj) {
+        var accum = "";
+        for (var prop in obj) {
+            if (accum !== "") accum += " ";
+            accum = accum + this.attrib(prop, obj[prop])
+        }
+        return accum;
+    },
+    /**
+     * Render a string element.
+     * @param name The name of the element.
+     * @param attrs Optional json object describing the desired attributes.
+     * @param content Optional string which will be the content.
+     */
+    elem: function(name, attrs, content) {
+        if (Ext.isDefined(attrs) && !Ext.isDefined(content)) {
+            // only one optional arg supplied
+            if (!Ext.isObject(attrs)) {
+                // need to shuffle
+                content = attrs;
+                attrs = undefined;
+            }
+        }
+
+
+        var attrString = "";
+        if (Ext.isDefined(attrs)) attrString = " " + this.attribs(attrs);
+        var lead = '<' + name + attrString;
+        if (Ext.isDefined(content)) return lead + '>' + content + '</' + name + '>';
+        else return lead + "/>";
+    }
+};
