210fbfa8096b5802062ae57a060c98191695e62a
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-01-18 03:37:45 +0000

    [learn-extjs] RichSettings - Back to use of 'config' field.

--- learn/extjs/src/main/webapp/app/view/richsettings/Panel.js
+++ learn/extjs/src/main/webapp/app/view/richsettings/Panel.js
@@ -1,57 +1,42 @@
 Ext.define("LE.view.richsettings.Panel", {
     extend: "Ext.container.Container",
     alias: 'widget.richPanel',
-    // put field directly among defaults and hope it does not collide
-    paragraphs: ['&lt;none&gt;'],
 
-    // the less component-y, more object-y way
-//    config: {
-//        settings: []
-//    },
+    config: {
+        paragraphs: []
+    },
 
-    // NOTE: No compatibility of constructor with 'config' fields
-    // Also, we can't just plonk fields in the base of the class in case they collide with
-    // parent configuration.
-    useless____constructor: function(settings) {
-        log("constructor with config", settings);
-//        this.config.settings = settings;
-        this.setSettings = settings;
-
-        // NOTE: No args conveyed.
-        return this.callParent();
-    }
-    ,
+    constructor: function(config) {
+        log("constructor with config", config);
+//        TODO: Validate?
+        return this.callParent(arguments);
+    },
     initComponent: function() {
         log('initComponent');
         var result = this.callParent(arguments);
         log('initComponent callParent result:', result);
-        this.syncDisplay(this.paragraphs);
+        this.syncDisplay(this.getParagraphs());
         return result;
     },
-    getParagraphs: function() {
-        return this.paragraphs;
-    },
-    setParagraphs: function(paras) {
+    // No good...Uncaught TypeError: Cannot call method 'apply' of undefined
+//    setParagraphs: function(paras) {
+//        this.callParent(arguments);
+//        this.syncDisplay();
+//    },
+    applyParagraphs: function(paras) {
         if (!Ext.isArray(paras)) {
             log('ERROR: Rejected paragraph change because it is not an array:"',
                 paras,
                 '".  Ext.getDisplayName(arguments.callee):',
                 Ext.getDisplayName(arguments.callee));
+            return undefined;
         }
-        this.paragraphs = paras;
+        // This is nasty...would be nice to respond to notification of change to a property.
         this.syncDisplay(paras);
+        return paras;
     },
-    // This was the .config{} way.
-//    applySettings: function(settings) {
-//        log("Applying settings: ", settings);
-//        if (!Ext.isArray(settings)) return undefined;
-////        This is ugly.  Need to cascade the change but it is not committed yet.
-//        this.syncDisplay(settings);
-//        return settings;
-//    },
-    syncDisplay: function() {
-//        var settings = this.getSettings();
-        var paras = this.paragraphs;
+    syncDisplay: function(paras) {
+//        var paras = this.getParagraphs();
         log('Syncing with ', paras);
         var html = "";
         for (var ix in paras) {
