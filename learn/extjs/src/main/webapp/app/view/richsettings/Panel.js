8dd4f7731d57d0601e1272bd15f568a495fec08c
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-01-18 04:03:15 +0000

    [learn-extjs] RichSettings - Back to use of 'config' field.

--- learn/extjs/src/main/webapp/app/view/richsettings/Panel.js
+++ learn/extjs/src/main/webapp/app/view/richsettings/Panel.js
@@ -1,20 +1,25 @@
 Ext.define("LE.view.richsettings.Panel", {
-    extend: "Ext.container.Container",
+    extend: "Ext.panel.Panel",
     alias: 'widget.richPanel',
-
+    border: 1,
     config: {
-        paragraphs: []
+        paragraphs: ['default-paragraphs']
     },
 
     constructor: function(config) {
         log("constructor with config", config);
-//        TODO: Validate?
+        log("this.config", this.config);
+//        if (!this.validate(config.paragraphs)) {
+//            restore defaults (?)
+//            config.paragraphs = this.config.paragraphs;
+//        }
         return this.callParent(arguments);
     },
     initComponent: function() {
         log('initComponent');
         var result = this.callParent(arguments);
         log('initComponent callParent result:', result);
+        if (!this.validate(this.getParagraphs())) this.setParagraphs(this.config.paragraphs);
         this.syncDisplay(this.getParagraphs());
         return result;
     },
@@ -24,16 +29,20 @@ Ext.define("LE.view.richsettings.Panel", {
 //        this.syncDisplay();
 //    },
     applyParagraphs: function(paras) {
+        if (!this.validate(paras)) return undefined;
+        // This is nasty...would be nice to respond to notification of change to a property.
+        this.syncDisplay(paras);
+        return paras;
+    },
+    validate: function(paras) {
         if (!Ext.isArray(paras)) {
             log('ERROR: Rejected paragraph change because it is not an array:"',
                 paras,
                 '".  Ext.getDisplayName(arguments.callee):',
                 Ext.getDisplayName(arguments.callee));
-            return undefined;
+            return false;
         }
-        // This is nasty...would be nice to respond to notification of change to a property.
-        this.syncDisplay(paras);
-        return paras;
+        return true;
     },
     syncDisplay: function(paras) {
 //        var paras = this.getParagraphs();
