5a409ed5e2fcfb13fe385f36409f23614ceec6d5
pwl <1179301+p33t@users.noreply.github.com>
2012-01-19 07:48:41 +0000

    [learn-extjs] hasMany attempt.

--- learn/extjs/src/test/javascript/hierarchy/HierarchyUtil.js
+++ learn/extjs/src/test/javascript/hierarchy/HierarchyUtil.js
@@ -1,7 +1,22 @@
 var Hierarchy = {
+    boot: function() {
+        testApp();
+        Ext.define('Child', {
+            extend: 'Ext.data.Model',
+            idgen: 'sequential',
+            fields: ['name', 'parent_id']
+//    belongsTo: 'hierarchy.Parent'
+        });
+
+        Ext.define('Parent', {
+            extend: 'Ext.data.Model',
+            idgen: 'sequential',
+            fields: ['name']
+            // enabling this line makes everything barf
+            //hasMany: {model: 'Child', name: 'children'}
+        });
+    },
     parent: function () {
-        return Ext.create('LE.model.hierarchy.Parent');
+        return Ext.create('Parent');
     }
 };
-
-
