0e3e522c910ab8873e148b43e0b1a59ed7842fbf
pwl <1179301+p33t@users.noreply.github.com>
2012-01-24 07:22:45 +0000

    [learn-extjs] Window Form: Put item selector on a form.

--- learn/extjs/src/main/webapp/app/view/windowform/Window.js
+++ learn/extjs/src/main/webapp/app/view/windowform/Window.js
@@ -1,25 +1,82 @@
 Ext.define('LE.view.windowform.Window', {
     extend: 'Ext.window.Window',
+    requires: ['Ext.ux.form.ItemSelector'],
     alias: 'widget.wfWindow',
     closeAction: 'hide',
     title: 'Hello',
     height: 200,
     width: 400,
     layout: 'fit',
-    tbar: [
-        '->',
-        {text: 'All'},
-        {text: 'None'},
-        '-',
-        {iconCls: 'col-move-bottom'},
-        {iconCls: 'col-move-top'}
-    ],
-    items: {  // Let's put an empty grid in just to illustrate fit layout
-        xtype: 'grid',
-        border: false,
-        columns: [
-            {header: 'World'}
-        ],                 // One header just for show. There's no data,
-        store: Ext.create('Ext.data.ArrayStore', {}) // A dummy empty data store
+    onRender: function() {
+        this.callParent(arguments);
+        log('Rendered wfWindow:', this);
+        log('Callback is', this.getCallback());
+    },
+    items: {
+        xtype: 'form',
+        items: {
+            xtype: 'itemselector',
+            name: 'itemselector',
+            id: 'itemselector-field',
+            anchor: '100%',
+            fieldLabel: 'Selection',
+            store: Ext.create('Ext.data.ArrayStore', {
+                data: [
+                    [123,'One Hundred Twenty Three'],
+                    [1, 'One'],
+                    [2, 'Two'],
+                    [3, 'Three'],
+                    [4, 'Four'],
+                    [5, 'Five'],
+                    [6, 'Six'],
+                    [7, 'Seven'],
+                    [8, 'Eight'],
+                    [9, 'Nine']
+                ],
+                fields: ['value','text'],
+                sortInfo: {
+                    field: 'value',
+                    direction: 'ASC'
+                }
+            }),
+            displayField: 'text',
+            valueField: 'value',
+            value: ['3', '4', '6'],
+            allowBlank: false,
+            msgTarget: 'side'
+        },
+        buttons: [
+            {
+                text: 'Reset',
+                handler: function() {
+                    this.up('form').getForm().reset();
+                }
+            },
+            {
+                text: 'Submit',
+                formBind: true, //only enabled once the form is valid
+                disabled: true,
+                handler: function() {
+                    var basicForm = this.up('form');
+                    var form = basicForm.getForm();
+                    if (form.isValid()) {
+                        var values = form.getFieldValues();
+                        log("Values: ", values);
+                        var win = basicForm.up('wfWindow');
+                        log("wfWindow", win);
+                        win.hide();
+                        var callback = win.getCallback();
+                        log("Callback: ", callback);
+                        // TODO: Figure out this.
+//                        callback(values);
+                    }
+                }
+            }
+        ]
+    },
+    config: {
+        callback: function(result) {
+            log('Result from Window Form: ', result);
+        }
     }
 });
