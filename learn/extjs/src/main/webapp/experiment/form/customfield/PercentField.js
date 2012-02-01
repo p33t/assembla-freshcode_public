a28ce64fe138dfbc5c6e64aa4bde203e705da108
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-02-01 01:50:09 +0000

    [learn-extjs] Custom field: Custom check in AltXxx.

---
+++ learn/extjs/src/main/webapp/experiment/form/customfield/PercentField.js
@@ -0,0 +1,17 @@
+// TODO: Slider stuff.
+/**
+ * A number field with a slider underneath for small adjustments.
+ * NOTE: vTypes don't seem to have a string<->altValue conversion like valueToRaw() and rawToValue() so can't use.
+ */
+Ext.define('PercentField', {
+    extend: 'Ext.form.field.Text',
+    alias: 'widget.percentfield',
+    regex: Percent.REG_EXP,
+    regexText: 'Must be between 0% and 100% inclusive, no decimals, "%" optional.',
+    rawToValue: Percent.fromText,
+    valueToRaw: Percent.toText,
+    onBlur: function() {
+        // trigger reformat... is there a nicer way?
+        if (this.isValid()) this.setValue(this.getValue());
+    }
+});
