8b4915e84337a1f4739d9567b3b57120aee6e686
pwl <1179301+p33t@users.noreply.github.com>
2012-02-02 01:01:31 +0000

    [learn-extjs] Custom fields: Composed controls.

--- learn/extjs/src/main/webapp/experiment/form/customfield/CoordField.js
+++ learn/extjs/src/main/webapp/experiment/form/customfield/CoordField.js
@@ -4,28 +4,51 @@
 Ext.define('CoordField', {
     extend: 'Ext.form.FieldContainer',
     alias: 'widget.coordfield',
-    mixins: ['Ext.form.field.Field'],
+    mixins: {field: 'Ext.form.field.Field'},
     layout: 'hbox',
     defaults: {
         // prevent participation if form values / submission.
         getModelData: function() {return null;},
         getSubmitData: function() {return null;},
-        hideLabel: true
+        hideLabel: true,
+        xtype: 'numberfield',
+        maxValue: 10,
+        minValue: -10,
+        allowBlank: false
     },
     items: [
         {
-            xtype: 'numberfield',
             value: 0
         },
         {
-            xtype: 'numberfield',
-            value: 0
+            value: 0 //'xxx'
         }
     ],
-    getValue: function() {
+    // TODO: Need to use this in an event handler or something.
+    readValue: function() {
         var nums = this.query('numberfield');
-        return Ext.Array.map(nums, function(num){
-            return num.getValue();
-        });
+        return [nums[0].getValue(), nums[1].getValue()];
+    },
+    writeValue: function(arr) {
+        var nums = this.query('numberfield');
+
+        function process(ix) {
+            var dimen = arr[ix];
+            if (!Ext.isNumber(dimen)) dimen = null;
+            nums[ix].setValue(dimen);
+        }
+
+        process(0);
+        process(1);
+    },
+    initComponent: function() {
+        this.callParent(arguments);
+        this.initField();
+    },
+    setValue: function(v) {
+        log('this', this);
+        var result = this.mixins.field.setValue.call(this, v);
+        this.writeValue(v);
+        return result;
     }
 });
