c9370b366f0fc07083dfe46959a035197ad29cf5
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-02-09 01:09:34 +0000

    [learn-extjs] Charts.

--- learn/extjs/src/test/javascript/DateTest.js
+++ learn/extjs/src/test/javascript/DateTest.js
@@ -1,16 +1,22 @@
-var Epoch = new Date(0);
+// NOTE: It is not possible to escape from the current time zone (?!)
+var Epoch_NOT = new Date(0);
 var Day = 24 * 60 * 60 * 1000;
 /**
  * This can be used for period description.  The ISO-8601 Weeknumber is in our favour.
  */
 var Day0 = new Date(-3 * Day);
+function fmt(date, pattern) {return Ext.Date.format(date, pattern);}
 describe('Date exploration', function() {
     it('epoch', function() {
-        expect(Ext.Date.format(Epoch, 'W')).toBe('01');
-        expect(Ext.Date.format(Epoch, 'D')).toBe('Thu');
+        expect(fmt(Epoch_NOT, 'W')).toBe('01');
+        expect(fmt(Epoch_NOT, 'D')).toBe('Thu');
+        //expect(fmt(Epoch_NOT, 'H')).toBe('00');
     });
     it('day0', function() {
-        expect(Ext.Date.format(Day0, 'W')).toBe('01');
-        expect(Ext.Date.format(Day0, 'D')).toBe('Mon');
+        expect(fmt(Day0, 'W')).toBe('01');
+        expect(fmt(Day0, 'D')).toBe('Mon');
+    });
+    itx('utc', function() {
+        expect(fmt(Epoch_NOT, 'Z')).toBe('0');
     });
 });
