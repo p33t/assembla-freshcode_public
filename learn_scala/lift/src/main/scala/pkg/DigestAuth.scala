86be0991cd5d8b1b42a4287caf43908436616ba2
pwl <1179301+p33t@users.noreply.github.com>
2012-02-20 07:03:58 +0000

    [scala-lift]: Refactor.

---
+++ learn_scala/lift/src/main/scala/pkg/DigestAuth.scala
@@ -0,0 +1,33 @@
+package pkg
+
+import net.liftweb.http.auth._
+import net.liftweb.sitemap.Loc.HttpAuthProtected
+import net.liftweb.common.{Loggable, Full, Empty}
+
+object DigestAuth extends Loggable {
+  private val DrNonPriv = AuthRole("non-privileged")
+  private val DrSuperUser = AuthRole("superuser", DrNonPriv)
+  // No specific roles required, only need to be logged in
+  val DigestAuthenticated = HttpAuthProtected(req => Empty)
+  val DigestSuperUser = HttpAuthProtected(req => Full(DrSuperUser))
+
+  val authentication = HttpDigestAuthentication("Password is 's3cret'") {
+      case (username, _, authenticates) => {
+        logger.info("Authenticating: " + username)
+        // NOTE: Need the password in plain text to use digest auth
+        if (authenticates("s3cret")) {
+          logger.info("Auth succeeded for " + username)
+          // set up roles for remainder of request
+          val role = {
+            if (username == "super") DrSuperUser
+            else DrNonPriv
+          }
+          userRoles(List(role))
+          true
+        } else {
+          logger.warn("Auth failed for " + username)
+          false
+        }
+      }
+    }
+}
