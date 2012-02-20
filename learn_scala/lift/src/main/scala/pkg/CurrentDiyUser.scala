713dcb7b99aa2b857bb276170940593489e97549
pwl <1179301+p33t@users.noreply.github.com>
2012-02-20 06:11:55 +0000

    [scala-lift]: Refactor as 'diy' in preparation for HttpAuth implementation of authentication.

--- learn_scala/lift/src/main/scala/pkg/CurrentDiyUser.scala
+++ learn_scala/lift/src/main/scala/pkg/CurrentDiyUser.scala
@@ -2,10 +2,10 @@ package pkg
 
 import net.liftweb.http.SessionVar
 
-object CurrentUser {
+object CurrentDiyUser {
   private object username extends SessionVar[Option[String]](None)
 
-  def isLoggedIn = username.is.isDefined
+  def isLoggedIn = username.is != None
 
   def login(uname: String) {
     username.set(Some(uname))
