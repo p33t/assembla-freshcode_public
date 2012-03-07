81e84bb8be1b8cd6f98fb3c7c2dccd63c333ffd3
pwl <1179301+p33t@users.noreply.github.com>
2012-03-07 23:18:36 +0000

    [learn-gwt] Start rework to create application with title bar.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/MainPanel.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/MainPanel.java
@@ -1,11 +1,17 @@
 package biz.freshcode.learn.gwt.client;
 
 import com.smartgwt.client.widgets.HTMLPane;
+import com.smartgwt.client.widgets.layout.VLayout;
+import com.smartgwt.client.widgets.menu.MenuBar;
 
-public class MainPanel extends HTMLPane {
+public class MainPanel extends VLayout {
     {
         // NOTE: Seems to cause scroll bar (container is 100% height)
 //        setShowEdges(true);
-        setContents("<p>This is the contents.  It carries on for a little bit but I don't know if it will get a scroll bar.</p>");
+
+//        MenuBar mb = new MenuBar();
+        HTMLPane html = new HTMLPane();
+        html.setContents("<p>This is the contents.  It carries on for a little bit but I don't know if it will get a scroll bar.</p>");
+        addMember(html);
     }
 }
