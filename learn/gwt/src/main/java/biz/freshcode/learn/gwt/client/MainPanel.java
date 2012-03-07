5f329c7c0c78984d2ae265fdb6df44d44772c782
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-03-07 23:57:31 +0000

    [learn-gwt] Menu bar and 'alert' menu item.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/MainPanel.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/MainPanel.java
@@ -1,17 +1,35 @@
 package biz.freshcode.learn.gwt.client;
 
+import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.HTMLPane;
 import com.smartgwt.client.widgets.layout.VLayout;
+import com.smartgwt.client.widgets.menu.Menu;
 import com.smartgwt.client.widgets.menu.MenuBar;
+import com.smartgwt.client.widgets.menu.MenuItem;
+import com.smartgwt.client.widgets.menu.events.ClickHandler;
+import com.smartgwt.client.widgets.menu.events.MenuItemClickEvent;
 
 public class MainPanel extends VLayout {
     {
         // NOTE: Seems to cause scroll bar (container is 100% height)
 //        setShowEdges(true);
 
-//        MenuBar mb = new MenuBar();
+        MenuBar mb = new MenuBar();
+        addMember(mb);
+        Menu m = new Menu();
+        m.setTitle("Experiments");
+        MenuItem alert = new MenuItem("Alert");
+        m.addItem(alert);
+        alert.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(MenuItemClickEvent menuItemClickEvent) {
+                SC.say("Self destruct sequence has initiated.");
+            }
+        });
+        mb.setMenus(m);
+
         HTMLPane html = new HTMLPane();
-        html.setContents("<p>This is the contents.  It carries on for a little bit but I don't know if it will get a scroll bar.</p>");
         addMember(html);
+        html.setContents("<p>This is the contents.  It carries on for a little bit but I don't know if it will get a scroll bar.</p>");
     }
 }
