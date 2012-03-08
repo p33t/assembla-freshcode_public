aa952b5c24ca0f3b990500beea21333c3b2798fe
pwl <1179301+p33t@users.noreply.github.com>
2012-03-08 05:17:30 +0000

    [learn-gwt] Remove SmartGWT.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/EntryPoint.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/EntryPoint.java
@@ -1,14 +1,11 @@
 package biz.freshcode.learn.gwt.client;
 
 import com.google.gwt.user.client.ui.RootPanel;
-import com.smartgwt.client.widgets.layout.VLayout;
 
 public class EntryPoint implements com.google.gwt.core.client.EntryPoint {
     @Override
     public void onModuleLoad() {
         MainPanel main = new MainPanel();
-        main.setHeight100();
-        main.setWidth100();
         RootPanel.get().add(main);
     }
 }
--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/MainPanel.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/MainPanel.java
@@ -1,35 +1,31 @@
 package biz.freshcode.learn.gwt.client;
 
-import com.smartgwt.client.util.SC;
-import com.smartgwt.client.widgets.HTMLPane;
-import com.smartgwt.client.widgets.layout.VLayout;
-import com.smartgwt.client.widgets.menu.Menu;
-import com.smartgwt.client.widgets.menu.MenuBar;
-import com.smartgwt.client.widgets.menu.MenuItem;
-import com.smartgwt.client.widgets.menu.events.ClickHandler;
-import com.smartgwt.client.widgets.menu.events.MenuItemClickEvent;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class MainPanel extends VLayout {
+public class MainPanel extends Composite {
     {
         // NOTE: Seems to cause scroll bar (container is 100% height)
 //        setShowEdges(true);
 
-        MenuBar mb = new MenuBar();
-        addMember(mb);
-        Menu m = new Menu();
-        m.setTitle("Experiments");
-        MenuItem alert = new MenuItem("Alert");
-        m.addItem(alert);
-        alert.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(MenuItemClickEvent menuItemClickEvent) {
-                SC.say("Self destruct sequence has initiated.");
-            }
-        });
-        mb.setMenus(m);
+//        MenuBar mb = new MenuBar();
+//        addMember(mb);
+//        Menu m = new Menu();
+//        m.setTitle("Experiments");
+//        MenuItem alert = new MenuItem("Alert");
+//        m.addItem(alert);
+//        alert.addClickHandler(new ClickHandler() {
+//            @Override
+//            public void onClick(MenuItemClickEvent menuItemClickEvent) {
+//                SC.say("Self destruct sequence has initiated.");
+//            }
+//        });
+//        mb.setMenus(m);
 
-        HTMLPane html = new HTMLPane();
-        addMember(html);
-        html.setContents("<p>This is the contents.  It carries on for a little bit but I don't know if it will get a scroll bar.</p>");
+        VerticalPanel vp = new VerticalPanel();
+        HTMLPanel html = new HTMLPanel("<p>This is the contents.  It carries on for a little bit but I don't know if it will get a scroll bar.</p>");
+        vp.add(html);
+        initWidget(vp);
     }
 }
