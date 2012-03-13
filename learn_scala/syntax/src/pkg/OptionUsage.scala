681b2bafbe6d1ee5d33e4b55c4e27b8f170fffad
pwl <1179301+p33t@users.noreply.github.com>
2012-03-13 06:25:18 +0000

    [learn-scala] javainterop

---
+++ learn_scala/syntax/src/pkg/OptionUsage.scala
@@ -0,0 +1,13 @@
+package pkg
+
+
+object OptionUsage {
+  def main(args: Array[String]) {
+
+  }
+
+  // Trying to optionally add stuff to a list without using 'var'
+  def listConstruction() {
+
+  }
+}
