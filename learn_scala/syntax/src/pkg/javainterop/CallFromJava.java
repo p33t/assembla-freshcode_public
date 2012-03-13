e996c907aa5014f2a78af35d5dd91903e9fd1bae
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-03-13 06:33:55 +0000

    [learn-scala] javainterop

--- learn_scala/syntax/src/pkg/javainterop/CallFromJava.java
+++ learn_scala/syntax/src/pkg/javainterop/CallFromJava.java
@@ -1,8 +1,12 @@
 package pkg.javainterop;
 
 public class CallFromJava {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         Simple s = new Simple();
         System.out.println(s.hello("Bruce"));
+
+//Doesn't seem to be necessary...        Class.forName("pkg.javainterop.AnObject$");
+        int hundy = pkg.javainterop.AnObject$.MODULE$.plus1(99);
+        System.out.println("Want 100: " + hundy);
     }
 }
