c7190c46b303d2818d11f1d6d0daf29411fd67c0
pwl <1179301+p33t@users.noreply.github.com>
2012-03-15 05:40:18 +0000

    [gwtlift] Comment.

--- learn_scala/gwtlift/src/main/scala/pkg/xhr/StatelessDispatchTable.scala
+++ learn_scala/gwtlift/src/main/scala/pkg/xhr/StatelessDispatchTable.scala
@@ -7,10 +7,12 @@ import common._
 import util._
 import net.liftweb.json.JsonAST.{JInt, JArray}
 
-
+/**
+ * This is not used anywhere.  Seems better to use RPC.
+ */
 object StatelessDispatchTable extends RestHelper {
   serve(List("xhr") prefix {
+    // This works.  Navigate to ../xhr/smoke
     case List("smoke") JsonGet _ => JArray(List(JInt(1), JInt(2), JInt(3)))
   })
-
 }
