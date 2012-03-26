478301b9360fb830666e688ead1cb49b27d7c2ed
pwl <1179301+p33t@users.noreply.github.com>
2012-03-26 23:53:16 +0000

    [learn-gwt] UiBinder.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uibinder/Basic.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uibinder/Basic.java
@@ -2,9 +2,14 @@ package biz.freshcode.learn.gwt.client.uibinder;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.ResizeComposite;
 import com.google.gwt.user.client.ui.Widget;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 // NOTE: Using ResizeComposite here breaks the UI.
 // Something about wrapped widget not implementing RequiresResize (?)
@@ -13,8 +18,14 @@ public class Basic extends Composite {
     }
 
     private static Binder binder = GWT.create(Binder.class);
+    @UiField
+    ListBox listBox;
 
     {
+        // This must come first to bind fields to instance variables
         initWidget(binder.createAndBindUi(this));
+        for (String trans : Arrays.asList("Ferry", "Train", "Bus")) {
+            listBox.addItem(trans);
+        }
     }
 }
