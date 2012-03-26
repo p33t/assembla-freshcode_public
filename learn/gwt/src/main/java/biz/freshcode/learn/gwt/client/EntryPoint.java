e1b7072fd9afe9d99cd93b9bd01f03648e53ebc6
pwl <1179301+p33t@users.noreply.github.com>
2012-03-26 06:53:35 +0000

    [learn-gwt] XxxLayoutPanel rework (for standards mode).

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/EntryPoint.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/EntryPoint.java
@@ -1,14 +1,15 @@
 package biz.freshcode.learn.gwt.client;
 
 import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class EntryPoint implements com.google.gwt.core.client.EntryPoint {
     @Override
     public void onModuleLoad() {
         MainPanel main = new MainPanel();
-        // RootLayoutPanel causes an error here... not sure why.
-        RootPanel.get().add(main);
+        // RootLayoutPanel implements ProvidesResize
+        RootLayoutPanel.get().add(main);
         DOM.removeChild(RootPanel.getBodyElement(), DOM.getElementById("loading"));
     }
 }
