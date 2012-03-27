3cb7acd50d5d7d094b439582f51fd11e3a62ed84
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-03-27 02:18:23 +0000

    [learn-gwt] Logging.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uibinder/Basic.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uibinder/Basic.java
@@ -8,10 +8,13 @@ import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Widget;
 
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 // NOTE: Using ResizeComposite here breaks the UI.
 // Something about wrapped widget not implementing RequiresResize (?)
 public class Basic extends Composite {
+    Logger logger = Logger.getLogger(getClass().getName());
+
     interface Binder extends UiBinder<Widget, Basic> {
     }
 
@@ -22,8 +25,10 @@ public class Basic extends Composite {
     {
         // This must come first to bind fields to instance variables
         initWidget(binder.createAndBindUi(this));
+        logger.info("Initializing...");
         for (String trans : Arrays.asList("Ferry", "Train", "Bus")) {
             listBox.addItem(trans);
         }
+        logger.info("Finished Initializing");
     }
 }
