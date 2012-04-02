8a33ea43425a851b871cb6f765ac3e34a494520d
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-04-02 05:07:14 +0000

    [learn-gwt] Learning GXT.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/MainPanel.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/MainPanel.java
@@ -2,8 +2,11 @@ package biz.freshcode.learn.gwt.client.uispike;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer.BorderLayoutData;
 
 import java.util.logging.Logger;
 
@@ -14,8 +17,11 @@ public class MainPanel extends Composite {
     }
 
     private static Binder binder = GWT.create(Binder.class);
+    @UiField(provided = true)
+    BorderLayoutData westData;
 
     {
+        westData = new BorderLayoutData(100);
         initWidget(binder.createAndBindUi(this));
     }
 }
