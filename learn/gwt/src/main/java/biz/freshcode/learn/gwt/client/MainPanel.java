038fc26535456335ed43963b5e48afbef25e772c
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-04-03 07:06:44 +0000

    [learn-gwt] GXT learn.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/MainPanel.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/MainPanel.java
@@ -10,6 +10,7 @@ import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
+import com.sencha.gxt.widget.core.client.menu.Menu;
 
 public class MainPanel extends Composite {
     DockLayoutPanel pnl = new DockLayoutPanel(Style.Unit.EM);
@@ -21,17 +22,16 @@ public class MainPanel extends Composite {
 
         MenuBar mb = new MenuBar();
         pnl.addNorth(mb, 2);
+
+        mb.addItem("Experiments", experiments());
+        mb.addItem("GXT", mbGxt());
+
+        pnl.add(content);
+        initWidget(pnl);
+    }
+
+    private MenuBar experiments() {
         MenuBar exps = new MenuBar(true);
-        mb.addItem("Experiments", exps);
-
-        exps.addItem("UI Spike", new Command() {
-            public void execute() {
-                // Resize doesn't work unless it's GXT all the way (so far)
-                Widget w = GWT.create(biz.freshcode.learn.gwt.client.uispike.MainPanel.class);
-                replaceRoot(w);
-            }
-        });
-
         exps.addItem("Alert", new Command() {
             public void execute() {
                 Window.alert("Consider yourself alerted");
@@ -40,9 +40,47 @@ public class MainPanel extends Composite {
 
         MenuBar uiBinder = uiBinder();
         exps.addItem("Ui Binder", uiBinder);
+        return exps;
+    }
 
-        pnl.add(content);
-        initWidget(pnl);
+    private MenuBar mbGxt() {
+        MenuBar mbGxt = new MenuBar(true);
+
+        mbGxt.addItem("UI Spike", new Command() {
+            public void execute() {
+                Widget w = GWT.create(biz.freshcode.learn.gwt.client.uispike.MainPanel.class);
+                replaceRoot(w);
+            }
+        });
+
+        MenuBar uibGxt = uibGxt();
+        mbGxt.addItem("Ui Binder", uibGxt);
+        return mbGxt;
+    }
+
+    private MenuBar uibGxt() {
+        MenuBar mb = new MenuBar(true);
+        mb.addItem("Border Eg", new Command() {
+            public void execute() {
+                IsWidget w = GWT.create(BorderLayoutUiBinderExample.class);
+                replaceRoot(w.asWidget());
+            }
+        });
+
+        mb.addItem("Tutorial 1 (simple frame)", new Command() {
+            public void execute() {
+                IsWidget w = GWT.create(Tutorial1.class);
+                replaceRoot(w.asWidget());
+            }
+        });
+
+        mb.addItem("Tutorial 2 (menu + event)", new Command() {
+            public void execute() {
+                IsWidget w = GWT.create(Tutorial2.class);
+                replaceRoot(w.asWidget());
+            }
+        });
+        return mb;
     }
 
     private void replaceRoot(Widget w) {
@@ -64,27 +102,6 @@ public class MainPanel extends Composite {
             public void execute() {
                 Widget w = GWT.create(Composed.class);
                 replaceContent(w);
-            }
-        });
-
-        mb.addItem("Border Eg", new Command() {
-            public void execute() {
-                IsWidget w = GWT.create(BorderLayoutUiBinderExample.class);
-                replaceRoot(w.asWidget());
-            }
-        });
-
-        mb.addItem("Tutorial 1 (simple frame)", new Command() {
-            public void execute() {
-                IsWidget w = GWT.create(Tutorial1.class);
-                replaceRoot(w.asWidget());
-            }
-        });
-
-        mb.addItem("Tutorial 2 (menu + event)", new Command() {
-            public void execute() {
-                IsWidget w = GWT.create(Tutorial2.class);
-                replaceRoot(w.asWidget());
             }
         });
         return mb;
