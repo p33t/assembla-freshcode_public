be16b7a8c07e365d6c5ad6141c2ddb2a01f3041b
pwl <1179301+p33t@users.noreply.github.com>
2012-04-03 02:32:19 +0000

    [learn-gwt] Learning GXT: Dramas with border layout.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/MainPanel.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/MainPanel.java
@@ -2,6 +2,9 @@ package biz.freshcode.learn.gwt.client;
 
 import biz.freshcode.learn.gwt.client.uibinder.Basic;
 import biz.freshcode.learn.gwt.client.uibinder.Composed;
+import biz.freshcode.learn.gwt.client.uibinder.eg.BorderLayoutUiBinderExample;
+import biz.freshcode.learn.gwt.client.uibinder.eg.Tutorial1;
+import biz.freshcode.learn.gwt.client.uibinder.eg.Tutorial2;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.Command;
@@ -23,8 +26,9 @@ public class MainPanel extends Composite {
 
         exps.addItem("UI Spike", new Command() {
             public void execute() {
+                // Resize doesn't work unless it's GXT all the way (so far)
                 Widget w = GWT.create(biz.freshcode.learn.gwt.client.uispike.MainPanel.class);
-                replaceContent(w);
+                replaceRoot(w);
             }
         });
 
@@ -41,6 +45,12 @@ public class MainPanel extends Composite {
         initWidget(pnl);
     }
 
+    private void replaceRoot(Widget w) {
+        RootPanel root = RootPanel.get();
+        root.clear();
+        root.add(w);
+    }
+
     private MenuBar uiBinder() {
         MenuBar mb = new MenuBar(true);
         mb.addItem("Basic", new Command() {
@@ -54,6 +64,27 @@ public class MainPanel extends Composite {
             public void execute() {
                 Widget w = GWT.create(Composed.class);
                 replaceContent(w);
+            }
+        });
+
+        mb.addItem("Border Eg", new Command() {
+            public void execute() {
+                IsWidget w = GWT.create(BorderLayoutUiBinderExample.class);
+                replaceRoot(w.asWidget());
+            }
+        });
+
+        mb.addItem("Tutorial 1 (simple frame)", new Command() {
+            public void execute() {
+                IsWidget w = GWT.create(Tutorial1.class);
+                replaceRoot(w.asWidget());
+            }
+        });
+
+        mb.addItem("Tutorial 2 (menu + event)", new Command() {
+            public void execute() {
+                IsWidget w = GWT.create(Tutorial2.class);
+                replaceRoot(w.asWidget());
             }
         });
         return mb;
