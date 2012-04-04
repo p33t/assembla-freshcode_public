8e90cc104447e76b86ddeb941740045b488994d8
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-04-04 03:44:32 +0000

    [learn-gwt] UiSpike: Refactor.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/MainPanel.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/MainPanel.java
@@ -3,21 +3,23 @@ package biz.freshcode.learn.gwt.client.uispike;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.core.client.util.Margins;
+import com.sencha.gxt.widget.core.client.Component;
 import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer.BorderLayoutData;
 
 import java.util.logging.Logger;
 
-public class MainPanel extends Composite {
+public class MainPanel implements IsWidget {
     private static Binder binder = GWT.create(Binder.class);
     Logger logger = Logger.getLogger(getClass().getName());
 
-    {
-        initWidget(binder.createAndBindUi(this));
+    public Widget asWidget() {
+        return binder.createAndBindUi(this);
     }
 
-    interface Binder extends UiBinder<Widget, MainPanel> {
+    interface Binder extends UiBinder<Component, MainPanel> {
     }
 
     /**
