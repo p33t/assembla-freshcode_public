1840fb1053f0ada12172744c7734566d5aa00633
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-04-04 04:08:08 +0000

    [learn-gwt] UiSpike: Refactor.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/BeanBuilder.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/BeanBuilder.java
@@ -1,11 +1,14 @@
 package biz.freshcode.learn.gwt.client.uispike;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * A marker annotation that enables locating of bean builders (potentially for regeneration)
  */
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
 public @interface BeanBuilder {
     Class value();
