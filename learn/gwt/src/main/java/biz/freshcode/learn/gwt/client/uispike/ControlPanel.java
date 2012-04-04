73933e470fe0fe6247b4a1f2e6c751aaf2c9a12f
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-04-04 05:20:22 +0000

    [learn-gwt] UiSpike: Scrolling.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/ControlPanel.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/ControlPanel.java
@@ -2,19 +2,26 @@ package biz.freshcode.learn.gwt.client.uispike;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.core.client.dom.ScrollSupport;
 import com.sencha.gxt.widget.core.client.Component;
+import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 
 import java.util.logging.Logger;
 
 public class ControlPanel implements IsWidget {
     private static Binder binder = GWT.create(Binder.class);
     Logger logger = Logger.getLogger(getClass().getName());
+    @UiField
+    VerticalLayoutContainer north;
 
     public Widget asWidget() {
-        return binder.createAndBindUi(this);
+        Component c = binder.createAndBindUi(this);
+        north.setScrollMode(ScrollSupport.ScrollMode.AUTOY);
+        return c;
     }
 
     interface Binder extends UiBinder<Component, ControlPanel> {
