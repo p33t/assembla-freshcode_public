805eaec8f7134d59f6cfea57075d12d62854f0e9
pwl <1179301+p33t@users.noreply.github.com>
2012-04-05 06:36:59 +0000

    [learn-gwt] UiSpike: First Draft.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/LineItem.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/LineItem.java
@@ -3,6 +3,7 @@ package biz.freshcode.learn.gwt.client.uispike;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
@@ -10,28 +11,39 @@ import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.core.client.dom.ScrollSupport;
 import com.sencha.gxt.widget.core.client.Component;
 import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.container.SimpleContainer;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.info.Info;
 
 import java.util.ArrayList;
 
 public class LineItem implements IsWidget {
     private static final Binder binder = GWT.create(Binder.class);
-    @UiField(provided = true)
-    HTMLPanel pnlContent;
+    @UiField
+    SimpleContainer pnlContent;
     @UiField
     TextButton btnEdit;
     @UiField(provided = true)
     Label lblName;
+    private final Row row;
 
     public LineItem(Row row) {
-        // TODO: Replace with a template
-        pnlContent = new HTMLPanel("<p>Numer: " + row.getNum() + "</p>");
-        lblName = new Label(row.getStr());
+        this.row = row;
     }
 
     @Override
     public Widget asWidget() {
-        return binder.createAndBindUi(this);
+        lblName = new Label(row.getStr());
+        Component c = binder.createAndBindUi(this);
+        // TODO: Replace with a template
+        pnlContent.setWidget(new HTMLPanel("<p>Number: " + row.getNum() + "</p>"));
+        return c;
+    }
+
+    @UiHandler("btnEdit")
+    public void onEdit(SelectEvent evt) {
+        Info.display("Note", "Editing " + lblName.getText());
     }
 
     interface Binder extends UiBinder<Component, LineItem> {
