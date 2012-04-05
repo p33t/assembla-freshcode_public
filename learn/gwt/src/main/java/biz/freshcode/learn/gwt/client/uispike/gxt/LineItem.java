b65e12216e17913e9c9449cf8a0a9e2881c9d5ec
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-04-05 07:17:00 +0000

    [learn-gwt] UiSpike: XTemplate.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/gxt/LineItem.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/gxt/LineItem.java
@@ -1,6 +1,7 @@
 package biz.freshcode.learn.gwt.client.uispike.gxt;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -8,6 +9,7 @@ import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.core.client.XTemplates;
 import com.sencha.gxt.core.client.dom.ScrollSupport;
 import com.sencha.gxt.widget.core.client.Component;
 import com.sencha.gxt.widget.core.client.button.TextButton;
@@ -20,6 +22,8 @@ import java.util.ArrayList;
 
 public class LineItem implements IsWidget {
     private static final Binder binder = GWT.create(Binder.class);
+    private static final RowRenderer renderer = GWT.create(RowRenderer.class);
+
     @UiField(provided = true)
     HTMLPanel pnlContent;
     @UiField
@@ -35,8 +39,7 @@ public class LineItem implements IsWidget {
     @Override
     public Widget asWidget() {
         lblName = new Label(row.getStr());
-        // TODO: Replace with a template
-        pnlContent = new HTMLPanel("<p>Number: " + row.getNum() + "</p>");
+        pnlContent = new HTMLPanel(renderer.render(row));
         return binder.createAndBindUi(this);
     }
 
@@ -46,5 +49,10 @@ public class LineItem implements IsWidget {
     }
 
     interface Binder extends UiBinder<Component, LineItem> {
+    }
+
+    interface RowRenderer extends XTemplates {
+        @XTemplate(source = "row.html")
+        public SafeHtml render(Row row);
     }
 }
