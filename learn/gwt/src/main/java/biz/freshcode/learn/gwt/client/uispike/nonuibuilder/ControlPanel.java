e298be6f2ee13b974fcebff651ce84a54274a5c9
pwl <1179301+p33t@users.noreply.github.com>
2012-04-10 03:48:12 +0000

    [learn-gwt] UiSpike: Non-UiBuilder implementation.

---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/nonuibuilder/ControlPanel.java
@@ -0,0 +1,78 @@
+package biz.freshcode.learn.gwt.client.uispike.nonuibuilder;
+
+import biz.freshcode.learn.gwt.client.uispike.builder.BorderLayoutDataBuilder;
+import biz.freshcode.learn.gwt.client.uispike.gxt.Row;
+import com.google.gwt.user.client.Random;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.core.client.dom.ScrollSupport;
+import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.toolbar.FillToolItem;
+import com.sencha.gxt.widget.core.client.toolbar.SeparatorToolItem;
+import com.sencha.gxt.widget.core.client.toolbar.ToolBar;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public class ControlPanel implements IsWidget {
+    final Logger logger = Logger.getLogger(getClass().getName());
+    final Host host;
+
+    VerticalLayoutContainer lines = new VerticalLayoutContainer();
+
+    ArrayList<LineItem> items = new ArrayList<LineItem>();
+
+    public ControlPanel(Host host) {
+        this.host = host;
+    }
+
+    public Widget asWidget() {
+        BorderLayoutContainer c = new BorderLayoutContainer();
+        ToolBar tb = new ToolBar();
+        tb.add(new FillToolItem());
+        tb.add(new TextButton("Process", new SelectEvent.SelectHandler() {
+            public void onSelect(SelectEvent event) {
+                process();
+            }
+        }));
+        tb.add(new SeparatorToolItem());
+        tb.add(new TextButton("Add", new SelectEvent.SelectHandler() {
+            public void onSelect(SelectEvent event) {
+                add();
+            }
+        }));
+        c.setNorthWidget(tb, new BorderLayoutDataBuilder()
+                .size(40) // Would have been nice for toolbar to automatically set container height
+                .borderLayoutData);
+        c.setCenterWidget(lines);
+        lines.setScrollMode(ScrollSupport.ScrollMode.AUTOY);
+        return c;
+    }
+
+    private void process() {
+        ArrayList<Row> rows = new ArrayList<Row>();
+        for (LineItem line : items) {
+            rows.add(line.getRow());
+        }
+        host.notifyProcess(rows);
+    }
+
+    private void add() {
+        Row row = new Row();
+        int i = Random.nextInt();
+        row.setNum(i);
+        row.setStr("#" + i);
+        LineItem line = new LineItem(row);
+        lines.add(line);
+        items.add(line);
+        logger.info("Added line #" + i);
+    }
+
+    interface Host {
+        // NOTE: More efficient to use a concrete implementation
+        void notifyProcess(ArrayList<Row> rows);
+    }
+}
---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/nonuibuilder/LineItem.java
@@ -0,0 +1,53 @@
+package biz.freshcode.learn.gwt.client.uispike.nonuibuilder;
+
+import biz.freshcode.learn.gwt.client.uispike.gxt.Row;
+import biz.freshcode.learn.gwt.client.uispike.gxt.RowBuilder;
+import biz.freshcode.learn.gwt.client.uispike.gxt.RowRenderer;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.info.Info;
+import com.sencha.gxt.widget.core.client.toolbar.FillToolItem;
+import com.sencha.gxt.widget.core.client.toolbar.LabelToolItem;
+import com.sencha.gxt.widget.core.client.toolbar.ToolBar;
+
+public class LineItem implements IsWidget {
+    private static final RowRenderer renderer = GWT.create(RowRenderer.class);
+
+    private final Row row;
+
+    public LineItem(Row row) {
+        this.row = row;
+    }
+
+    public Row getRow() {
+        return new RowBuilder()
+                .initFrom(row)
+                .row;
+    }
+
+    @Override
+    public Widget asWidget() {
+        VerticalLayoutContainer c = new VerticalLayoutContainer();
+        ToolBar tb = new ToolBar();
+        tb.add(new LabelToolItem(row.getStr()));
+        tb.add(new FillToolItem());
+        tb.add(new TextButton("Edit...", new SelectEvent.SelectHandler() {
+            @Override
+            public void onSelect(SelectEvent event) {
+                onEdit();
+            }
+        }));
+        c.add(tb);
+        c.add(new HTMLPanel(renderer.render(row)));
+        return c;
+    }
+
+    private void onEdit() {
+        Info.display("Note", "Editing " + row.getStr());
+    }
+}
