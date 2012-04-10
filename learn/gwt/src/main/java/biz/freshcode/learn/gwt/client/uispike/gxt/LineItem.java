63043c88385e708f9e360914968783e66c3b0549
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-04-10 03:47:42 +0000

    [learn-gwt] UiSpike: Reusable RowRenderer.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/gxt/LineItem.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/gxt/LineItem.java
@@ -1,25 +1,17 @@
 package biz.freshcode.learn.gwt.client.uispike.gxt;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.IsWidget;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
-import com.sencha.gxt.core.client.XTemplates;
-import com.sencha.gxt.core.client.dom.ScrollSupport;
 import com.sencha.gxt.widget.core.client.Component;
 import com.sencha.gxt.widget.core.client.button.TextButton;
-import com.sencha.gxt.widget.core.client.container.SimpleContainer;
-import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import com.sencha.gxt.widget.core.client.info.Info;
 import com.sencha.gxt.widget.core.client.toolbar.LabelToolItem;
-
-import java.util.ArrayList;
 
 public class LineItem implements IsWidget {
     private static final Binder binder = GWT.create(Binder.class);
@@ -58,8 +50,4 @@ public class LineItem implements IsWidget {
     interface Binder extends UiBinder<Component, LineItem> {
     }
 
-    interface RowRenderer extends XTemplates {
-        @XTemplate(source = "row.html")
-        public SafeHtml render(Row row);
-    }
 }
