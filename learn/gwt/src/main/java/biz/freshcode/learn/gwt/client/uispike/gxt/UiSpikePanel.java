0fc5845be3220acfba1ec08facd924e5f8490314
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-04-10 02:04:53 +0000

    [learn-gwt] UiSpike: Conveying content to another panel.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/gxt/UiSpikePanel.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uispike/gxt/UiSpikePanel.java
@@ -3,8 +3,11 @@ package biz.freshcode.learn.gwt.client.uispike.gxt;
 import biz.freshcode.learn.gwt.client.uispike.builder.BorderLayoutDataBuilder;
 import biz.freshcode.learn.gwt.client.uispike.builder.MarginsBuilder;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.widget.core.client.Component;
@@ -32,6 +35,8 @@ public class UiSpikePanel implements IsWidget, ControlPanel.Host {
     @Override
     public void notifyProcess(ArrayList<Row> rows) {
         logger.info("Processing " + rows);
+        String content = SafeHtmlUtils.htmlEscape(rows.toString());
+        pnlCenter.setWidget(new HTMLPanel(content));
     }
 
     interface Binder extends UiBinder<Component, UiSpikePanel> {
