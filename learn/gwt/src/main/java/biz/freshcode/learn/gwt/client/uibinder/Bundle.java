7906a6565a285d35092ea5badc7e22b6f6b10653
pwl <1179301+p33t@users.noreply.github.com>
2012-04-11 07:08:56 +0000

    [learn-gwt] CssResources & UiBinder caveats.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uibinder/Bundle.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/uibinder/Bundle.java
@@ -1,22 +1,23 @@
 package biz.freshcode.learn.gwt.client.uibinder;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.ImageResource;
 
 public interface Bundle extends ClientBundle {
-    @Source("logo-med.jpg")
-    public ImageResource logo();
+    // NOTE: These are for use in Java client code (not UI Binder)
+    Bundle INSTANCE = GWT.create(Bundle.class);
+    // Need to make sure style is injected into module
+    boolean DOM_MUTATED = INSTANCE.style().ensureInjected();
 
-    // YIKES: This isn't working...
-//From style.css    .redText {
-//        color: red;
-//    }
-//
-//    @Source("style.css")
-//    public Style style();
-//
-//    public interface Style extends CssResource {
-//        // I think this is checked by the compiler.
-//        String redText();
-//    }
+    @Source("logo-med.jpg")
+    ImageResource logo();
+
+    @Source("style.css")
+    Style style();
+
+    interface Style extends CssResource {
+        String redText();
+    }
 }
