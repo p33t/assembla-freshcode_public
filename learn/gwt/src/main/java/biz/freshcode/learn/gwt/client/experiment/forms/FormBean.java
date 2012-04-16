f6715431f7bddff427711f4122afea894ba78888
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-04-16 07:07:43 +0000

    [learn-gwt] Forms: Sub beans editing.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/FormBean.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/FormBean.java
@@ -3,6 +3,8 @@ package biz.freshcode.learn.gwt.client.experiment.forms;
 import com.google.web.bindery.autobean.shared.AutoBean;
 import com.google.web.bindery.autobean.shared.AutoBeanFactory;
 
+import java.util.List;
+
 public interface FormBean {
     void setStr(String str);
 
@@ -11,6 +13,10 @@ public interface FormBean {
     void setNum(Integer num);
 
     Integer getNum();
+
+    List<FormBeanSub> getSubs();
+
+    void setSubs(List<FormBeanSub> subs);
 
     public interface Factory extends AutoBeanFactory {
         AutoBean<FormBean> create();
--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/Landing.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/Landing.java
@@ -2,22 +2,33 @@ package biz.freshcode.learn.gwt.client.experiment.forms;
 
 import biz.freshcode.learn.gwt.client.uispike.builder.TextButtonBuilder;
 import biz.freshcode.learn.gwt.client.uispike.builder.container.FlowLayoutContainerBuilder;
-import biz.freshcode.learn.gwt.client.uispike.builder.container.HtmlLayoutContainerBuilder;
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.web.bindery.autobean.shared.AutoBean;
-import com.sencha.gxt.widget.core.client.Dialog;
+import com.sencha.gxt.core.client.util.Util;
 import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.container.FlowLayoutContainer;
-import com.sencha.gxt.widget.core.client.container.HtmlLayoutContainer;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
+
+import java.util.Date;
+import java.util.List;
 
 public class Landing extends AbstractIsWidget {
     FormBean.Factory factory = GWT.create(FormBean.Factory.class);
     // Keep this around so that changes can be accumulated.
     private AutoBean<FormBean> formBean = factory.create();
+
+    {
+        // Populate some default sub beans
+        FormBean bean = formBean.as();
+        List<FormBeanSub> subs = Util.<FormBeanSub>createList();
+        FormBeanSub sub = new FormBeanSub();
+        sub.setDt(new Date(0L));
+        sub.setName("Epoch");
+        subs.add(sub);
+        bean.setSubs(subs);
+    }
 
     @Override
     protected Widget createWidget() {
