2d1a2c9d96f7dcf9469808818410316c1bd1c485
pwl <1179301+p33t@users.noreply.github.com>
2012-04-16 04:03:26 +0000

    [learn-gwt] Dialog (forms).

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/FormDialog.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/FormDialog.java
@@ -4,11 +4,19 @@ import biz.freshcode.learn.gwt.client.uispike.builder.DialogBuilder;
 import biz.freshcode.learn.gwt.client.uispike.builder.TextButtonBuilder;
 import biz.freshcode.learn.gwt.client.uispike.builder.VerticalLayoutContainerBuilder;
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.sencha.gxt.widget.core.client.Dialog;
 import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 
+import java.util.logging.Logger;
+
 public class FormDialog extends AbstractIsWidget<Dialog> {
+
+    Logger logger = Logger.getLogger(getClass().getName());
+
     @Override
     protected Dialog createWidget() {
         TextButton btnOk;
@@ -17,11 +25,13 @@ public class FormDialog extends AbstractIsWidget<Dialog> {
                 .width(500)
                 .height(300)
                 .add(new VerticalLayoutContainerBuilder()
-                        // TODO: Work out dialog buttons.
-                        .add(btnOk = new TextButtonBuilder()
-                                .text("OK")
-                                .textButton)
+                        .add(new HTMLPanel("<p>This is the main area</p>"))
                         .verticalLayoutContainer)
+                // The predefined buttons are a litle useless.  You have to dig them out again to define handlers (?)
+                .predefinedButtons(new Dialog.PredefinedButton[0])
+                .addButton(btnOk = new TextButtonBuilder()
+                        .text("OK")
+                        .textButton)
                 .dialog;
 
         btnOk.addSelectHandler(new SelectEvent.SelectHandler() {
