0e8366314fc679474e3fc8c8860277e95ce19ec9
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-04-24 00:54:13 +0000

    [learn-gwt] Refactor / intro HrMinConverter.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/HrMinPropertyEditor.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/HrMinPropertyEditor.java
@@ -1,54 +1,27 @@
 package biz.freshcode.learn.gwt.client.experiment.forms;
 
-import com.google.gwt.regexp.shared.MatchResult;
-import com.google.gwt.regexp.shared.RegExp;
 import com.sencha.gxt.widget.core.client.form.PropertyEditor;
 
 import java.text.ParseException;
 
 /**
- * Translates between an h:mm string and a long millisecond value.
- * Negatives are permitted.
- * Hour values > 23 are permitted.
- * The ':mm' portion is optional.
+ * Converts string to / from a long.
+ * @see HrMinConverter
  */
 public class HrMinPropertyEditor extends PropertyEditor<Long> {
     public static final HrMinPropertyEditor INSTANCE = new HrMinPropertyEditor();
 
-    private static final int MINUTE = 60 * 1000;
-    private static final int HOUR = 60 * MINUTE;
-    private static final RegExp RE = RegExp.compile("^(-?)(\\d+)(:([0-5]?)(\\d))?$");
-
     @Override
     public Long parse(CharSequence text) throws ParseException {
         if (text == null) return null;
-        String s = text.toString().trim();
-        if (s.isEmpty()) return null;
-        MatchResult mr = RE.exec(s);
-        if (mr == null) throw new ParseException("Must be in the form '-h:m', '-' and ':m' are optional", 0);
-        int hrs = Integer.parseInt(mr.getGroup(2));
-        int min = 0;
-        if (!isEmpty(mr.getGroup(3))) min = Integer.parseInt(mr.getGroup(4) + mr.getGroup(5));
-        long ms = hrs * HOUR + min * MINUTE;
-        if ("-".equals(mr.getGroup(1))) ms = ms * -1;
-        return ms;
-    }
-
-    private boolean isEmpty(String s) {
-        return s == null || s.isEmpty();
+        String str = text.toString();
+        if (str.trim().isEmpty()) return null;
+        if (!HrMinConverter.RE.test(str)) throw new ParseException(HrMinConverter.RE_MSG, 0);
+        return HrMinConverter.INSTANCE.convertFieldValue(str);
     }
 
     @Override
     public String render(Long time) {
-        if (time == null) return "";
-        String s = "";
-        Long t = Math.abs(time);
-        if (time < 0) s += "-";
-        s += t / HOUR;
-        s += ":";
-        long mins = t % HOUR / MINUTE;
-        if (mins < 10) s += "0";
-        s += mins;
-        return s;
+        return HrMinConverter.INSTANCE.convertModelValue(time);
     }
 }
