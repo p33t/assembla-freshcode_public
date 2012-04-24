6e76388294cf2fe4a92d9abcc2037dd83408afca
pwl <1179301+p33t@users.noreply.github.com>
2012-04-24 05:49:33 +0000

    [learn-gwt] Forms: Hr Min Spinners (WIP).

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/HrMinField.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/HrMinField.java
@@ -1,21 +1,24 @@
 package biz.freshcode.learn.gwt.client.experiment.forms;
 
+import com.sencha.gxt.cell.core.client.form.SpinnerFieldCell;
 import com.sencha.gxt.widget.core.client.event.ParseErrorEvent;
 import com.sencha.gxt.widget.core.client.form.NumberField;
 
 import java.util.logging.Logger;
-
 
 // NOTE: Can't use SpinnerField<Long> as parent class because it won't accept ':' char.
 public class HrMinField extends NumberField<Long> {
     private Logger logger = Logger.getLogger(getClass().getName());
 
     public HrMinField() {
-        super(new HrMinPropertyEditor());
+        this(new SpinnerFieldCell(new HrMinPropertyEditor()));
+    }
+
+    private HrMinField(SpinnerFieldCell<Long> cell) {
+        super(cell, cell.getPropertyEditor());
+        cell.setHideTrigger(false);
         setDecimalSeparator(":"); // permit the separator
         setAllowDecimals(true);
-//        This shows the single / drop-down trigger
-//        setHideTrigger(false);
 
         // NOTE: There is already a SideErrorHandler in errorSupport field.
         addParseErrorHandler(new ParseErrorEvent.ParseErrorHandler() {
