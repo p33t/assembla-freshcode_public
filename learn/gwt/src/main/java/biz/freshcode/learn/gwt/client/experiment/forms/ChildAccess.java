0ce149571a310d5ea0d8ff0aadeb9d2c44b4892a
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-04-30 04:24:57 +0000

    [learn-gwt] Forms: Finish column working properly (FinishCell was a red herring).

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/ChildAccess.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/ChildAccess.java
@@ -2,7 +2,6 @@ package biz.freshcode.learn.gwt.client.experiment.forms;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.editor.client.Editor;
-import com.sencha.gxt.core.client.IdentityValueProvider;
 import com.sencha.gxt.core.client.ValueProvider;
 import com.sencha.gxt.data.shared.ModelKeyProvider;
 import com.sencha.gxt.data.shared.PropertyAccess;
@@ -26,10 +25,5 @@ interface ChildAccess extends PropertyAccess<FormBeanChild> {
 
     ValueProvider<FormBeanChild, Long> duration();
 
-/*
-NOTE: This is not working.  Strange error indicating custom ValueProviders are not simple....
-00:00:10.238 [ERROR] Errors in '/home/pleong/work/learn/gwt/target/.generated/biz/freshcode/learn/gwt/client/experiment/forms/FormBeanChild_finish_ValueProviderImpl.java'
-00:00:10.238 [ERROR] Line 3: Implicit super constructor FinishProvider() is undefined for default constructor. Must define an explicit constructor
- */
-    IdentityValueProvider<FormBeanChild> finish();
+    // NOTE: Don't use this interface to provide generated / calculated values.  See FinishProvider.
 }
