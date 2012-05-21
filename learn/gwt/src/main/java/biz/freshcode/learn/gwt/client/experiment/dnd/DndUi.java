8c334ad670ea32aab2cd84ed1b59915b948371ee
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-05-21 02:03:46 +0000

    [learn-gwt] Dnd learn.

---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/dnd/DndUi.java
@@ -0,0 +1,18 @@
+package biz.freshcode.learn.gwt.client.experiment.dnd;
+
+import biz.freshcode.learn.gwt.client.uispike.builder.BorderLayoutContainerBuilder;
+import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.sencha.gxt.widget.core.client.container.Container;
+
+/**
+ * A Ui driven by Drag 'n Drop.
+ */
+public class DndUi extends AbstractIsWidget<Container> {
+    @Override
+    protected Container createWidget() {
+        return new BorderLayoutContainerBuilder()
+                .widget(new HTMLPanel("<p>Center hopefully</p>"))
+                .borderLayoutContainer;
+    }
+}
