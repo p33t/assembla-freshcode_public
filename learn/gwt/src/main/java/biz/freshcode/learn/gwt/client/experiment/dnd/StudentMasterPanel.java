7bfb5c1076d5f7ee8e8cc4e2c0630e32dbf8c3cb
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-05-21 06:59:38 +0000

    [learn-gwt] Dnd learn - fix single instance in treeview problem.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/dnd/StudentMasterPanel.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/dnd/StudentMasterPanel.java
@@ -2,6 +2,7 @@ package biz.freshcode.learn.gwt.client.experiment.dnd;
 
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.data.shared.ModelKeyProvider;
 import com.sencha.gxt.data.shared.TreeStore;
 import com.sencha.gxt.widget.core.client.tree.Tree;
 
@@ -12,7 +13,15 @@ import java.util.List;
 public class StudentMasterPanel extends AbstractIsWidget {
     @Override
     protected Widget createWidget() {
-        TreeStore<Named> ts = new TreeStore<Named>(Named.ACCESS.id());
+        // Facilitates adding the same object multiles times in a tree.  We use a wrapper ref object.
+        ModelKeyProvider<Ref<Named>> instanceUniqueId = new ModelKeyProvider<Ref<Named>>() {
+            @Override
+            public String getKey(Ref<Named> item) {
+                return "ref " + System.identityHashCode(item);
+            }
+        };
+
+        TreeStore<Ref<Named>> ts = new TreeStore<Ref<Named>>(instanceUniqueId);
 
         List<Course> courses = DndUtil.COURSES;
         List<Student> students = DndUtil.STUDENTS;
@@ -23,23 +32,28 @@ public class StudentMasterPanel extends AbstractIsWidget {
 
         ts.addSubTree(0, ads);
 
-        return new Tree(ts, Named.ACCESS.name());
+        return new Tree(ts, DndUtil.valueProvider(Named.ACCESS.name()));
     }
 
     private Course course(String name, List<Student> ss) {
         return DndUtil.course(name, ss);
     }
 
-    public static class CourseAdapter implements TreeStore.TreeNode<Named> {
+    /**
+     * Adapts a course for display in a tree.
+     */
+    public static class CourseAdapter implements TreeStore.TreeNode<Ref<Named>> {
         private final Course course;
+        private final Ref<Named> ref;
 
         public CourseAdapter(Course course) {
             this.course = course;
+            ref = DndUtil.ref((Named) course);
         }
 
         @Override
-        public List<? extends TreeStore.TreeNode<Named>> getChildren() {
-            List<TreeStore.TreeNode<Named>> l = new ArrayList<TreeStore.TreeNode<Named>>();
+        public List<? extends TreeStore.TreeNode<Ref<Named>>> getChildren() {
+            List<TreeStore.TreeNode<Ref<Named>>> l = new ArrayList<TreeStore.TreeNode<Ref<Named>>>();
             for (Student s : course.getAttendees()) {
                 l.add(new StudentAdapter(s));
             }
@@ -47,26 +61,31 @@ public class StudentMasterPanel extends AbstractIsWidget {
         }
 
         @Override
-        public Named getData() {
-            return course;
+        public Ref<Named> getData() {
+            return ref;
         }
     }
 
-    public static class StudentAdapter implements TreeStore.TreeNode<Named> {
+    /**
+     * Adapts a Student for display in a tree.
+     */
+    public static class StudentAdapter implements TreeStore.TreeNode<Ref<Named>> {
         private final Student student;
+        private final Ref<Named> ref;
 
         public StudentAdapter(Student student) {
             this.student = student;
+            ref = DndUtil.ref((Named) student);
         }
 
         @Override
-        public List<? extends TreeStore.TreeNode<Named>> getChildren() {
+        public List<? extends TreeStore.TreeNode<Ref<Named>>> getChildren() {
             return Collections.EMPTY_LIST;
         }
 
         @Override
-        public Named getData() {
-            return student;
+        public Ref<Named> getData() {
+            return ref;
         }
     }
 }
