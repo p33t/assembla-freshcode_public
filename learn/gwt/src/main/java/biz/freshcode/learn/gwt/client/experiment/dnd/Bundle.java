b0da5c31f55e4f361883172f12d4ec25c2a0b745
pwl <1179301+p33t@users.noreply.github.com>
2012-05-21 06:00:20 +0000

    [learn-gwt] Dnd learn - white bgnd

---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/dnd/Bundle.java
@@ -0,0 +1,18 @@
+package biz.freshcode.learn.gwt.client.experiment.dnd;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+public interface Bundle extends ClientBundle {
+    Bundle INSTANCE = GWT.create(Bundle.class);
+    // Needed
+    boolean DOM_MUTATED = INSTANCE.style().ensureInjected();
+
+    @Source("style.css")
+    Style style();
+
+    interface Style extends CssResource {
+        String whiteBgnd();
+    }
+}
