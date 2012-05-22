0e1e29eff09b20d88a0cf7de498ac1697228cea3
pwl <1179301+p33t@users.noreply.github.com>
2012-05-22 01:58:00 +0000

    [learn-gwt] Dnd learn - Refactor.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/dnd/ExamWidget.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/dnd/ExamWidget.java
@@ -2,19 +2,19 @@ package biz.freshcode.learn.gwt.client.experiment.dnd;
 
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
 import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.Widget;
-import com.sencha.gxt.data.shared.TreeStore;
 import com.sencha.gxt.dnd.core.client.DndDropEvent;
 import com.sencha.gxt.dnd.core.client.DropTarget;
 import com.sencha.gxt.widget.core.client.container.SimpleContainer;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
 import java.util.logging.Logger;
 
 /**
  *
  */
-public class ExamWidget extends AbstractIsWidget {
+public class ExamWidget extends AbstractIsWidget<SimpleContainer> {
     private Logger log = Logger.getLogger(getClass().getName());
     private final Exam exam;
 
@@ -23,7 +23,7 @@ public class ExamWidget extends AbstractIsWidget {
     }
 
     @Override
-    protected Widget createWidget() {
+    protected SimpleContainer createWidget() {
         final SimpleContainer container = new SimpleContainer();
         container.add(renderPanel());
         container.setStyleName(Bundle.INSTANCE.style().dropElem(), true);
@@ -33,34 +33,21 @@ public class ExamWidget extends AbstractIsWidget {
             @Override
             public void onDrop(DndDropEvent event) {
                 Object data = event.getData();
-                boolean processed = false;
-                if (data instanceof List) {
-                    List l = (List) data;
-                    Set<Student> students = new HashSet<Student>();
-                    for (Object elem : l) {
-                        if (elem instanceof TreeStore.TreeNode) {
-                            Object nodeData = ((TreeStore.TreeNode) elem).getData();
-                            if (nodeData instanceof Ref) {
-                                Object payload = ((Ref) nodeData).getObj();
-                                if (payload instanceof Student) students.add((Student) payload);
-                                else if (payload instanceof Course) students.addAll(((Course) payload).getAttendees());
-                            }
-                        }
-                    }
-                    if (!students.isEmpty()) {
-                        processed = true;
-                        students.addAll(exam.getAttendees());
-                        ArrayList<Student> attendees = new ArrayList<Student>(students);
-                        exam.setAttendees(Collections.unmodifiableList(attendees));
-                        container.setWidget(renderPanel());
-                    }
-
-
-                }
-                if (!processed) log.warning("Nothing to process from " + event);
+                processDrop(data);
             }
         });
         return container;
+    }
+
+    private void processDrop(Object data) {
+        Set<Student> students = DndUtil.droppedStudents(data);
+        if (students.isEmpty()) log.warning("Nothing to process from data " + data);
+        else {
+            students.addAll(exam.getAttendees());
+            ArrayList<Student> attendees = new ArrayList<Student>(students);
+            exam.setAttendees(Collections.unmodifiableList(attendees));
+            asWidget().setWidget(renderPanel());
+        }
     }
 
     private HTMLPanel renderPanel() {
