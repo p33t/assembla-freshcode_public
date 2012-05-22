bcd28710dbf3caa2f28c2c8146232b59f59fdda9
pwl <1179301+p33t@users.noreply.github.com>
2012-05-22 03:43:15 +0000

    [learn-gwt] Dnd learn - Drop on Exams panel to create.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/dnd/DndCenter.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/dnd/DndCenter.java
@@ -4,28 +4,63 @@ import biz.freshcode.learn.gwt.client.uispike.builder.VerticalLayoutContainerBui
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.IsWidget;
-import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.core.client.util.Util;
+import com.sencha.gxt.dnd.core.client.DndDropEvent;
 import com.sencha.gxt.dnd.core.client.DropTarget;
+import com.sencha.gxt.widget.core.client.box.PromptMessageBox;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
+import com.sencha.gxt.widget.core.client.event.HideEvent;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
 
 /**
  *
  */
-public class DndCenter extends AbstractIsWidget {
+public class DndCenter extends AbstractIsWidget<VerticalLayoutContainer> {
+    private Logger log = Logger.getLogger(getClass().getName());
+
     @Override
-    protected Widget createWidget() {
+    protected VerticalLayoutContainer createWidget() {
         VerticalLayoutContainer container = new VerticalLayoutContainerBuilder()
-                .add(new HTMLPanel("<h2>Exams</h2>"))
+                .add(new HTMLPanel("<h2>Exams</h2><p>Drag one or more tree elements to this outer panel or to one of the exam panels.</p>"))
                 .add(elem("Prog'g 101", DndUtil.STUDENTS))
                 .add(elem("History 202", Util.createList(DndUtil.STUDENTS.get(0))))
                 .styleName(Bundle.INSTANCE.style().blackBorder(), true)
                 .verticalLayoutContainer;
         DropTarget target = new DropTarget(container);
+        target.addDropHandler(new DndDropEvent.DndDropHandler() {
+            @Override
+            public void onDrop(DndDropEvent event) {
+                processDrop(event.getData());
+            }
+        });
         target.setOverStyle(Bundle.INSTANCE.style().dragOver());
         return container;
+    }
+
+    private void processDrop(Object data) {
+        Set<Student> students = DndUtil.droppedStudents(data);
+        if (students.isEmpty()) {
+            log.warning("No students in " + data);
+            return;
+        }
+        final List<Student> attendees = new ArrayList<Student>(students);
+        final PromptMessageBox box = new PromptMessageBox("New Exam", "Exam name:");
+        box.addHideHandler(new HideEvent.HideHandler() {
+            @Override
+            public void onHide(HideEvent event) {
+                String name = box.getValue();
+                // null when 'cancelled'
+                if (name == null || name.isEmpty()) return;
+                IsWidget examWidget = elem(name, Collections.unmodifiableList(attendees));
+                asWidget().add(examWidget);
+            }
+        });
+        box.show();
     }
 
     private IsWidget elem(String name, List<Student> attendees) {
