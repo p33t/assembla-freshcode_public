d86e3a9297a519374dcab0d958e601b8559d2042
pwl <1179301+p33t@users.noreply.github.com>
2012-07-02 03:31:15 +0000

    [learn-gwt] Exploring AutoBeans.

--- learn/gwt/src/test/java/biz/freshcode/learn/gwt/client/autobean/AutoBeanTest.java
+++ learn/gwt/src/test/java/biz/freshcode/learn/gwt/client/autobean/AutoBeanTest.java
@@ -9,28 +9,47 @@ import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
 public class AutoBeanTest {
-    TestBean.Factory factory = AutoBeanFactorySource.create(TestBean.Factory.class);
-    
+    TestBean.Factory factTest = AutoBeanFactorySource.create(TestBean.Factory.class);
+    NestedBean.Factory factNested = AutoBeanFactorySource.create(NestedBean.Factory.class);
+
     @Test
-    public void confirmCanonicalEqualsDoesNotWork() {
-        TestBean tb = testBean("s1");
-        TestBean tb2 = testBean("s1");
-        assertFalse(tb.equals(tb2));
+    public void confirmCanonicalEqualsDoesNOTWork() {
+        TestBean t1 = testBean("s1");
+        TestBean t2 = testBean("s1");
+        // BAD
+        assertFalse(t1.equals(t2));
+    }
+
+    @Test
+    public void confirmUtilsEqualsWorksUnNested() {
+        TestBean t1 = testBean("s1");
+        TestBean t2 = testBean("s1");
+        assertTrue(deepEquals(t1, t2));
+        t2.setStr("s2");
+        assertFalse(deepEquals(t1, t2));
     }
 
     @Test
     public void confirmUtilsEqualsWorks() {
-        TestBean tb = testBean("s1");
-        TestBean tb2 = testBean("s1");
-        assertTrue(deepEquals(tb, tb2));
-        tb2.setStr("s2");
-        assertFalse(deepEquals(tb, tb2));
+        TestBean t1 = testBean("s1");
+        TestBean t2 = testBean("s1");
+        t1.setNested(nested("n1"));
+        t2.setNested(nested("n1"));
+        assertTrue(deepEquals(t1, t2));
+        t2.getNested().setNest("n2");
+        assertFalse(deepEquals(t1, t2));
     }
 
     private TestBean testBean(String str) {
         TestBean tb2 = testBean();
         tb2.setStr(str);
         return tb2;
+    }
+
+    private NestedBean nested(String str) {
+        NestedBean n = factNested.auto().as();
+        n.setNest(str);
+        return n;
     }
 
     private boolean deepEquals(TestBean t1, TestBean t2) {
@@ -40,7 +59,7 @@ public class AutoBeanTest {
     }
 
     private TestBean testBean() {
-        AutoBean<TestBean> auto = factory.auto();
+        AutoBean<TestBean> auto = factTest.auto();
         return auto.as();
     }
 }
