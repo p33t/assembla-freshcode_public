5db413987bf5e819e5ee416ffa374795d53cc984
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-07-03 02:12:14 +0000

    [learn-gwt] Exploring AutoBeans.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/util/GenericAutoBeanFactory.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/util/GenericAutoBeanFactory.java
@@ -5,7 +5,8 @@ import com.google.web.bindery.autobean.shared.AutoBeanFactory;
 
 /**
  * An autobean factory that only produces a single type of autobean.
- * NOTE: Extending GenericAutoBeanFactory breaks AutoBeanFactorySource (and any unit testing) unless the 'auto' method is redeclared.
+ * NOTE: Extending GenericAutoBeanFactory breaks AutoBeanFactorySource (and any unit testing)
+ * unless the 'auto' method is redeclared.
  */
 public interface GenericAutoBeanFactory<T> extends AutoBeanFactory {
     AutoBean<T> auto();
