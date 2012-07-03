7db0f8fcd90dc3af676b0873afc2167f944f33a4
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-07-03 05:38:29 +0000

    [learn-gwt] Exploring GXT Grid.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/grid/GridDemo.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/grid/GridDemo.java
@@ -2,6 +2,15 @@ package biz.freshcode.learn.gwt.client.experiment.grid;
 
 import biz.freshcode.learn.gwt.client.uispike.builder.table.ColumnConfigBuilder;
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
+import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.cell.client.ValueUpdater;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.safecss.shared.SafeStylesUtils;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.core.client.ToStringValueProvider;
 import com.sencha.gxt.data.shared.ListStore;
@@ -11,8 +20,10 @@ import com.sencha.gxt.widget.core.client.grid.ColumnModel;
 import com.sencha.gxt.widget.core.client.grid.Grid;
 
 import java.util.List;
+import java.util.Set;
 
 import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.newListFrom;
+import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.newSetFrom;
 
 public class GridDemo extends AbstractIsWidget {
     private ListStore<RowEntity> store = new ListStore<RowEntity>(new ModelKeyProvider<RowEntity>() {
@@ -27,6 +38,30 @@ public class GridDemo extends AbstractIsWidget {
         List<ColumnConfig> configs = newListFrom(
                 new ColumnConfigBuilder(new ColumnConfig(new ToStringValueProvider<RowEntity>()))
                         .header("To String")
+                        .columnTextStyle(SafeStylesUtils.fromTrustedString("color:blue; text-align:center;"))
+                        .cell(new AbstractCell<String>() {
+                            @Override
+                            public void render(Context context, String value, SafeHtmlBuilder sb) {
+                                // Div causes events to echo
+//                                sb.appendHtmlConstant("<div style='color:blue; text-align:center;'>");
+                                sb.appendEscaped(value);
+//                                sb.appendHtmlConstant("</div>");
+                            }
+
+                            @Override
+                            public Set<String> getConsumedEvents() {
+                                return newSetFrom(
+                                        MouseOutEvent.getType().getName(),
+                                        MouseOverEvent.getType().getName()
+                                );
+                            }
+
+                            @Override
+                            public void onBrowserEvent(Context context, Element parent, String value, NativeEvent event, ValueUpdater<String> stringValueUpdater) {
+                                String msg = event.getType() + " on " + value;
+                                GWT.log(msg);
+                            }
+                        })
                         .columnConfig
         );
         ColumnModel colModel = new ColumnModel(configs);
