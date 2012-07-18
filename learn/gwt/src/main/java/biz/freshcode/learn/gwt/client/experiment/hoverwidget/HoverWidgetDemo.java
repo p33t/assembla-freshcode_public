401494eff93534c2310b86e4071add109d416592
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-07-18 01:25:18 +0000

    [learn-gwt] Hover widgets.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/hoverwidget/HoverWidgetDemo.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/hoverwidget/HoverWidgetDemo.java
@@ -1,12 +1,35 @@
 package biz.freshcode.learn.gwt.client.experiment.hoverwidget;
 
+import biz.freshcode.learn.gwt.client.experiment.hoverwidget.reuse.HoverWidgetSupport;
+import biz.freshcode.learn.gwt.client.experiment.mouseover.MouseOverState;
+import biz.freshcode.learn.gwt.client.uispike.builder.VerticalLayoutContainerBuilder;
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
-import com.google.gwt.user.client.ui.HTMLPanel;
+import biz.freshcode.learn.gwt.client.util.DummySelectHandler;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.button.ToolButton;
+import com.sencha.gxt.widget.core.client.container.HtmlLayoutContainer;
 
 public class HoverWidgetDemo extends AbstractIsWidget {
+    private HoverWidgetSupport<ToolButton> hoverSupp;
+
     @Override
     protected Widget createWidget() {
-        return new HTMLPanel("<p>Hello</p>");
+        ToolButton hoverWidget = new ToolButton(ToolButton.SAVE, new DummySelectHandler("Saved"));
+        final HtmlLayoutContainer host = new HtmlLayoutContainer("<p>Hover Over This</p>");
+
+        hoverSupp = new HoverWidgetSupport<ToolButton>(hoverWidget);
+
+        new MouseOverState(host, new MouseOverState.Callback() {
+            @Override
+            public void stateChange(MouseOverState mos) {
+                if (mos.isDraggingOver()) hoverSupp.disablePopup();
+                else if (mos.isOver()) hoverSupp.enablePopup(host.getAbsoluteLeft(), host.getAbsoluteTop());
+                else hoverSupp.disablePopup();
+            }
+        });
+
+        return new VerticalLayoutContainerBuilder()
+                .add(host)
+                .verticalLayoutContainer;
     }
 }
---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/hoverwidget/reuse/HoverWidgetSupport.java
@@ -0,0 +1,108 @@
+package biz.freshcode.learn.gwt.client.experiment.hoverwidget.reuse;
+
+import biz.freshcode.learn.gwt.client.experiment.mouseover.MouseOverState;
+import biz.freshcode.learn.gwt.client.uispike.builder.container.PopupPanelBuilder;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.core.client.util.Point;
+
+import static biz.freshcode.learn.gwt.client.experiment.hoverwidget.reuse.Bundle.STYLE;
+
+public class HoverWidgetSupport<W extends Widget> {
+    protected final W hoverWidget;
+    private final MouseOverState mosPopup;
+    private final PopupPanel popup;
+    private Point popupCoord;
+
+    public HoverWidgetSupport(W hoverWidget) {
+        this.hoverWidget = hoverWidget;
+        popup = new PopupPanelBuilder()
+                .addStyleName(STYLE.hoverWidgetPopup())
+                .popupPanel;
+        mosPopup = new MouseOverState(popup, new MouseOverState.Callback() {
+            @Override
+            public void stateChange(MouseOverState mos) {
+                checkPopup();
+            }
+        });
+    }
+
+    /**
+     * Checks and updates the state of the popup.  This is designed to not repeat echo'd operations.
+     * So the popup state can be jittery but will still display cleanly.
+     */
+    public void checkPopup() {
+        Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+            @Override
+            public void execute() {
+                Point coord = popupCoord;
+                if (mosPopup.isOver()) {
+                    // mouse is currently over the popup
+                    // the popup will probably be disabled by now but it must remain shown
+                } else if (coord != null) {
+                    // popup enabled
+                    showPopupIfNecessary(coord);
+                } else {
+                    hidePopupIfNecessary();
+                }
+            }
+        });
+    }
+
+    public void disablePopup() {
+        popupCoord = null;
+        checkPopup();
+    }
+
+    public void enablePopup(int left, int top) {
+        popupCoord = new Point(left, top);
+        checkPopup();
+    }
+
+    /**
+     * Subclasses can customise the hoverWidget just before it is shown.
+     */
+    protected void customizeHoverWidget(W hoverWidget) {
+        // do nothing by default
+    }
+
+    /**
+     * Optional notification to subclasses.
+     *
+     * @param coord
+     */
+    protected void popupHidden(Point coord) {
+        // nothing
+    }
+
+    private void showPopupIfNecessary(Point coord) {
+        if (coord.equals(currentCoord())) {
+            // location is accurate (event may have been echoed)
+            if (!popup.isShowing()) showPopup();
+        } else {
+            // different location
+            hidePopupIfNecessary();
+            popup.setPopupPosition(coord.getX(), coord.getY());
+            showPopup();
+        }
+    }
+
+    private void showPopup() {
+        customizeHoverWidget(hoverWidget);
+        popup.setWidget(hoverWidget); // TODO: Move his to constructor?
+        popup.show();
+    }
+
+    private void hidePopupIfNecessary() {
+        if (popup.isShowing()) {
+            Point coord = currentCoord();
+            popup.hide();
+            popupHidden(coord);
+        }
+    }
+
+    private Point currentCoord() {
+        return new Point(popup.getPopupLeft(), popup.getPopupTop());
+    }
+}
