34af1d817522648d489748e203e2c3b3bf83c7f1
pwl <1179301+p33t@users.noreply.github.com>
2012-07-25 05:09:39 +0000

    [learn-gwt] GIN dependency injection.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/AppActivityMapper.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/AppActivityMapper.java
@@ -5,18 +5,25 @@ import biz.freshcode.learn.gwt.client.experiment.mvp.gwtmvp.GwtMvp;
 import com.google.gwt.activity.shared.Activity;
 import com.google.gwt.activity.shared.ActivityMapper;
 import com.google.gwt.place.shared.Place;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
 
 /**
  * Detects specific Place instances and creates the corresponding Activity.
  */
+@Singleton
 public class AppActivityMapper implements ActivityMapper {
     public static final Place DEFAULT_PLACE = new Place() {
     };
 
+    @Inject
+    Provider<GwtMvp> gmProvider;
+
     @Override
     public Activity getActivity(Place place) {
         if (place instanceof GmPlace) {
-            return new GwtMvp((GmPlace) place);
+            return gmProvider.get().goTo((GmPlace) place);
         }
         if (DEFAULT_PLACE.equals(place)) {
             return new MainActivity();
