b5b524fd02d5a59d8643726f38c752e3eb620682
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-07-25 04:36:19 +0000

    [learn-gwt] GIN dependency injection.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/EntryPoint.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/EntryPoint.java
@@ -1,33 +1,46 @@
 package biz.freshcode.learn.gwt.client;
 
 import biz.freshcode.learn.gwt.client.inject.AppInjector;
+import com.google.gwt.activity.shared.ActivityManager;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.place.shared.PlaceController;
+import com.google.gwt.place.shared.PlaceHistoryHandler;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.inject.Singleton;
+import com.google.web.bindery.event.shared.EventBus;
+
+import javax.inject.Inject;
 
 import static biz.freshcode.learn.gwt.client.AppActivityMapper.DEFAULT_PLACE;
 
 public class EntryPoint implements com.google.gwt.core.client.EntryPoint {
-    // TODO: Should this be a constant?
-    public static final AppInjector INJECTOR = GWT.create(AppInjector.class);
 
     @Override
     public void onModuleLoad() {
-        MainPanel mainPanel = new MainPanel();
+        AppInjector injector = GWT.create(AppInjector.class);
+        injector.bootstrap();
+    }
 
-        // TODO: Perhaps have an internal injectable entrypoint implementation that doesn't use INJECTOR so much.
-        INJECTOR.activityManager().setDisplay(mainPanel);
+    @Singleton
+    public static class Bootstrap {
+        @Inject
+        public Bootstrap(ActivityManager mgr,
+                         PlaceHistoryHandler handler,
+                         PlaceController ctlr,
+                         EventBus bus,
+                         MainPanel pnl) {
+            GWT.log("Bootstrapping Module");
+            mgr.setDisplay(pnl);
+            handler.register(ctlr, bus, DEFAULT_PLACE);
 
-        // Start PlaceHistoryHandler with our PlaceHistoryMapper
-        INJECTOR.historyHandler().register(INJECTOR.placeController(), INJECTOR.eventBus(), DEFAULT_PLACE);
+            // Remove 'loading' spinner.
+            DOM.removeChild(RootPanel.getBodyElement(), DOM.getElementById("loading"));
 
-
-        // Remove 'loading' spinner.
-        DOM.removeChild(RootPanel.getBodyElement(), DOM.getElementById("loading"));
-
-        RootLayoutPanel.get().add(mainPanel);
-        // Goes to the place represented on URL else default place
-        INJECTOR.historyHandler().handleCurrentHistory();
+            RootLayoutPanel.get().add(pnl);
+            // Goes to the place represented on URL else default place
+            handler.handleCurrentHistory();
+        }
     }
 }
