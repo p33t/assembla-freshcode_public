2033ba80cff150d9a2c6b26dacae83fa63dc517a
pwl <1179301+p33t@users.noreply.github.com>
2012-07-30 06:55:42 +0000

    [learn-gwt] Toolbar demo - date selection.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/toolbar/ToolBarDemo.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/toolbar/ToolBarDemo.java
@@ -2,19 +2,28 @@ package biz.freshcode.learn.gwt.client.experiment.toolbar;
 
 import biz.freshcode.learn.gwt.client.IsRootContent;
 import biz.freshcode.learn.gwt.client.uispike.builder.BorderLayoutContainerBuilder;
+import biz.freshcode.learn.gwt.client.uispike.builder.PopupBuilder;
 import biz.freshcode.learn.gwt.client.uispike.builder.ToolBarBuilder;
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.DatePicker;
+import com.sencha.gxt.widget.core.client.Popup;
 import com.sencha.gxt.widget.core.client.button.ToolButton;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.info.Info;
 import com.sencha.gxt.widget.core.client.toolbar.FillToolItem;
 import com.sencha.gxt.widget.core.client.toolbar.LabelToolItem;
 import com.sencha.gxt.widget.core.client.toolbar.SeparatorToolItem;
 import com.sencha.gxt.widget.core.client.toolbar.ToolBar;
 
+import java.util.Date;
+
 public class ToolBarDemo extends AbstractIsWidget implements IsRootContent {
     private LabelToolItem lbl;
     private ToolBar toolBar;
+    private ToolButton dater;
 
     @Override
     protected Widget createWidget() {
@@ -36,6 +45,8 @@ public class ToolBarDemo extends AbstractIsWidget implements IsRootContent {
                                 changeLabel("A very big label that will need resize");
                             }
                         }))
+                        .add(new SeparatorToolItem())
+                        .add(dater = new ToolButton(ToolButton.MAXIMIZE, new DaterHandler()))
                         .add(new FillToolItem())
                         .toolBar)
                 .borderLayoutContainer;
@@ -44,5 +55,24 @@ public class ToolBarDemo extends AbstractIsWidget implements IsRootContent {
     private void changeLabel(String str) {
         lbl.setLabel(str);
         toolBar.forceLayout();
+    }
+
+    private class DaterHandler implements SelectEvent.SelectHandler {
+        @Override
+        public void onSelect(SelectEvent event) {
+            DatePicker picker;
+            final Popup popup = new PopupBuilder()
+                    .add(picker = new DatePicker())
+                    .borders(true)
+                    .popup;
+            picker.addValueChangeHandler(new ValueChangeHandler<Date>() {
+                @Override
+                public void onValueChange(ValueChangeEvent<Date> evt) {
+                    popup.hide();
+                    Info.display("Chose", evt.getValue().toString());
+                }
+            });
+            popup.show(dater);
+        }
     }
 }
