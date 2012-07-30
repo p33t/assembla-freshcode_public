0dc023a484abe233bd8ba8ec009e3164abfb3119
pwl <1179301+p33t@users.noreply.github.com>
2012-07-30 04:44:23 +0000

    [learn-gwt] Toolbar demo.

---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/toolbar/ToolBarDemo.java
@@ -0,0 +1,50 @@
+package biz.freshcode.learn.gwt.client.experiment.toolbar;
+
+import biz.freshcode.learn.gwt.client.IsRootContent;
+import biz.freshcode.learn.gwt.client.uispike.builder.BorderLayoutContainerBuilder;
+import biz.freshcode.learn.gwt.client.uispike.builder.ToolBarBuilder;
+import biz.freshcode.learn.gwt.client.uispike.builder.container.BoxLayoutDataBuilder;
+import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.button.ToolButton;
+import com.sencha.gxt.widget.core.client.container.SimpleContainer;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.toolbar.LabelToolItem;
+import com.sencha.gxt.widget.core.client.toolbar.ToolBar;
+
+public class ToolBarDemo extends AbstractIsWidget implements IsRootContent {
+    private LabelToolItem lbl;
+    private ToolBar toolBar;
+
+    @Override
+    protected Widget createWidget() {
+        return new BorderLayoutContainerBuilder()
+                .northWidget(toolBar = new ToolBarBuilder()
+                        .add(new SimpleContainer(), new BoxLayoutDataBuilder()
+                                .flex(1)
+                                .boxLayoutData)
+                        .add(new ToolButton(ToolButton.LEFT, new SelectEvent.SelectHandler() {
+                            @Override
+                            public void onSelect(SelectEvent event) {
+                                changeLabel("Small Label");
+                            }
+                        }))
+                        .add(lbl = new LabelToolItem("Starting label... that is very long"))
+                        .add(new ToolButton(ToolButton.RIGHT, new SelectEvent.SelectHandler() {
+                            @Override
+                            public void onSelect(SelectEvent event) {
+                                changeLabel("A very big label that will need resize");
+                            }
+                        }))
+                        .add(new SimpleContainer(), new BoxLayoutDataBuilder()
+                                .flex(1)
+                                .boxLayoutData)
+                        .toolBar)
+                .borderLayoutContainer;
+    }
+
+    private void changeLabel(String str) {
+        lbl.setLabel(str);
+        toolBar.forceLayout();
+    }
+}
