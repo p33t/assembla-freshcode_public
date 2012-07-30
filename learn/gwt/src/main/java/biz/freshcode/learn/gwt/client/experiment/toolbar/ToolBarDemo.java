9f8a8d41b5430d6daa173f2b4375726c3866508d
pwl <1179301+p33t@users.noreply.github.com>
2012-07-30 04:52:54 +0000

    [learn-gwt] Toolbar demo.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/toolbar/ToolBarDemo.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/toolbar/ToolBarDemo.java
@@ -3,13 +3,13 @@ package biz.freshcode.learn.gwt.client.experiment.toolbar;
 import biz.freshcode.learn.gwt.client.IsRootContent;
 import biz.freshcode.learn.gwt.client.uispike.builder.BorderLayoutContainerBuilder;
 import biz.freshcode.learn.gwt.client.uispike.builder.ToolBarBuilder;
-import biz.freshcode.learn.gwt.client.uispike.builder.container.BoxLayoutDataBuilder;
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.widget.core.client.button.ToolButton;
-import com.sencha.gxt.widget.core.client.container.SimpleContainer;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.toolbar.FillToolItem;
 import com.sencha.gxt.widget.core.client.toolbar.LabelToolItem;
+import com.sencha.gxt.widget.core.client.toolbar.SeparatorToolItem;
 import com.sencha.gxt.widget.core.client.toolbar.ToolBar;
 
 public class ToolBarDemo extends AbstractIsWidget implements IsRootContent {
@@ -20,25 +20,23 @@ public class ToolBarDemo extends AbstractIsWidget implements IsRootContent {
     protected Widget createWidget() {
         return new BorderLayoutContainerBuilder()
                 .northWidget(toolBar = new ToolBarBuilder()
-                        .add(new SimpleContainer(), new BoxLayoutDataBuilder()
-                                .flex(1)
-                                .boxLayoutData)
+                        .add(new FillToolItem())
                         .add(new ToolButton(ToolButton.LEFT, new SelectEvent.SelectHandler() {
                             @Override
                             public void onSelect(SelectEvent event) {
                                 changeLabel("Small Label");
                             }
                         }))
+                        .add(new SeparatorToolItem())
                         .add(lbl = new LabelToolItem("Starting label... that is very long"))
+                        .add(new SeparatorToolItem())
                         .add(new ToolButton(ToolButton.RIGHT, new SelectEvent.SelectHandler() {
                             @Override
                             public void onSelect(SelectEvent event) {
                                 changeLabel("A very big label that will need resize");
                             }
                         }))
-                        .add(new SimpleContainer(), new BoxLayoutDataBuilder()
-                                .flex(1)
-                                .boxLayoutData)
+                        .add(new FillToolItem())
                         .toolBar)
                 .borderLayoutContainer;
     }
