718735ba81e14b2c0feb8d9763b07abe2e68447a
pwl <1179301+p33t@users.noreply.github.com>
2012-08-01 01:48:49 +0000

    [learn-gwt] XTemplate

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/xtemplate/XTemplateDemo.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/xtemplate/XTemplateDemo.java
@@ -1,12 +1,22 @@
 package biz.freshcode.learn.gwt.client.experiment.xtemplate;
 
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.core.client.XTemplates;
 
 public class XTemplateDemo extends AbstractIsWidget {
+    Renderer renderer = GWT.create(Renderer.class);
+
     @Override
     protected Widget createWidget() {
-        return new HTMLPanel("<p>XTemplate</p>");
+        return new HTMLPanel(renderer.templateXml());
+    }
+
+    public interface Renderer extends XTemplates {
+        @XTemplates.XTemplate(source = "template.xml")
+        SafeHtml templateXml();
     }
 }
