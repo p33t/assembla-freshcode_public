98fd1227ff916f4540115afef62a3e3c22acedaf
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-08-01 02:17:00 +0000

    [learn-gwt] XTemplate

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/xtemplate/XTemplateDemo.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/xtemplate/XTemplateDemo.java
@@ -7,16 +7,44 @@ import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.core.client.XTemplates;
 
+import java.util.List;
+
+import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.newListFrom;
+
 public class XTemplateDemo extends AbstractIsWidget {
     Renderer renderer = GWT.create(Renderer.class);
 
     @Override
     protected Widget createWidget() {
-        return new HTMLPanel(renderer.templateXml());
+        Bean b = new Bean();
+        b.setStr("3 Numbers");
+        b.setStrings(newListFrom("One", "Two", "Three"));
+        return new HTMLPanel(renderer.templateXml(b));
+    }
+
+    public static class Bean {
+        private String str;
+        private List<String> strings;
+
+        public List<String> getStrings() {
+            return strings;
+        }
+
+        public void setStrings(List<String> strings) {
+            this.strings = strings;
+        }
+
+        public String getStr() {
+            return str;
+        }
+
+        public void setStr(String str) {
+            this.str = str;
+        }
     }
 
     public interface Renderer extends XTemplates {
         @XTemplates.XTemplate(source = "template.xml")
-        SafeHtml templateXml();
+        SafeHtml templateXml(Bean bean);
     }
 }
