9b0127399d19750e82c8571bfb362b285415b8fa
pwl <1179301+p33t@users.noreply.github.com>
2012-08-27 07:23:38 +0000

    [learn-gwt] Better popups.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/window/WindowDemo.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/window/WindowDemo.java
@@ -8,6 +8,7 @@ import biz.freshcode.learn.gwt.client.builder.gxt.WindowBuilder;
 import biz.freshcode.learn.gwt.client.builder.gxt.container.BorderLayoutContainerBuilder;
 import biz.freshcode.learn.gwt.client.builder.gxt.container.HorizontalLayoutContainerBuilder;
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
+import com.google.gwt.core.shared.GWT;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.widget.core.client.Dialog;
@@ -23,14 +24,14 @@ public class WindowDemo extends AbstractIsWidget implements IsRootContent {
     private TextButton btnDialog;
     private Dialog dialog = new DialogBuilder()
             .headingHtml("<p>Heading</p>")
-            .widget(new HTMLPanel("<p>This guy has buttons support... otherwise same as 'Window'</p>"))
-            .predefinedButtons(Dialog.PredefinedButton.CLOSE)
-            .hideOnButtonClick(true)
+            .widget(new HTMLPanel("<p>This guy has buttons support... otherwise same as 'Window'and<br/>some<br/>extra<br/>lines<br/>thanks</p>"))
+            .predefinedButtons()
+//            .hideOnButtonClick(true)
             .autoHide(true)
             .dialog;
     private Window window = new WindowBuilder()
             .headingHtml("<p>Heading</p>")
-            .widget(new HTMLPanel("<p>Window Contents</p>"))
+            .widget(new HTMLPanel("<p>Window Contentsand<br/>some<br/>extra<br/>lines<br/>thanks</p>"))
             .autoHide(true)
 //            .constrain(false)  ... does nothing
 //            .deferHeight(true) ... does nothing
@@ -38,7 +39,7 @@ public class WindowDemo extends AbstractIsWidget implements IsRootContent {
     private TextButton btnWindow;
     private TextButton btnPopup;
     private Popup popup = new PopupBuilder()
-            .widget(new HTMLPanel("<p>Popup has no heading</p>"))
+            .widget(new HTMLPanel("<p>Popup has no heading<br/>and<br/>some<br/>extra<br/>lines<br/>thanks</p>"))
             .borders(true)
 //            .animate(true) // causes auto hiding problems for this and 'dialog' (?!)
             .title("The Title")
@@ -53,18 +54,18 @@ public class WindowDemo extends AbstractIsWidget implements IsRootContent {
                         .addStyleName(STYLE.bgndTile())
                         .hTMLPanel)
                 .southWidget(new HorizontalLayoutContainerBuilder()
-                        .add(btnDialog = new TextButton("Show Dialog", new SelectEvent.SelectHandler() {
-                            @Override
-                            public void onSelect(SelectEvent event) {
-                                dialog.setPosition(btnDialog.getAbsoluteLeft(), btnDialog.getAbsoluteTop());
-                                dialog.show();
-                            }
-                        }))
                         .add(btnWindow = new TextButton("Show Window", new SelectEvent.SelectHandler() {
                             @Override
                             public void onSelect(SelectEvent event) {
-                                window.setPosition(btnWindow.getAbsoluteLeft(), btnWindow.getAbsoluteTop());
+                                position(window, btnWindow);
                                 window.show();
+                            }
+                        }))
+                        .add(btnDialog = new TextButton("Show Dialog", new SelectEvent.SelectHandler() {
+                            @Override
+                            public void onSelect(SelectEvent event) {
+                                position(dialog, btnDialog);
+                                dialog.show();
                             }
                         }))
                         .add(btnPopup = new TextButton("Show Popup", new SelectEvent.SelectHandler() {
@@ -75,6 +76,15 @@ public class WindowDemo extends AbstractIsWidget implements IsRootContent {
                         }))
                         .horizontalLayoutContainer, new BorderLayoutContainer.BorderLayoutData(30))
                 .borderLayoutContainer;
+    }
+
+    private void position(Window w, Widget orient) {
+        GWT.log("Showing at " + orient.getAbsoluteLeft() + "," + orient.getAbsoluteTop());
+        // This appears to only work the first time ?!
+        w.setPagePosition(orient.getAbsoluteLeft(), orient.getAbsoluteTop());
+//        Always 0,0 ?!
+//        XElement elem = w.getElement();
+//        GWT.log("Resulting position: " + elem.getAbsoluteLeft() + "," + elem.getAbsoluteTop());
     }
 
 }
