871091598f5e0806c50a786e38229d9e8bffb6f6
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2012-10-02 23:44:16 +0000

    [learn-squeryl] CustomType example.

--- learn_scala/squeryl/src/main/scala/pkg/db/AppSchema.scala
+++ learn_scala/squeryl/src/main/scala/pkg/db/AppSchema.scala
@@ -2,6 +2,9 @@ package pkg.db
 
 import org.squeryl.PrimitiveTypeMode._
 import org.squeryl.{Session, SessionFactory, Schema}
+import org.squeryl.customtypes.{StringField, CustomTypesMode, CustomType}
+
+//import CustomTypesMode._ ... only needed where 'statements are defined'.  Prevents compile otherwise.
 
 class T1(val id: Long,
          val name: String,
@@ -10,6 +13,14 @@ class T1(val id: Long,
    * No arg constructor is required when there are nullable fields.
    */
   def this() = this (0, "", Some(""))
+}
+
+// A Custom type.  It indirectly extends CustomType.
+class RevString(val str: String) extends StringField(str.reverse)
+
+// Uses a custom type
+class Custom(val id: Long, val cust: RevString) {
+  def this() = this(0, new RevString(""))
 }
 
 object AppSchema extends Schema {
@@ -26,4 +37,13 @@ object AppSchema extends Schema {
     t.name is (dbType("varchar(255)")),
     t.nicName is (dbType("varchar(32)"))
   ))
+
+  val custom = table[Custom]
+  
+  {
+    import CustomTypesMode._
+    on(custom)(t => declare(
+      t.cust is (dbType("varchar(127)"))
+    ))
+  }
 }
