52f8e93edb3b12633bbbe12f4a32812c5342b8c2
pwl <1179301+p33t@users.noreply.github.com>
2012-11-19 00:27:13 +0000

    [gwt] Another forms attempt.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms2/Forms2Demo.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms2/Forms2Demo.java
@@ -1,15 +1,15 @@
 package biz.freshcode.learn.gwt.client.experiment.forms2;
 
 import biz.freshcode.learn.gwt.client.builder.gxt.grid.ColumnConfigBuilder;
-import biz.freshcode.learn.gwt.client.builder.gxt.grid.ColumnModelBuilder;
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
-import biz.freshcode.learn.gwt.client.util.AppCollectionUtil;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.core.client.IdentityValueProvider;
 import com.sencha.gxt.data.shared.ListStore;
 import com.sencha.gxt.widget.core.client.form.TextField;
 import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
 import com.sencha.gxt.widget.core.client.grid.ColumnModel;
 import com.sencha.gxt.widget.core.client.grid.Grid;
+import com.sencha.gxt.widget.core.client.grid.RowNumberer;
 import com.sencha.gxt.widget.core.client.grid.editing.GridInlineEditing;
 
 import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.newListFrom;
@@ -17,23 +17,30 @@ import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.newListFrom;
 public class Forms2Demo extends AbstractIsWidget {
 
     ListStore<TwoBean> store = new ListStore<TwoBean>(TwoBean.ACCESS.id());
-    
+
     @Override
     protected Widget createWidget() {
         store.setAutoCommit(true);
         store.add(new TwoBean());
         store.add(new TwoBean());
 
-        ColumnConfig<TwoBean, String> c0;
+        ColumnConfig<TwoBean, String> editCol;
 
-        Grid<TwoBean> grid = new Grid<TwoBean>(store, new ColumnModel<TwoBean>(AppCollectionUtil.<ColumnConfig<TwoBean, ?>>newListFrom(
-                c0 = new ColumnConfigBuilder<TwoBean, String>(TwoBean.ACCESS.revStr())
+        RowNumberer<TwoBean> nums;
+        Grid<TwoBean> grid = new Grid<TwoBean>(store, new ColumnModel<TwoBean>(newListFrom(
+                nums = new RowNumberer<TwoBean>(new IdentityValueProvider<TwoBean>()),
+                new ColumnConfigBuilder<TwoBean, String>(TwoBean.ACCESS.idValue())
+                        .header("Id")
+                        .columnConfig,
+                editCol = new ColumnConfigBuilder<TwoBean, String>(TwoBean.ACCESS.revStr())
                         .header("Rev Str")
                         .columnConfig
-        )));
+                )));
+
+        nums.initPlugin(grid); // needed according to doco
 
         // editing
-        new GridInlineEditing<TwoBean>(grid).addEditor(c0, new TextField());
+        new GridInlineEditing<TwoBean>(grid).addEditor(editCol, new TextField());
 
         return grid;
 
--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms2/TwoBean.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms2/TwoBean.java
@@ -1,6 +1,7 @@
 package biz.freshcode.learn.gwt.client.experiment.forms2;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.editor.client.Editor;
 import com.sencha.gxt.core.client.ValueProvider;
 import com.sencha.gxt.data.shared.ModelKeyProvider;
 import com.sencha.gxt.data.shared.PropertyAccess;
@@ -17,6 +18,8 @@ public class TwoBean {
     static interface Access extends PropertyAccess<TwoBean> {
         ModelKeyProvider<TwoBean> id();
         ValueProvider<TwoBean, String> revStr();
+        @Editor.Path("id")
+        ValueProvider<TwoBean, String> idValue();
     }
     
     public String getRevStr() {
