6d3db9a904e462be98abb43eaa53af40b4f3ecea
pwl <1179301+p33t@users.noreply.github.com>
2012-12-05 06:50:43 +0000

    [gwt] Minor refactor...enable aggressive SSL key generation (more convenient).

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/jsni/ServerTimeUtil.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/jsni/ServerTimeUtil.java
@@ -2,7 +2,8 @@ package biz.freshcode.learn.gwt.client.experiment.jsni;
 
 public class ServerTimeUtil {
     public static native int serverTimeDiff() /*-{
-        if ("number" == typeof $wnd.serverTimeDiff) return $wnd.serverTimeDiff;
+        var diff = $wnd.serverTimeDiff;
+        if ("number" == typeof diff) return diff;
         return 1234; // testing value
     }-*/;
 }
