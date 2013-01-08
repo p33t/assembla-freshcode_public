587796bd94e9e213667df277a4b3d358e8eabba2
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2013-01-08 00:39:41 +0000

    [gwt] Dynamic Layout Demo (WIP)

---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/dynamiclayout/DynamicLayoutDemo.java
@@ -0,0 +1,72 @@
+package biz.freshcode.learn.gwt.client.experiment.dynamiclayout;
+
+import biz.freshcode.learn.gwt.client.builder.gxt.WindowBuilder;
+import biz.freshcode.learn.gwt.client.builder.gxt.container.FlowLayoutContainerBuilder;
+import biz.freshcode.learn.gwt.client.builder.gxt.container.VBoxLayoutContainerBuilder;
+import biz.freshcode.learn.gwt.client.builder.gxt.form.TextAreaBuilder;
+import biz.freshcode.learn.gwt.client.uispike.builder.container.BoxLayoutDataBuilder;
+import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.Window;
+import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.container.HtmlLayoutContainer;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.info.Info;
+
+import static com.sencha.gxt.core.client.dom.ScrollSupport.ScrollMode.AUTOY;
+import static com.sencha.gxt.widget.core.client.container.VBoxLayoutContainer.VBoxLayoutAlign.STRETCH;
+
+public class DynamicLayoutDemo extends AbstractIsWidget {
+    @Override
+    protected Widget createWidget() {
+        return new TextButton("Popup", new SelectEvent.SelectHandler() {
+            @Override
+            public void onSelect(SelectEvent event) {
+                popup();
+            }
+        });
+    }
+
+    private void popup() {
+        Window window = new WindowBuilder()
+                .headingText("Dynamic Layout Demo")
+                .widget(new VBoxLayoutContainerBuilder()
+                        .vBoxLayoutAlign(STRETCH)
+                        .add(new FlowLayoutContainerBuilder()
+                                .scrollMode(AUTOY)
+                                .add(new HtmlLayoutContainer("[None]"))
+                                .flowLayoutContainer)
+                        .add(new TextAreaBuilder()
+                                .text("Add / Remove messages and note how window behaves\nBonus line")
+                                .textArea,
+                                new BoxLayoutDataBuilder()
+                                        .flex(1)
+                                        .boxLayoutData)
+                        .vBoxLayoutContainer)
+                .width(300)
+                .height(200)
+                .addButton(new TextButton("Add Msg", new SelectEvent.SelectHandler() {
+                    @Override
+                    public void onSelect(SelectEvent event) {
+                        addMsg();
+                    }
+                }))
+                .addButton(new TextButton("Clear Msgs", new SelectEvent.SelectHandler() {
+                    @Override
+                    public void onSelect(SelectEvent event) {
+                        clearMsgs();
+                    }
+                }))
+                .window;
+        window.show();
+
+    }
+
+    private void clearMsgs() {
+        Info.display("Clear Msg", "x");
+    }
+
+    private void addMsg() {
+        Info.display("Add Msg", "x");
+    }
+}
