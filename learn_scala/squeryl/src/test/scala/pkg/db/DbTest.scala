b26ec7531a1ac8340564977d0f3b9fde1781ea8f
pwl <1179301+p33t@users.noreply.github.com>
2013-02-18 19:29:25 +0000

    [learn-squeryl] JUnit runner.

--- learn_scala/squeryl/src/test/scala/pkg/db/DbTest.scala
+++ learn_scala/squeryl/src/test/scala/pkg/db/DbTest.scala
@@ -1,11 +1,13 @@
 package pkg.db
 
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.Suite
 
+@RunWith(classOf[JUnitRunner])
+class DbTest extends Suite {
 
-class DbTest {
-
-  @Test
   def testConnect() {
     pkg.db.Db.withConnection {
       _ => ()
