f9700f07edef347d086dff756126985df265e3e7
pwl <1179301+p33t@users.noreply.github.com>
2013-03-05 23:40:34 +0000

    [gwt] Flex table / landing page example.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/flextable/FlexTableDemo.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/flextable/FlexTableDemo.java
@@ -1,13 +1,18 @@
 package biz.freshcode.learn.gwt.client.experiment.flextable;
 
-import biz.freshcode.learn.gwt.client.builder.gwt.FlexTableBuilder;
+import biz.freshcode.learn.gwt.client.uispike.builder.TextButtonBuilder;
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.cell.core.client.ButtonCell;
 import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.container.BoxLayoutContainer;
 import com.sencha.gxt.widget.core.client.container.VBoxLayoutContainer;
-import com.sencha.gxt.widget.core.client.toolbar.ToolBar;
+
+import static biz.freshcode.learn.gwt.client.experiment.flextable.Bundle.BUNDLE;
+import static biz.freshcode.learn.gwt.client.experiment.flextable.Bundle.STYLE;
+import static com.sencha.gxt.cell.core.client.ButtonCell.IconAlign;
 
 
 /**
@@ -17,29 +22,27 @@ import com.sencha.gxt.widget.core.client.toolbar.ToolBar;
 public class FlexTableDemo extends AbstractIsWidget {
     @Override
     protected Widget createWidget() {
-        FlexTable tbl = new FlexTableBuilder()
-                .widget(0, 0, btn())
-                .widget(1, 1, btn())
-                .widget(1, 2, btn())
-                .widget(2, 1, btn())
-                .widget(2, 2, btn())
-                .flexTable;
-
-        ToolBar tb = new ToolBar();
-        tb.setPixelSize(300, 300);
-        tb.add(tbl);
+        FlexTable tbl = new FlexTable();
+        tbl.setCellSpacing(50);
+        button(tbl, 0, 0, "Zero");
+        button(tbl, 2, 1, "Longish");
+        button(tbl, 2, 2, "Two<br/>Two");
 
         VBoxLayoutContainer c = new VBoxLayoutContainer();
         c.setVBoxLayoutAlign(VBoxLayoutContainer.VBoxLayoutAlign.CENTER);
         c.setPack(BoxLayoutContainer.BoxLayoutPack.CENTER);
-        c.add(tb);
+        c.add(tbl);
         return c;
     }
 
-    private TextButton btn() {
-        TextButton b = new TextButton("Hello");
-        b.setWidth(50);
-        b.setHeight(50);
-        return b;
+    private void button(FlexTable flexTable, int row, int col, String html) {
+        TextButton b = new TextButtonBuilder()
+                .hTML(html)
+                .icon(BUNDLE.iconPng())
+                .addStyleName(STYLE.discreteButton())
+                .iconAlign(IconAlign.TOP)
+                .textButton;
+        flexTable.setWidget(row, col, b);
+        flexTable.getFlexCellFormatter().setHorizontalAlignment(row, col, HasHorizontalAlignment.ALIGN_CENTER);
     }
 }
