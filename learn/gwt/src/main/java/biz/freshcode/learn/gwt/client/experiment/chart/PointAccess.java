c854c57b3c20f9807205f802ee0902446d0a4db2
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2013-04-09 07:22:49 +0000

    [gwt] Chart: Direct Draw Chart attempt.

---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/chart/PointAccess.java
@@ -0,0 +1,14 @@
+package biz.freshcode.learn.gwt.client.experiment.chart;
+
+import com.google.gwt.core.client.GWT;
+import com.sencha.gxt.core.client.ValueProvider;
+import com.sencha.gxt.core.client.util.Point;
+import com.sencha.gxt.data.shared.PropertyAccess;
+
+public interface PointAccess extends PropertyAccess<Point> {
+    PointAccess ACCESS = GWT.create(PointAccess.class);
+
+    ValueProvider<Point, Integer> x();
+
+    ValueProvider<Point, Integer> y();
+}
