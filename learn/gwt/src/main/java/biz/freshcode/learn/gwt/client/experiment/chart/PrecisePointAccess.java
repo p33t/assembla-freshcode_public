342475c1681fbdc262b6aa2c8419c14a8efc5486
pwl <1179301+p33t@users.noreply.github.com>
2013-04-10 00:40:53 +0000

    [gwt] Chart: Migrate XyBean to PrecisePoint.

---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/chart/PrecisePointAccess.java
@@ -0,0 +1,15 @@
+package biz.freshcode.learn.gwt.client.experiment.chart;
+
+import com.google.gwt.core.client.GWT;
+import com.sencha.gxt.core.client.ValueProvider;
+import com.sencha.gxt.core.client.util.Point;
+import com.sencha.gxt.core.client.util.PrecisePoint;
+import com.sencha.gxt.data.shared.PropertyAccess;
+
+public interface PrecisePointAccess extends PropertyAccess<PrecisePoint> {
+    PrecisePointAccess ACCESS = GWT.create(PrecisePointAccess.class);
+
+    ValueProvider<PrecisePoint, Double> x();
+
+    ValueProvider<PrecisePoint, Double> y();
+}
