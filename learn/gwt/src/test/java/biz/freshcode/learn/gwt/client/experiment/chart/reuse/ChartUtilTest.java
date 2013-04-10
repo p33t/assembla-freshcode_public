0024d55af5da5664748513b6aedbe9402f966150
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2013-04-10 04:58:47 +0000

    [gwt] Chart: ChartUtil.interpolate() for -ve y values.

--- learn/gwt/src/test/java/biz/freshcode/learn/gwt/client/experiment/chart/reuse/ChartUtilTest.java
+++ learn/gwt/src/test/java/biz/freshcode/learn/gwt/client/experiment/chart/reuse/ChartUtilTest.java
@@ -59,4 +59,38 @@ public class ChartUtilTest extends TestCase {
 
         assertEquals(expected, ChartUtil.interpolate(seriesMap));
     }
+
+    public void testInterpolateNegative() {
+        List<PrecisePoint> straight = newListFrom(
+                new PrecisePoint(4, -8),
+                new PrecisePoint(8, -8)
+        );
+
+        List<PrecisePoint> angle = newListFrom(
+                new PrecisePoint(2, -4),
+                new PrecisePoint(6, -4),
+                new PrecisePoint(10, 0)
+        );
+        Map<String, List<PrecisePoint>> seriesMap = newMap();
+        seriesMap.put(STRAIGHT, straight);
+        seriesMap.put(ANGLE, angle);
+
+        List<ChartElem> expected = newListFrom(
+                new ChartElem(2)
+                        .addY(ANGLE, -4),
+                new ChartElem(4)
+                        .addY(ANGLE, -4)
+                        .addY(STRAIGHT, -8),
+                new ChartElem(6)
+                        .addY(ANGLE, -4)
+                        .addY(STRAIGHT, -8),
+                new ChartElem(8)
+                        .addY(ANGLE, -2)
+                        .addY(STRAIGHT, -8),
+                new ChartElem(10)
+                        .addY(ANGLE, 0)
+        );
+
+        assertEquals(expected, ChartUtil.interpolate(seriesMap));
+    }
 }
