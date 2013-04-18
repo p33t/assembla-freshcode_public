684b76e803ea7b952ec9644db009fa06d66de3cd
pwl <1179301+p33t@users.noreply.github.com>
2013-04-18 06:15:43 +0000

    [gwt] Trigger Field: Getting started.

---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/triggerfield/TriggerFieldDemo.java
@@ -0,0 +1,20 @@
+package biz.freshcode.learn.gwt.client.experiment.triggerfield;
+
+import biz.freshcode.learn.gwt.client.builder.gxt.container.VerticalLayoutContainerBuilder;
+import biz.freshcode.learn.gwt.client.uispike.builder.field.FieldLabelBuilder;
+import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.form.DateField;
+
+public class TriggerFieldDemo extends AbstractIsWidget {
+
+    @Override
+    protected Widget createWidget() {
+        return new VerticalLayoutContainerBuilder()
+                .add(new FieldLabelBuilder()
+                        .text("Date Field")
+                        .add(new DateField())
+                        .fieldLabel)
+                .verticalLayoutContainer;
+    }
+}
