b5e431d23757018523bb4ad771384d7ee580e21c
pwl <1179301+p33t@users.noreply.github.com>
2013-04-24 01:14:48 +0000

    [gwt] Tree widget selection.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/tree/TreeDemo.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/tree/TreeDemo.java
@@ -8,6 +8,8 @@ import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.core.client.IdentityValueProvider;
 import com.sencha.gxt.data.shared.TreeStore;
 import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer;
+import com.sencha.gxt.widget.core.client.info.Info;
+import com.sencha.gxt.widget.core.client.selection.SelectionChangedEvent;
 import com.sencha.gxt.widget.core.client.tree.Tree;
 
 import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.newListFrom;
@@ -15,18 +17,29 @@ import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.newListFrom;
 public class TreeDemo extends AbstractIsWidget {
     @Override
     protected Widget createWidget() {
+        Tree<String, String> tree;
+        BorderLayoutContainer blc = new BorderLayoutContainerBuilder()
+                .westWidget(new ContentPanelBuilder()
+                        .headingText("My Tree")
+                        .widget(tree = createTree())
+                        .contentPanel, new BorderLayoutContainer.BorderLayoutData(1 / 3.0))
+                .borderLayoutContainer;
+        tree.getSelectionModel().addSelectionChangedHandler(new SelectionChangedEvent.SelectionChangedHandler<String>() {
+            @Override
+            public void onSelectionChanged(SelectionChangedEvent<String> event) {
+                Info.display("Selection", event.getSelection().toString());
+            }
+        });
+        return blc;
+
+    }
+
+    private Tree<String, String> createTree() {
         TreeStore<String> store = new TreeStore<String>(new IdentityHashProvider<String>());
         store.add(newListFrom("p1", "p2"));
         store.add("p1", newListFrom("c11", "c12", "c13"));
         store.add("p2", newListFrom("c21", "c22", "c23"));
         IdentityValueProvider<String> stringProvide = new IdentityValueProvider<String>();
-        Tree<String, String> tree = new Tree<String, String>(store, stringProvide);
-
-        return new BorderLayoutContainerBuilder()
-                .westWidget(new ContentPanelBuilder()
-                        .headingText("My Tree")
-                        .widget(tree)
-                        .contentPanel, new BorderLayoutContainer.BorderLayoutData(1 / 3.0))
-                .borderLayoutContainer;
+        return new Tree<String, String>(store, stringProvide);
     }
 }
