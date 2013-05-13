6a00588754174844442b45d40ab28accf11cc70e
pwl <1179301+p33t@users.noreply.github.com>
2013-05-13 03:46:16 +0000

    [learn-gwtp] Using custom entry point.

--- learn/gwtp/learn-gwtp/src/main/java/biz/freshcode/learn/gwtp/client/boot/EntryPoint.java
+++ learn/gwtp/learn-gwtp/src/main/java/biz/freshcode/learn/gwtp/client/boot/EntryPoint.java
@@ -1,37 +1,16 @@
 package biz.freshcode.learn.gwtp.client.boot;
 
-import biz.freshcode.learn.gwtp.shared.dispatch.SdAction;
-import biz.freshcode.learn.gwtp.shared.dispatch.StrResult;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.widget.client.TextButton;
-import com.gwtplatform.dispatch.shared.DispatchAsync;
 import com.gwtplatform.mvp.client.ApplicationController;
-import com.gwtplatform.mvp.client.DelayedBindRegistry;
-import com.gwtplatform.mvp.client.proxy.PlaceManager;
 
 public class EntryPoint implements com.google.gwt.core.client.EntryPoint {
-    //    public final ClientGinjector ginjector = GWT.create(ClientGinjector.class);
-    private HTML fb;
-    private DispatchAsync dispatch;
+//    private HTML fb;
+//    private DispatchAsync dispatch;
 
     @Override
     public void onModuleLoad() {
-//        Not used in puzzzlebazaar...
-//        ApplicationController c = GWT.create(ApplicationController.class);
-//        c.init();
-
-        AppInjector injector = GWT.create(AppInjector.class);
-        DelayedBindRegistry.bind(injector);
-
-
-//        dispatch = injector.getDispatch();
-        PlaceManager mgr = injector.getPlaceManager();
-        mgr.revealDefaultPlace();
+        ApplicationController c = GWT.create(ApplicationController.class);
+        c.init();
 
 //        RootPanel root = RootPanel.get();
 //        TextButton btn;
@@ -45,23 +24,23 @@ public class EntryPoint implements com.google.gwt.core.client.EntryPoint {
 //        root.add(fb = new HTML("<p>It loaded</p>"));
 
     }
-
-    private void go() {
-        feedback("Calling RPC");
-        dispatch.execute(new SdAction("Bruce"), new AsyncCallback<StrResult>() {
-            @Override
-            public void onFailure(Throwable caught) {
-                GWT.log("No can do: " + caught);
-            }
-
-            @Override
-            public void onSuccess(StrResult result) {
-                feedback("Sent 'Bruce'<br/>Result '" + result.getStr() + "'");
-            }
-        });
-    }
-
-    private void feedback(String msg) {
-        fb.setHTML(fb.getHTML() + "<p>" + msg + "</p>");
-    }
+//
+//    private void go() {
+//        feedback("Calling RPC");
+//        dispatch.execute(new SdAction("Bruce"), new AsyncCallback<StrResult>() {
+//            @Override
+//            public void onFailure(Throwable caught) {
+//                GWT.log("No can do: " + caught);
+//            }
+//
+//            @Override
+//            public void onSuccess(StrResult result) {
+//                feedback("Sent 'Bruce'<br/>Result '" + result.getStr() + "'");
+//            }
+//        });
+//    }
+//
+//    private void feedback(String msg) {
+//        fb.setHTML(fb.getHTML() + "<p>" + msg + "</p>");
+//    }
 }
