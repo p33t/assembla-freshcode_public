43dfbd644737dee025b1f57ccd65225790ae5d7a
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2013-05-15 01:37:30 +0000

    [learn-gwtp] Local boot time constants.

---
+++ learn/gwtp/src/main/java/biz/freshcode/learn/gwtp/client/boot/LocalBootTime.java
@@ -0,0 +1,14 @@
+package biz.freshcode.learn.gwtp.client.boot;
+
+import com.google.inject.BindingAnnotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@BindingAnnotation
+public @interface LocalBootTime {
+}
