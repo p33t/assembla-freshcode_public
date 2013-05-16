9037e9cb6d847f69c08ef850df2ec417b1eed395
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2013-05-16 03:54:48 +0000

    [learn-gwtp] Introduce Builder generation and a footer.

---
+++ learn/gwtp/src/test/java/pkg/BeanBuilderGenerator.java
@@ -0,0 +1,39 @@
+package pkg;
+
+import biz.freshcode.b_generation.BeanBuilderWriter;
+import biz.freshcode.b_generation.BgenUtil;
+import biz.freshcode.b_generation.DefaultBeanBuilderGenerator;
+import biz.freshcode.learn.gwtp.client.builder.BeanBuilder;
+import biz.freshcode.learn.gwtp.client.builder.Construct;
+import com.sencha.gxt.widget.core.client.toolbar.ToolBar;
+
+public class BeanBuilderGenerator extends DefaultBeanBuilderGenerator {
+    static final Class CLASS = ToolBar.class; //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    public static final String BASE_PKG = "biz.freshcode.learn.gwtp.client.builder";
+
+
+    public static void main(String[] args) {
+        new BeanBuilderWriter()
+                .addMapping("com.sencha.gxt.widget.core.client", BASE_PKG + ".gxt")
+                .addMapping("com.sencha.gxt.chart.client", BASE_PKG + ".gxt")
+                .addMapping("com.sencha.gxt.core.client", BASE_PKG + ".gxt")
+                .addMapping("com.google.gwt.user.client.ui", BASE_PKG + ".gwt")
+                .generator(new BeanBuilderGenerator())
+                .write(CLASS);
+    }
+
+    @Override
+    protected String getBuilderAnnotation(Class beanClass) {
+        return "@SuppressWarnings(\"UnusedDeclaration\")\n" +
+                "@" + BeanBuilder.class.getName() + "(" + beanClass.getSimpleName() + ".class)";
+    }
+
+    /**
+     * Add the custom parent class for the builder.
+     */
+    @Override
+    protected String getBuilderParent(Class beanCls, String builderType) {
+        String parentName = BgenUtil.className(Construct.Parent.class);
+        return parentName + "<" + builderType + ">";
+    }
+}
