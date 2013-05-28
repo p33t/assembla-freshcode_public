7c9e4688abed1957993d0d8b84926d898d318a9d
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2013-05-28 02:32:32 +0000

    [gwt] Refactor for reuse.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/busy/BusyDemo.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/busy/BusyDemo.java
@@ -3,8 +3,6 @@ package biz.freshcode.learn.gwt.client.experiment.busy;
 import biz.freshcode.learn.gwt.client.builder.gxt.container.VerticalLayoutContainerBuilder;
 import biz.freshcode.learn.gwt.client.uispike.builder.ContentPanelBuilder;
 import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
-import com.google.gwt.core.shared.GWT;
-import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.widget.core.client.ContentPanel;
@@ -12,7 +10,8 @@ import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.button.ToolButton;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 
-import static biz.freshcode.learn.gwt.client.experiment.busy.Bundle.STYLE;
+import static biz.freshcode.learn.gwt.client.experiment.busy.reuse.BusyUtil.clearBusy;
+import static biz.freshcode.learn.gwt.client.experiment.busy.reuse.BusyUtil.markBusy;
 
 public class BusyDemo extends AbstractIsWidget {
     private ContentPanel pnlTarget;
@@ -23,14 +22,14 @@ public class BusyDemo extends AbstractIsWidget {
                 .add(new TextButton("Start Busy", new SelectEvent.SelectHandler() {
                     @Override
                     public void onSelect(SelectEvent event) {
-                        start();
+                        markBusy(pnlTarget);
                     }
                 }))
                 .add(space())
                 .add(new TextButton("Reset", new SelectEvent.SelectHandler() {
                     @Override
                     public void onSelect(SelectEvent event) {
-                        reset();
+                        clearBusy(pnlTarget);
                     }
                 }))
                 .add(space())
@@ -45,28 +44,4 @@ public class BusyDemo extends AbstractIsWidget {
         return new HTML("<p>&nbsp;</p>");
     }
 
-    private void reset() {
-        pnlTarget.enable();
-        if (!ensureNotBusy()) {
-            GWT.log("No busy spinner to remove");
-        }
-// By id...   DOM.getElementById("busySpinner").removeFromParent();
-    }
-
-    private boolean ensureNotBusy() {
-        Element first = pnlTarget.getElement().getFirstChildElement();
-        if (first != null && first.removeClassName(STYLE.busySpinner())) {
-            first.removeFromParent();
-            return true;
-        }
-        return false;
-    }
-
-    private void start() {
-        pnlTarget.disable();
-        ensureNotBusy();
-        pnlTarget.getElement().insertFirst("<div" +
-                " class='" + STYLE.busySpinner() + "'/>"
-        );
-    }
 }
