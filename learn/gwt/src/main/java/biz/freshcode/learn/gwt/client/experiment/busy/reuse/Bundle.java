28fe5627ac564ed15f4b99b0c323c31a0d15b88f
pwl <1179301+p33t@users.noreply.github.com>
2013-05-28 02:32:32 +0000

    [gwt] Refactor for reuse.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/busy/reuse/Bundle.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/busy/reuse/Bundle.java
@@ -1,4 +1,4 @@
-package biz.freshcode.learn.gwt.client.experiment.busy;
+package biz.freshcode.learn.gwt.client.experiment.busy.reuse;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
