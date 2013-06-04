defbee7dce4b48e784effac89a9a9fb987c637ea
pwl <1179301+p33t@users.noreply.github.com>
2013-06-04 00:13:00 +0000

    [learn-gwtp] Comment regarding generic 'Result' wrappers.

--- learn/gwtp/src/main/java/biz/freshcode/learn/gwtp/shared/dispatch/StrResult.java
+++ learn/gwtp/src/main/java/biz/freshcode/learn/gwtp/shared/dispatch/StrResult.java
@@ -2,6 +2,11 @@ package biz.freshcode.learn.gwtp.shared.dispatch;
 
 import com.gwtplatform.dispatch.shared.Result;
 
+/**
+ * NOTE: It might seem like we need generic wrappers to weave in 'Result'
+ * but that would cause pretty bad Serial'n policy bloat.
+ * Also, SimpleResult is not useful because it cannot be Serialized and hopefully will be deprecated.
+ */
 public class StrResult implements Result {
     private String str;
 
