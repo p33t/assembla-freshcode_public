ba92e3c7a0bd9886527b03263a5f855800282ddf
pwl <1179301+p33t@users.noreply.github.com>
2013-06-11 06:06:00 +0000

    [gwt] Forms3 - Validation bug illustration.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms3/PersonFormFields.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms3/PersonFormFields.java
@@ -1,10 +1,16 @@
 package biz.freshcode.learn.gwt.client.experiment.forms3;
 
+import biz.freshcode.learn.gwt.client.builder.gxt.form.TextFieldBuilder;
 import com.google.gwt.editor.client.Editor;
+import com.sencha.gxt.widget.core.client.form.TextField;
+import com.sencha.gxt.widget.core.client.form.validator.MinLengthValidator;
 
 /**
  * Fields for editing a Person.
  */
 public class PersonFormFields implements Editor<Person> {
+    TextField name = new TextFieldBuilder()
+            .addValidator(new MinLengthValidator(3))
+            .textField;
     PasswordEditor newPassword = new PasswordEditor();
 }
