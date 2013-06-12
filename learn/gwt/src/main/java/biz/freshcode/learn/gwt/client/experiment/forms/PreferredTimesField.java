e17e6e7d805da2d332d9e8a66324b33bb72607f5
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2013-06-12 01:38:46 +0000

    [gwt] Forms - Switch to CheckBoxCell from GridInlineEditing.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/PreferredTimesField.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/forms/PreferredTimesField.java
@@ -6,30 +6,24 @@ import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.sencha.gxt.cell.core.client.form.CheckBoxCell;
 import com.sencha.gxt.data.shared.ListStore;
-import com.sencha.gxt.data.shared.event.StoreRecordChangeEvent;
 import com.sencha.gxt.data.shared.event.StoreUpdateEvent;
-import com.sencha.gxt.widget.core.client.event.CompleteEditEvent;
 import com.sencha.gxt.widget.core.client.form.AdapterField;
-import com.sencha.gxt.widget.core.client.form.CheckBox;
 import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
 import com.sencha.gxt.widget.core.client.grid.ColumnModel;
 import com.sencha.gxt.widget.core.client.grid.Grid;
-import com.sencha.gxt.widget.core.client.grid.editing.GridEditing;
-import com.sencha.gxt.widget.core.client.grid.editing.GridInlineEditing;
 
 import java.util.List;
 import java.util.Set;
 
-import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.newList;
-import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.newListFrom;
-import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.newSet;
+import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.*;
 
 public class PreferredTimesField extends AdapterField<Set<AmPm>> implements HasValueChangeHandlers<Set<AmPm>> {
     private final ListStore<AmPmFlag> store;
     private Set<AmPm> publishedValue;
 
-    private static GridEditing<AmPmFlag> createGridEtc() {
+    private static Grid<AmPmFlag> createGridEtc() {
         // no need to populate... set value will populate
         ListStore<AmPmFlag> store = new ListStore<AmPmFlag>(AmPmFlag.ACCESS.id());
         store.setAutoCommit(true);
@@ -47,45 +41,37 @@ public class PreferredTimesField extends AdapterField<Set<AmPm>> implements HasV
                         .columnConfig
         )));
 
-        GridInlineEditing<AmPmFlag> editing = new GridInlineEditing<AmPmFlag>(grid);
-        editing.addEditor(colPref, new CheckBox());
-
-//        Interferes with editing process...
-//        CheckBoxCell cbc = new CheckBoxCell();
-//        colPref.setCell(cbc);
+        // NOTE: This will edit the datasource directly without GridInlineEditing.
+        // Its behaviour is nicer too.
+        CheckBoxCell cbc = new CheckBoxCell();
+        colPref.setCell(cbc);
 
         grid.getView().setForceFit(true);
-        return editing;
+        return grid;
     }
 
     public PreferredTimesField() {
         this(createGridEtc());
     }
 
-    private PreferredTimesField(GridEditing<AmPmFlag> ge) {
-        super(ge.getEditableGrid());
-        this.store = ge.getEditableGrid().getStore();
+    private PreferredTimesField(Grid<AmPmFlag> grid) {
+        super(grid);
+        this.store = grid.getStore();
         publishedValue = getValue();
 
-        ge.addCompleteEditHandler(new CompleteEditEvent.CompleteEditHandler<AmPmFlag>() {
-            @Override
-            public void onCompleteEdit(CompleteEditEvent<AmPmFlag> event) {
-                GWT.log("Edit complete");
-                setPublishedValue(getValue());
-            }
-        });
-
-        store.addStoreRecordChangeHandler(new StoreRecordChangeEvent.StoreRecordChangeHandler<AmPmFlag>() {
-            @Override
-            public void onRecordChange(StoreRecordChangeEvent<AmPmFlag> event) {
-                GWT.log("Record changed: " + event.getRecord().getModel());
-            }
-        });
+//  Does nothing.
+//        store.addStoreRecordChangeHandler(new StoreRecordChangeEvent.StoreRecordChangeHandler<AmPmFlag>() {
+//            @Override
+//            public void onRecordChange(StoreRecordChangeEvent<AmPmFlag> event) {
+//                GWT.log("Record changed: " + event.getRecord().getModel());
+//            }
+//        });
 
         store.addStoreUpdateHandler(new StoreUpdateEvent.StoreUpdateHandler<AmPmFlag>() {
             @Override
             public void onUpdate(StoreUpdateEvent<AmPmFlag> event) {
                 GWT.log("Store Update: " + event.getItems());
+                setPublishedValue(getValue());
             }
         });
     }
