ecee5f203a03c5edd67181dcdcd2a8e124e08f2c
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2013-06-24 23:36:59 +0000

    [scala-testing2] Comment.

--- learn_scala/testing2/src/test/scala/pkg/basic/BasicTest.scala
+++ learn_scala/testing2/src/test/scala/pkg/basic/BasicTest.scala
@@ -11,11 +11,14 @@ class BasicTest extends FunSuite {
   test("test infrastructure working") {
     val stack = new mutable.Stack[Int]
 
+    // Exception interception
     val thrown = intercept[NoSuchElementException] {
       stack.pop()
     }
     assert(thrown.getMessage === "head of empty list")
 
+    // simple assertion
+    // NOTE: the '===' function returns an Option[String]
     stack.push(3)
     assert(stack.pop === 3)
   }
