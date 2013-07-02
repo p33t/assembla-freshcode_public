5f485bbb3a3c7e383e69e90b410e6e398d9740c8
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2013-07-02 02:00:32 +0000

    [learn-gwtp] Refactor.

--- learn/gwtp/src/main/java/biz/freshcode/learn/gwtp/client/popup/GwtPopup.java
+++ learn/gwtp/src/main/java/biz/freshcode/learn/gwtp/client/popup/GwtPopup.java
@@ -1,13 +1,28 @@
 package biz.freshcode.learn.gwtp.client.popup;
 
+import biz.freshcode.learn.gwtp.client.builder.gwt.PopupPanelBuilder;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
+import com.gwtplatform.mvp.client.PopupViewImpl;
 import com.gwtplatform.mvp.client.PresenterWidget;
 
-public class GwtPopup extends PresenterWidget<GxtPopupView> {
+public class GwtPopup extends PresenterWidget<GwtPopup.View> {
 
     @Inject
-    public GwtPopup(EventBus eventBus, GxtPopupView view) {
+    public GwtPopup(EventBus eventBus, View view) {
         super(eventBus, view);
+    }
+
+    public static class View extends PopupViewImpl {
+        @Inject
+        public View(EventBus eventBus) {
+            super(eventBus);
+            initWidget(new PopupPanelBuilder()
+                    .autoHideEnabled(true)
+    //                .modal(true)
+                    .add(new HTML("<p>Yay!</p>"))
+                    .popupPanel);
+        }
     }
 }
