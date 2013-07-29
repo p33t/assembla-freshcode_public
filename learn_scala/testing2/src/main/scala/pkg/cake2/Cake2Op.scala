44b1d38d0d3e9ca31cc2789b1d0e94786fde3d21
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2013-07-29 01:21:10 +0000

    [scala-testing2] Comment.

--- learn_scala/testing2/src/main/scala/pkg/cake2/Cake2Op.scala
+++ learn_scala/testing2/src/main/scala/pkg/cake2/Cake2Op.scala
@@ -3,6 +3,7 @@ package pkg.cake2
 
 object Cake2Op {
   // this one is easily unit tested
+  // Note that all out-of-band args are dynamic
   def apply(outOfBand: => String)(directArg: String) = {
     outOfBand + "_" + directArg
   }
