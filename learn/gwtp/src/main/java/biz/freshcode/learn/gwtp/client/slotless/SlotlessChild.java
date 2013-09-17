7b33014d9de043bab9588ab877fc4e267c5d5ce9
pwl <1179301+p33t@users.noreply.github.com>
2013-09-17 03:42:47 +0000

    [learn-gwtp] 'slotless' demo.

---
+++ learn/gwtp/src/main/java/biz/freshcode/learn/gwtp/client/slotless/SlotlessChild.java
@@ -0,0 +1,27 @@
+package biz.freshcode.learn.gwtp.client.slotless;
+
+import com.google.gwt.user.client.ui.HTML;
+import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
+import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.ViewImpl;
+import com.gwtplatform.mvp.client.annotations.NameToken;
+import com.gwtplatform.mvp.client.annotations.ProxyStandard;
+
+public class SlotlessChild extends Presenter<SlotlessChild.View, SlotlessChild.Proxy> {
+    @Inject
+    public SlotlessChild(EventBus eventBus, View view, Proxy proxy) {
+        super(eventBus, view, proxy);
+    }
+
+    @ProxyStandard
+    public interface Proxy extends com.gwtplatform.mvp.client.proxy.Proxy<SlotlessChild> {
+    }
+
+    public static class View extends ViewImpl {
+        @Inject
+        public View() {
+            initWidget(new HTML("<p>Slotless Child</p>"));
+        }
+    }
+}
