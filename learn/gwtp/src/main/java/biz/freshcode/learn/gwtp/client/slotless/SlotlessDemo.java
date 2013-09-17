fc96eb9100734c343dbc39cd18fabeb577c177c1
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2013-09-17 03:45:54 +0000

    [learn-gwtp] 'slotless' demo.

--- learn/gwtp/src/main/java/biz/freshcode/learn/gwtp/client/slotless/SlotlessDemo.java
+++ learn/gwtp/src/main/java/biz/freshcode/learn/gwtp/client/slotless/SlotlessDemo.java
@@ -2,17 +2,22 @@ package biz.freshcode.learn.gwtp.client.slotless;
 
 import biz.freshcode.learn.gwtp.client.boot.Root;
 import biz.freshcode.learn.gwtp.client.builder.gxt.ContentPanelBuilder;
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.core.shared.GWT;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.Presenter;
 import com.gwtplatform.mvp.client.ViewImpl;
 import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
-import com.sencha.gxt.widget.core.client.ContentPanel;
 
 public class SlotlessDemo extends Presenter<SlotlessDemo.View, SlotlessDemo.Proxy> {
     public static final String TOKEN = "slotlessDemo";
+
+    @Override
+    protected void onReveal() {
+        GWT.log("SlotlessDemo.onReveal()");
+        super.onReveal();
+    }
 
     @Inject
     public SlotlessDemo(EventBus eventBus, View view, Proxy proxy) {
