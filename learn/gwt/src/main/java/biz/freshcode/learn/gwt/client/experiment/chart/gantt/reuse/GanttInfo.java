56720399f795660537ff07efe0b3c06267c614b4
pwl <1179301+p33t@users.noreply.github.com>
2013-10-15 00:55:08 +0000

    [gwt] Start of Gantt bars.

---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/chart/gantt/reuse/GanttInfo.java
@@ -0,0 +1,15 @@
+package biz.freshcode.learn.gwt.client.experiment.chart.gantt.reuse;
+
+import java.util.Date;
+
+public class GanttInfo {
+    private final Iterable<GanttBar> bars;
+    private final Date zeroTime;
+    private final String title;
+
+    public GanttInfo(Iterable<GanttBar> bars, Date zeroTime, String title) {
+        this.bars = bars;
+        this.zeroTime = zeroTime;
+        this.title = title;
+    }
+}
