36cebd4b0ed381b7fe03480d1f20dabe749179c4
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2013-12-04 03:43:11 +0000

    [gwt] Grid: group by problems.

---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/gridgroupby/GridGroupByDemo.java
@@ -0,0 +1,154 @@
+package biz.freshcode.learn.gwt.client.experiment.gridgroupby;
+
+import biz.freshcode.learn.gwt.client.builder.gxt.container.BorderLayoutContainerBuilder;
+import biz.freshcode.learn.gwt.client.builder.gxt.grid.ColumnConfigBuilder;
+import biz.freshcode.learn.gwt.client.builder.gxt.grid.GroupingViewBuilder;
+import biz.freshcode.learn.gwt.client.util.AbstractIsWidget;
+import biz.freshcode.learn.gwt.client.util.IdentityHashProvider;
+import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
+import com.sencha.gxt.core.client.ValueProvider;
+import com.sencha.gxt.data.shared.ListStore;
+import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer;
+import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
+import com.sencha.gxt.widget.core.client.grid.ColumnModel;
+import com.sencha.gxt.widget.core.client.grid.Grid;
+import com.sencha.gxt.widget.core.client.grid.GroupingView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.newListFrom;
+
+public class GridGroupByDemo extends AbstractIsWidget<BorderLayoutContainer> {
+
+    public static final AbstractCell<StringCell> CELL = new AbstractCell<StringCell>() {
+        @Override
+        public void render(Context context, StringCell value, SafeHtmlBuilder sb) {
+            if (value != null) sb.append(value.getDisplay());
+        }
+    };
+
+    @Override
+    protected BorderLayoutContainer createWidget() {
+        ListStore<StringRow> store = new ListStore<StringRow>(new IdentityHashProvider<StringRow>());
+        int ixA, ixB;
+
+        int cols = 0;
+        List<ColumnConfig<StringRow, ?>> configs = Arrays.<ColumnConfig<StringRow, ?>>asList(
+                column(ixA = cols++)
+                        .header("A")
+                        .columnConfig,
+                column(ixB = cols++)
+                        .header("B")
+                        .columnConfig
+        );
+
+        store.addAll(newListFrom(stringRow("Alpha", "Bravo"), stringRow("Charlie", "Bravo")));
+
+        Grid<StringRow> grid = new Grid<StringRow>(store, new ColumnModel<StringRow>(configs));
+        GroupingView<StringRow> v = new GroupingViewBuilder<StringRow>()
+                .showGroupedColumn(false)
+                .forceFit(true)
+                .stripeRows(true)
+                .groupingView;
+        v.groupBy(configs.get(ixB));
+        grid.setView(v);
+
+        return new BorderLayoutContainerBuilder()
+                .centerWidget(grid)
+                .borderLayoutContainer;
+    }
+
+    private ColumnConfigBuilder<StringRow, StringCell> column(int ixCol) {
+        ValueProviderAdapter vp = new ValueProviderAdapter(ixCol);
+        return new ColumnConfigBuilder<StringRow, StringCell>(vp)
+                .comparator(new Comparator<StringCell>() {
+                    @Override
+                    public int compare(StringCell o1, StringCell o2) {
+                        return o1.getStr().compareTo(o2.getStr());
+                    }
+                })
+                .cell(CELL)
+                ;
+    }
+
+    private StringRow stringRow(String... vals) {
+        StringRow r = new StringRow();
+        for (String v : vals) r.add(new StringCell(v));
+        return r;
+    }
+
+    private static class ValueProviderAdapter implements ValueProvider<StringRow, StringCell> {
+        private final int colIx;
+
+        private ValueProviderAdapter(int colIx) {
+            this.colIx = colIx;
+        }
+
+        @Override
+        public StringCell getValue(StringRow r) {
+            return r.get(colIx);
+        }
+
+        @Override
+        public void setValue(StringRow r, StringCell value) {
+            // nothing
+        }
+
+        @Override
+        public String getPath() {
+            return "" + colIx;
+        }
+    }
+
+    private static class StringRow extends ArrayList<StringCell> {
+    }
+
+    private static class StringCell implements Comparable<StringCell> {
+        private String str;
+        private SafeHtml display;
+
+        StringCell(String str) {
+            this.str = str;
+            display = SafeHtmlUtils.fromString(str);
+        }
+
+        private SafeHtml getDisplay() {
+            return display;
+        }
+
+        private String getStr() {
+            return str;
+        }
+
+        @Override
+        public String toString() {
+            // NOTE: THis really shouldn't be necessary.  The cell from the column should be used.
+            return str;
+        }
+
+        @Override
+        public int hashCode() {
+            return str.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            //noinspection SimplifiableIfStatement
+            // NOTE: This really shouldn't be necessary.  The comparator for the column should be used.
+            if (obj instanceof StringCell) return compareTo((StringCell) obj) == 0;
+            return false;
+        }
+
+        @Override
+        public int compareTo(StringCell o) {
+            // NOTE: This really shouldn't be necessary.  The comparator for the column should be used.
+            return str.compareTo(o.str);
+        }
+    }
+}
