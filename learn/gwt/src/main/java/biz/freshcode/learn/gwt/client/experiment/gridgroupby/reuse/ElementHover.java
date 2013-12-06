03585a46d2c878fa3f9fd07bfb54e8098210a47b
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2013-12-06 01:00:48 +0000

    [gwt] Grid: Group Header hover.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/gridgroupby/reuse/ElementHover.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/experiment/gridgroupby/reuse/ElementHover.java
@@ -7,23 +7,32 @@ import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.client.ui.IsWidget;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.newList;
 import static biz.freshcode.learn.gwt.client.util.AppCollectionUtil.newMap;
+import static biz.freshcode.learn.gwt.client.util.AppObjectUtils.safeEquals;
 
+/**
+ * Facilitates hover operations on a SafeHtml rendered elements.
+ * Typically anything that uses this will also need a
+ * {@link biz.freshcode.learn.gwt.client.experiment.hoverwidget.reuse.HoverWidgetSupport}
+ *
+ * @param <T> The type of the token associated with an element.
+ */
 public class ElementHover<T> implements MouseOverHandler, MouseOutHandler {
     private static final String ID_PREFIX = "uniqueElementHoverId_";
     private static int nextId = 1;
     private final Callback<T> callback;
     private final Map<String, T> tokens = newMap();
-    private final List<HandlerRegistration> regos = newList();
+    private final List<HandlerRegistration> hRegos = newList();
 
     public ElementHover(IsWidget parent, Callback<T> callback) {
         this.callback = callback;
-        regos.add(parent.asWidget().addDomHandler(this, MouseOverEvent.getType()));
-        regos.add(parent.asWidget().addDomHandler(this, MouseOutEvent.getType()));
+        hRegos.add(parent.asWidget().addDomHandler(this, MouseOverEvent.getType()));
+        hRegos.add(parent.asWidget().addDomHandler(this, MouseOutEvent.getType()));
 
     }
 
@@ -34,7 +43,18 @@ public class ElementHover<T> implements MouseOverHandler, MouseOutHandler {
         tokens.clear();
     }
 
-    public SafeHtml wrapAndRegister(SafeHtml safeHtml, T token) {
+    /**
+     * Remove any registrations
+     */
+    public void clearToken(T token) {
+        for (Iterator<Map.Entry<String, T>> it = tokens.entrySet().iterator(); it.hasNext(); ) {
+            Map.Entry<String, T> me = it.next();
+            if (safeEquals(token, me.getValue())) it.remove();
+        }
+    }
+
+    public SafeHtml wrapAndRegister(SafeHtml safeHtml, T token, boolean uniqueToken) {
+        if (uniqueToken) clearToken(token);
         String id = ID_PREFIX + nextId++;
         tokens.put(id, token);
         return new SafeHtmlBuilder().appendHtmlConstant("<div id='" + id + "'>")
@@ -66,9 +86,9 @@ public class ElementHover<T> implements MouseOverHandler, MouseOutHandler {
         /**
          * Notification that the mouse is hovering over a dom element of interest.
          *
-         * @param source The invoker of the method.
-         * @param domElem The dom element that was the subject of the event
-         * @param token The token supplied to this utility during registration.
+         * @param source      The invoker of the method.
+         * @param domElem     The dom element that was the subject of the event
+         * @param token       The token supplied to this utility during registration.
          * @param mouseIsOver
          */
         void stateChange(ElementHover source, Element domElem, T token, boolean mouseIsOver);
