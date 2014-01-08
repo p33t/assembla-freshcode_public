2a1960da4f4844f5ad930e3c11150ce11d7b9c5e
pwl <1179301+p33t@users.noreply.github.com>
2014-01-08 01:43:35 +0000

    [learn-scala] Streams.

--- learn_scala/syntax/src/main/scala/pkg/Streams.scala
+++ learn_scala/syntax/src/main/scala/pkg/Streams.scala
@@ -7,7 +7,18 @@ object Streams {
     def myStream(i: Int): Stream[Int] = {
       if (i > 50) empty else i #:: myStream(i + 2)
     }
-    
-    myStream(2).foreach(println)
+
+    def oddsOnly(src: Stream[Int]): Stream[Int] = {
+      src match {
+        case i #:: tail =>
+          if (i % 2 == 0) oddsOnly(tail)
+          else i #:: oddsOnly(tail)
+        case Stream.Empty => Stream.Empty
+      }
+    }
+
+    myStream(2).foreach(print)
+    println()
+    oddsOnly(0 until 10 toStream).foreach(print)
   }
 }
