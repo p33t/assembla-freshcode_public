81af80e4a21eec39a14ebe2237be237f8ec5b751
pwl <1179301+p33t@users.noreply.github.com>
2014-01-17 07:27:42 +0000

    Dynamic

--- learn/gwt/src/test/java/pkg/BeanBuilderGenerator.java
+++ learn/gwt/src/test/java/pkg/BeanBuilderGenerator.java
@@ -3,15 +3,13 @@ package pkg;
 import biz.freshcode.b_generation.BeanBuilderWriter;
 import biz.freshcode.b_generation.BgenUtil;
 import biz.freshcode.b_generation.DefaultBeanBuilderGenerator;
-import biz.freshcode.learn.gwt.client.uispike.builder.BeanBuilder;
-import biz.freshcode.learn.gwt.client.uispike.builder.Construct;
-import com.sencha.gxt.chart.client.chart.Legend;
-import com.sencha.gxt.chart.client.draw.path.PathSprite;
+import biz.freshcode.learn.gwt.client.builder.BeanBuilder;
+import biz.freshcode.learn.gwt.client.builder.Construct;
+import com.sencha.gxt.widget.core.client.grid.GroupSummaryView;
 
 public class BeanBuilderGenerator extends DefaultBeanBuilderGenerator {
-    static final Class CLASS = Legend.class; //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    static final Class CLASS = GroupSummaryView.class; //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     public static final String BASE_PKG = "biz.freshcode.learn.gwt.client.builder";
-
 
     public static void main(String[] args) {
         new BeanBuilderWriter()
@@ -19,13 +17,15 @@ public class BeanBuilderGenerator extends DefaultBeanBuilderGenerator {
                 .addMapping("com.sencha.gxt.chart.client", BASE_PKG + ".gxt")
                 .addMapping("com.sencha.gxt.core.client", BASE_PKG + ".gxt")
                 .addMapping("com.google.gwt.user.client.ui", BASE_PKG + ".gwt")
+                .addMapping("biz.freshcode.learn.gwt.client", BASE_PKG + ".freshcode")
                 .generator(new BeanBuilderGenerator())
                 .write(CLASS);
     }
 
     @Override
     protected String getBuilderAnnotation(Class beanClass) {
-        return "@" + BeanBuilder.class.getName() + "(" + beanClass.getSimpleName() + ".class)";
+        return "@" + BeanBuilder.class.getName() + "(" + beanClass.getSimpleName() + ".class)\n" +
+                "@" + SuppressWarnings.class.getSimpleName() + "(\"UnusedDeclaration\")";
     }
 
     /**
