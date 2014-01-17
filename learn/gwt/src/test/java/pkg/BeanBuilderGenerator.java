b8fec8186320b1c6037af1820bfc5b3725688d65
pwl <1179301+p33t@users.noreply.github.com>
2014-01-17 07:31:55 +0000

    [gwt] Fix up builders to be consistent.

--- learn/gwt/src/test/java/pkg/BeanBuilderGenerator.java
+++ learn/gwt/src/test/java/pkg/BeanBuilderGenerator.java
@@ -12,6 +12,26 @@ public class BeanBuilderGenerator extends DefaultBeanBuilderGenerator {
     public static final String BASE_PKG = "biz.freshcode.learn.gwt.client.builder";
 
     public static void main(String[] args) {
+//        For migrating to new components.
+//        String str = new MigrationProbe()
+//                .builderScan(new BuilderScan().targetAnnotation(BeanBuilder.class))
+//                .beanClassReader(new MigrationProbe.BeanClassReader() {
+//                    @Override
+//                    public Class read(Class builderClass) {
+//                        BeanBuilder ann = (BeanBuilder) builderClass.getAnnotation(BeanBuilder.class);
+//                        if (ann == null)
+//                            throw ExceptionUtil.illegalState(builderClass.getName() + " does not have annotation");
+//                        return ann.value();
+//                    }
+//                })
+//                .packageMapper(
+//                        new PackageMapper()
+//                        // grab from below
+//                )
+//                .probe();
+//
+//        System.out.println(str);
+
         new BeanBuilderWriter()
                 .addMapping("com.sencha.gxt.widget.core.client", BASE_PKG + ".gxt")
                 .addMapping("com.sencha.gxt.chart.client", BASE_PKG + ".gxt")
