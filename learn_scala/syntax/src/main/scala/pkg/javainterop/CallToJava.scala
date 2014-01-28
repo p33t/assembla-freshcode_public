9a582aeec02ecfb19d8508a4ce1c4b6de248a5af
pwl <1179301+p33t@users.noreply.github.com>
2014-01-28 23:19:11 +0000

    [learn-scala] Update scala and check reflective instantiation of java class.

--- learn_scala/syntax/src/main/scala/pkg/javainterop/CallToJava.scala
+++ learn_scala/syntax/src/main/scala/pkg/javainterop/CallToJava.scala
@@ -4,5 +4,9 @@ package pkg.javainterop
 object CallToJava {
   def main(args: Array[String]) {
     println("Want xyzxyz:" + JavaCls.twice("xyz"))
+
+    val cls = classOf[JavaCls]
+    val jc = cls.newInstance()
+    println("Instantiated " + jc)
   }
 }
