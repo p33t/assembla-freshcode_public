af32e57857a0fcfa4635cdd5a6cb001b74f9814e
pwl <1179301+p33t@users.noreply.github.com>
2014-03-04 05:41:49 +0000

    [scala-testing2] Mocking a complex trait.

--- learn_scala/testing2/src/test/scala/pkg/mock/ComplexTrait.scala
+++ learn_scala/testing2/src/test/scala/pkg/mock/ComplexTrait.scala
@@ -3,7 +3,10 @@ package pkg.mock
 
 trait ComplexTrait {
   val procStr: String => String
+  val procIter: List[String] => Int
 
 
   def apply(str: String) = procStr(str)
+
+  def alt(ss: List[String]) = procIter(ss)
 }
