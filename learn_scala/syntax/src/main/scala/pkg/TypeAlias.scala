6ae24ea424854d3d6f675308b225fed873f2964a
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2014-04-08 01:45:07 +0000

    [learn-scala] Nested type alias problems.

--- learn_scala/syntax/src/main/scala/pkg/TypeAlias.scala
+++ learn_scala/syntax/src/main/scala/pkg/TypeAlias.scala
@@ -3,12 +3,26 @@ package pkg
 
 object TypeAlias {
   type CustomString = String
+  
+  type BaseType[A,B] = A => B
+  type AppliedType = BaseType[Int, String]
 
   def myPrint(cs: CustomString) {
     println(cs)
   }
 
+  def expectsBaseType[A,B](fn: BaseType[A,B])(a:A) {
+    println(fn(a))
+  }
+  
   def main(args: Array[String]) {
     myPrint("A normal string")
+    
+    val fn: AppliedType = i => "Int: " + i
+//  Need explicit type args
+// or declare expectsBaseType... (fn: A => B)...
+// It looks like inputs can only have single layer type aliases.
+// expectsBaseType(fn)(99)
+    expectsBaseType[Int, String](fn)(99)
   }
 }
