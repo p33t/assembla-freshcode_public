84e5e35b283257624957405a5f3b5b0e06e0ff7a
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2014-04-23 05:52:21 +0000

    [learn-scala] Chaining lazy args.

--- learn_scala/syntax/src/main/scala/pkg/LazyArgs.scala
+++ learn_scala/syntax/src/main/scala/pkg/LazyArgs.scala
@@ -15,11 +15,19 @@ object LazyArgs {
     println("Resulting count is " + count)
     println("Accessing lazy val: " + mc.arg)
 
+    val current = count
+    indirect(nextCount())
+    println("Called indirectly, count should be " + (current + 2) + ".  Count:" + count)
   }
 
   def myMethod(arg: => String) {
     println("1st time: " + arg)
     println("2nd time: " + arg)
+  }
+
+  def indirect(arg: => String) {
+    // the only way to break this is to assign to an intermediate variable
+    myMethod(arg)
   }
 
   // NOTE: 'call-by-name' not allowed for 'val' args
