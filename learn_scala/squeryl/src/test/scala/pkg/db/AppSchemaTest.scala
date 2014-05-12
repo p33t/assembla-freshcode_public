4520239dadb8821f824d407d51cf9bbd5b68e248
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2014-05-12 06:50:18 +0000

    [learn-squeryl] Updated code style.

--- learn_scala/squeryl/src/test/scala/pkg/db/AppSchemaTest.scala
+++ learn_scala/squeryl/src/test/scala/pkg/db/AppSchemaTest.scala
@@ -1,9 +1,9 @@
 package pkg.db
 
-import org.squeryl.PrimitiveTypeMode._
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
+import DbTypes._
 
 @RunWith(classOf[JUnitRunner])
 class AppSchemaTest extends FunSuite {
