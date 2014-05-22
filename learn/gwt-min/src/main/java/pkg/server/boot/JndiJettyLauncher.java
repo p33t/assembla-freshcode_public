5de51554ac7cab24991934b8c66a5f3bf8839af0
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2014-05-22 05:22:22 +0000

    [gwt-min] Explicit setting of configuration classes on Jetty launch.

--- learn/gwt-min/src/main/java/pkg/server/boot/JndiJettyLauncher.java
+++ learn/gwt-min/src/main/java/pkg/server/boot/JndiJettyLauncher.java
@@ -7,8 +7,22 @@ import org.eclipse.jetty.webapp.WebAppContext;
 import java.io.File;
 
 public class JndiJettyLauncher extends JettyLauncher {
+    private static final String[] CONFIG_CLASSES = new String[]{
+            "org.eclipse.jetty.webapp.WebInfConfiguration",
+            "org.eclipse.jetty.webapp.WebXmlConfiguration",
+            "org.eclipse.jetty.webapp.MetaInfConfiguration",
+            "org.eclipse.jetty.webapp.FragmentConfiguration",
+            //    "org.eclipse.jetty.plus.webapp.EnvConfiguration", // add for JNDI
+            //    "org.eclipse.jetty.plus.webapp.PlusConfiguration", // add for JNDI
+            "org.eclipse.jetty.webapp.JettyWebXmlConfiguration"
+            // Not necessary for Jetty 8..."org.eclipse.jetty.webapp.TagLibConfiguration",
+    };
+
+
     @Override
     protected WebAppContext createWebAppContext(TreeLogger logger, File appRootDir) {
-        return super.createWebAppContext(logger, appRootDir);
+        WebAppContext ctx = super.createWebAppContext(logger, appRootDir);
+        ctx.setConfigurationClasses(CONFIG_CLASSES);
+        return ctx;
     }
 }
