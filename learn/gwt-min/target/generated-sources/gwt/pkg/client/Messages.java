a7f4e8f84673ab23c9119699c00752dd3edc594d
pwl <1179301+p33t@users.noreply.github.com>
2014-05-22 01:47:11 +0000

    [gwt-min] Archetype output.

---
+++ learn/gwt-min/target/generated-sources/gwt/pkg/client/Messages.java
@@ -0,0 +1,26 @@
+package pkg.client;
+
+/**
+ * Interface to represent the messages contained in resource bundle:
+ * 	/home/pleong/work/learn/gwt-min/src/main/resources/pkg/client/Messages.properties'.
+ */
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  /**
+   * Translated "Enter your name".
+   * 
+   * @return translated "Enter your name"
+   */
+  @DefaultMessage("Enter your name")
+  @Key("nameField")
+  String nameField();
+
+  /**
+   * Translated "Send".
+   * 
+   * @return translated "Send"
+   */
+  @DefaultMessage("Send")
+  @Key("sendButton")
+  String sendButton();
+}
