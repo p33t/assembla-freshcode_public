c7c4950df829678c212b5aaa60334002fa65067f
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2014-05-28 00:58:52 +0000

    [learn-gwtp] Comment.

--- learn/gwtp/src/main/java/biz/freshcode/learn/gwtp/shared/AppUtil.java
+++ learn/gwtp/src/main/java/biz/freshcode/learn/gwtp/shared/AppUtil.java
@@ -5,11 +5,9 @@ public class AppUtil {
      * The name of the cookie to put the RPC token in for XSRF protection.
      * NOTE: This could possibly vary by app instance to avoid cookie collision with other apps on same server.
      * The cookie name would need to be set in the SessionInfo.
-     * See http://code.google.com/p/gwt-platform/issues/detail?id=429
-     * .... hmm..actually cannot DI a SessionInfo value because dependencies of GIN are decided at compile-time.
      *
      * ALSO!!!!!! This doesn't seem to work in Chrome in dev mode with address 127.0.0.1.  The cookie is not stored.
-     * Using 'localhost' does work.
+     * ====> Using 'localhost' does work.
      */
     public static final String XSRF_COOKIE = "XSRF-SAFETY";
 }
