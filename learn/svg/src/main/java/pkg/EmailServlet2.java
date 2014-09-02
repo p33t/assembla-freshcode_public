be08eb98150091c25f001fc489183afff4d43b68
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2014-09-02 06:02:15 +0000

    [learn-svg] Atttempt to use RequestDispatcher for capturing jsp.

---
+++ learn/svg/src/main/java/pkg/EmailServlet2.java
@@ -0,0 +1,30 @@
+package pkg;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static pkg.StringUtils.escapeHTML;
+import static pkg.StringUtils.newlinesToXHTMLBreaks;
+
+/**
+ * Trying to use a RequestDispatcher instead.
+ */
+public class EmailServlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher render = req.getRequestDispatcher("/index.jsp");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        render.include(req, new CapturingResponse(resp, baos));
+
+        ServletOutputStream stream = resp.getOutputStream();
+        stream.println("<html><head><title>Blah</title><body>Size: " + baos.size() + "<br/><pre>");
+        stream.println(newlinesToXHTMLBreaks(escapeHTML(baos.toString())));
+        stream.print("</pre></body></html>");
+    }
+}
--- learn/svg/src/main/webapp/index.jsp
+++ learn/svg/src/main/webapp/index.jsp
@@ -4,7 +4,7 @@
     <title>SVG Demo</title>
 </head>
 <body>
-<a href="show-download-print.jsp">Show / Download / Print</a><br/>
+<a href="show-download-print.jsp">Show / Download / Print</a> <a href="email?x=5&y=5&count=2&command=Show">Email</a><br/>
 <a href="css-styled.html">CSS Styled SVG</a><br/>
 <a href="rotate-timed.html">Rotate Timed SVG</a><br/>
 
