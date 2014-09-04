668624c5cbd497b991ea244b3d48a52b5aa4e976
pwl <1179301+p33t@users.noreply.github.com>
2014-09-04 07:32:23 +0000

    [learn-svg] Trying alternative jsp capture approach.

--- learn/svg/src/main/java/pkg/EmailServlet2.java
+++ learn/svg/src/main/java/pkg/EmailServlet2.java
@@ -2,15 +2,11 @@ package pkg;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-
-import static pkg.StringUtils.escapeHTML;
-import static pkg.StringUtils.newlinesToXHTMLBreaks;
 
 /**
  * Trying to use a RequestDispatcher instead.
@@ -18,13 +14,14 @@ import static pkg.StringUtils.newlinesToXHTMLBreaks;
 public class EmailServlet2 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher render = req.getRequestDispatcher("/index.jsp");
+        RequestDispatcher render = req.getRequestDispatcher("/render/index.jsp");
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        render.include(req, new CapturingResponse(resp, baos));
+        // NOTE: Must 'forward' instead of 'include' for download / header settings to work.
+        render.include(req, resp); // new CapturingResponse(resp, baos));
 
-        ServletOutputStream stream = resp.getOutputStream();
-        stream.println("<html><head><title>Blah</title><body>Size: " + baos.size() + "<br/><pre>");
-        stream.println(newlinesToXHTMLBreaks(escapeHTML(baos.toString())));
-        stream.print("</pre></body></html>");
+//        ServletOutputStream stream = resp.getOutputStream();
+//        stream.println("<html><head><title>Blah</title><body>Size: " + baos.size() + "<br/><pre>");
+//        stream.println(newlinesToXHTMLBreaks(escapeHTML(baos.toString())));
+//        stream.print("</pre></body></html>");
     }
 }
