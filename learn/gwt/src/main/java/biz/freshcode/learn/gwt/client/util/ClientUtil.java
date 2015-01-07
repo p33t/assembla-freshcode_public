298197a14c4c615efd474432fa70a2df4fce8642
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2015-01-07 04:02:33 +0000

    [gwt] Logging for GWT 2.7 now working.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/util/ClientUtil.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt/client/util/ClientUtil.java
@@ -6,8 +6,6 @@ public class ClientUtil {
     public static final Logger LOG = Logger.getLogger("FileReadButton");
 
     public static void log(String msg) {
-        // NOTE: Only sever works by default
-        // https://groups.google.com/forum/#!topic/google-web-toolkit-contributors/Tqu5Zw5H-II
-        LOG.severe(msg);
+        LOG.info(msg);
     }
 }
