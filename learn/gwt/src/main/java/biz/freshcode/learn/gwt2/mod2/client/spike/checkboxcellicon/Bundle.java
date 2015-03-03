5bb95a670428e7d9837ed8b032bd6eb3d90279c9
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2015-03-03 04:36:21 +0000

    [gwt] CheckBox icon something working.

---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt2/mod2/client/spike/checkboxcellicon/Bundle.java
@@ -0,0 +1,19 @@
+package biz.freshcode.learn.gwt2.mod2.client.spike.checkboxcellicon;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+public interface Bundle extends ClientBundle {
+    Bundle BUNDLE = GWT.create(Bundle.class);
+    // Need to make sure style is injected into module
+    boolean DOM_MUTATED = BUNDLE.style().ensureInjected();
+    Style STYLE = BUNDLE.style();
+
+    @Source("style.css")
+    Style style();
+
+    interface Style extends CssResource {
+        String centerAlign();
+    }
+}
