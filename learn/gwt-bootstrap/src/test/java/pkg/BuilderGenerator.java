b9f385843f9d906f2337d359be777ed9f199a829
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2015-08-14 07:16:43 +0000

    [gwt-bootstrap] Display a GwtBootstrap3 element.

---
+++ learn/gwt-bootstrap/src/test/java/pkg/BuilderGenerator.java
@@ -0,0 +1,15 @@
+package pkg;
+
+import biz.freshcode.b_generation.BeanBuilderWriter;
+import org.gwtbootstrap3.client.ui.html.Paragraph;
+
+import java.io.File;
+
+public class BuilderGenerator {
+    public static void main(String[] args) {
+        new BeanBuilderWriter()
+                .outputFolder(new File("src/main/java")) // The desired output folder
+                .addMapping("", "biz.freshcode.learn.gwt_bootstrap.client.builder")           // Map the package of the resulting builder
+                .write(Paragraph.class);                  // The bean class of the desired builder
+    }
+}
