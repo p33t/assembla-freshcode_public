9c8041637844ccbfb920e61d6ba3665bd87fa0e2
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2015-08-20 06:33:03 +0000

    [gwt-bootstrap] 'Loading...' message.

--- learn/gwt-bootstrap/src/main/java/biz/freshcode/learn/gwt_bootstrap/client/boot/EntryPoint.java
+++ learn/gwt-bootstrap/src/main/java/biz/freshcode/learn/gwt_bootstrap/client/boot/EntryPoint.java
@@ -1,11 +1,15 @@
 package biz.freshcode.learn.gwt_bootstrap.client.boot;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.gwtplatform.mvp.client.ApplicationController;
 
 public class EntryPoint implements com.google.gwt.core.client.EntryPoint {
     @Override
     public void onModuleLoad() {
+        RootPanel.getBodyElement().removeChild(DOM.getElementById("loading"));
+
         ApplicationController ctrl = GWT.create(ApplicationController.class);
         ctrl.init();
     }
