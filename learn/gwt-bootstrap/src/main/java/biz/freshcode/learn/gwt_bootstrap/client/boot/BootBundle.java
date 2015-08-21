da1cdd11db5443c2f863702987d12ae97606c78c
pwl <1179301+p33t@users.noreply.github.com>
2015-08-21 04:44:10 +0000

    [gwt-bootstrap] Playing with graphics.

--- learn/gwt-bootstrap/src/main/java/biz/freshcode/learn/gwt_bootstrap/client/boot/BootBundle.java
+++ learn/gwt-bootstrap/src/main/java/biz/freshcode/learn/gwt_bootstrap/client/boot/BootBundle.java
@@ -2,14 +2,25 @@ package biz.freshcode.learn.gwt_bootstrap.client.boot;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.ImageResource;
 
 public interface BootBundle extends ClientBundle {
     BootBundle BOOT_BUNDLE = GWT.create(BootBundle.class);
-//    Style STYLE = INSTANCE.style();
+    Style BOOT_STYLE = BOOT_BUNDLE.style();
     // Needed
-//    boolean DOM_MUTATED = STYLE.ensureInjected();
+    boolean DOM_MUTATED = BOOT_STYLE.ensureInjected();
 
     @Source("media/logo-sml.png")
     ImageResource logoSml();
+
+    @Source("media/strategy8.png")
+    ImageResource strategy8();
+
+    @Source("style.css")
+    Style style();
+
+    interface Style extends CssResource {
+        String lightBgnd();
+    }
 }
--- learn/gwt-bootstrap/src/main/java/biz/freshcode/learn/gwt_bootstrap/client/boot/PlaceToken.java
+++ learn/gwt-bootstrap/src/main/java/biz/freshcode/learn/gwt_bootstrap/client/boot/PlaceToken.java
@@ -3,4 +3,5 @@ package biz.freshcode.learn.gwt_bootstrap.client.boot;
 public class PlaceToken {
     public static final String TOK_HOME = "home";
     public static final String TOK_ALT = "alt";
+    public static final String TOK_GRAPHIC = "graphic";
 }
