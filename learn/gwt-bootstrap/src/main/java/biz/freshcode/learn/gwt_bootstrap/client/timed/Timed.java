c27d9bdfa26fcb10b018b273721cce3a31038d46
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2015-08-26 04:32:26 +0000

    [gwt-bootstrap] Timed display.

--- learn/gwt-bootstrap/src/main/java/biz/freshcode/learn/gwt_bootstrap/client/timed/Timed.java
+++ learn/gwt-bootstrap/src/main/java/biz/freshcode/learn/gwt_bootstrap/client/timed/Timed.java
@@ -1,65 +1,85 @@
 package biz.freshcode.learn.gwt_bootstrap.client.timed;
 
-import biz.freshcode.learn.gwt_bootstrap.client.boot.PlaceToken;
 import biz.freshcode.learn.gwt_bootstrap.client.boot.RootPresenter;
 import biz.freshcode.learn.gwt_bootstrap.client.builder.org.gwtbootstrap3.client.ui.ColumnBuilder;
 import biz.freshcode.learn.gwt_bootstrap.client.builder.org.gwtbootstrap3.client.ui.ImageBuilder;
 import biz.freshcode.learn.gwt_bootstrap.client.builder.org.gwtbootstrap3.client.ui.RowBuilder;
-import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.Presenter;
-import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.ViewImpl;
 import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.gwtplatform.mvp.client.annotations.ProxyCodeSplit;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
+import org.gwtbootstrap3.client.ui.Image;
 import org.gwtbootstrap3.client.ui.constants.ColumnSize;
 import org.gwtbootstrap3.client.ui.constants.ImageType;
+import org.gwtbootstrap3.client.ui.html.Paragraph;
+import org.gwtbootstrap3.extras.animate.client.ui.constants.Animation;
 
 import static biz.freshcode.learn.gwt_bootstrap.client.boot.BootBundle.BOOT_BUNDLE;
 import static biz.freshcode.learn.gwt_bootstrap.client.boot.BootBundle.BOOT_STYLE;
+import static biz.freshcode.learn.gwt_bootstrap.client.boot.PlaceToken.TOK_TIMED;
 
-public class Timed extends Presenter<View, Timed.Proxy> {
+public class Timed extends Presenter<Timed.View, Timed.Proxy> {
+
     @Inject
     public Timed(EventBus eventBus, View view, Proxy proxy) {
         super(eventBus, view, proxy, RootPresenter.SLOT);
     }
 
+    @Override
+    protected void onReset() {
+        super.onReset();
+        GWT.log("Timed is being reset");
+        Image img = getView().getImgMid();
+        img.removeStyleName(Animation.FADE_IN.getCssName());
+        img.getElement().getStyle().setOpacity(0);
+
+        Scheduler.get().scheduleFixedDelay(new Scheduler.RepeatingCommand() {
+            @Override
+            public boolean execute() {
+                if (isVisible()) {
+                    GWT.log("Adding animation");
+                    Image img = getView().getImgMid();
+                    img.addStyleName(Animation.FADE_IN.getCssName());
+                }
+                else {
+                    GWT.log("Not Visible");
+                }
+                return false;
+            }
+        }, 2000);
+    }
+
     @ProxyCodeSplit
-    @NameToken(PlaceToken.TOK_TIMED)
+    @NameToken(TOK_TIMED)
     public interface Proxy extends ProxyPlace<Timed> {
     }
 
     public static class View extends ViewImpl {
-        private final SimplePanel pnlMid;
-        private final SimplePanel pnlRight;
+        private final Image imgMid;
 
         @Inject
         public View() {
             initWidget(new RowBuilder()
                     .add(new ColumnBuilder(ColumnSize.XS_4)
-                            .add(new ImageBuilder()
+                            .add(new Paragraph("An image will appear in 2 secs..."))
+                            .column)
+                    .add(new ColumnBuilder(ColumnSize.XS_4)
+                            .add(imgMid = new ImageBuilder()
                                     .addStyleName(BOOT_STYLE.lightBgnd())
                                     .url(BOOT_BUNDLE.strategy8().getSafeUri())
                                     .type(ImageType.ROUNDED)
                                     .image)
                             .column)
-                    .add(new ColumnBuilder(ColumnSize.XS_4)
-                            .add(pnlMid = new SimplePanel())
-                            .column)
-                    .add(new ColumnBuilder(ColumnSize.XS_4)
-                            .add(pnlRight = new SimplePanel())
-                            .column)
                     .row);
         }
 
-        public SimplePanel getPnlMid() {
-            return pnlMid;
-        }
-
-        public SimplePanel getPnlRight() {
-            return pnlRight;
+        public Image getImgMid() {
+            return imgMid;
         }
     }
 }
