3b9e4e59699a8519947911a14654a288dfc514e8
pwl <1179301+p33t@users.noreply.github.com>
2015-08-26 01:14:16 +0000

    [gwt-bootstrap] Respoinsiveness.

--- learn/gwt-bootstrap/src/main/java/biz/freshcode/learn/gwt_bootstrap/client/grids/Grids.java
+++ learn/gwt-bootstrap/src/main/java/biz/freshcode/learn/gwt_bootstrap/client/grids/Grids.java
@@ -16,12 +16,13 @@ import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.gwtplatform.mvp.client.annotations.ProxyCodeSplit;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
 import org.gwtbootstrap3.client.ui.constants.ColumnOffset;
-import org.gwtbootstrap3.client.ui.constants.ColumnSize;
+import org.gwtbootstrap3.client.ui.constants.DeviceSize;
 import org.gwtbootstrap3.client.ui.constants.ImageType;
 import org.gwtbootstrap3.client.ui.html.Text;
 
 import static biz.freshcode.learn.gwt_bootstrap.client.boot.BootBundle.BOOT_BUNDLE;
 import static biz.freshcode.learn.gwt_bootstrap.client.boot.BootBundle.BOOT_STYLE;
+import static org.gwtbootstrap3.client.ui.constants.ColumnSize.*;
 
 public class Grids extends Presenter<View, Grids.Proxy> {
 
@@ -40,23 +41,34 @@ public class Grids extends Presenter<View, Grids.Proxy> {
         public View() {
             initWidget(new ContainerBuilder()
                     .add(new RowBuilder()
-                            .add(new ColumnBuilder(ColumnSize.SM_4, ColumnSize.XS_12)
+                            .add(new ColumnBuilder(XS_12)
+                                    .visibleOn(DeviceSize.XS)
                                     .add(new ImageBuilder()
                                             .addStyleName(BOOT_STYLE.lightBgnd())
                                             .url(BOOT_BUNDLE.strategy8().getSafeUri())
                                             .type(ImageType.ROUNDED)
                                             .image)
                                     .column)
-                            .add(new ColumnBuilder(ColumnSize.SM_8, ColumnSize.XS_12)
+                            .row)
+                    .add(new RowBuilder()
+                            .add(new ColumnBuilder(SM_3)
+                                    .hiddenOn(DeviceSize.XS)
+                                    .add(new ImageBuilder()
+                                            .addStyleName(BOOT_STYLE.lightBgnd())
+                                            .url(BOOT_BUNDLE.strategy8().getSafeUri())
+                                            .type(ImageType.ROUNDED)
+                                            .image)
+                                    .column)
+                            .add(new ColumnBuilder(XS_12, SM_9)
                                     .add(new ParagraphBuilder()
                                             .addStyleName("h2")
-                                            .add(new Text("The Title"))
+                                            .add(new Text("Plan & Manage Change"))
                                             .paragraph)
                                     .column)
                             .row)
                     .add(new RowBuilder()
-                            .add(new ColumnBuilder(ColumnSize.SM_4, ColumnSize.XS_12)
-                                    .offset(ColumnOffset.SM_4, ColumnOffset.XS_0)
+                            .add(new ColumnBuilder(SM_4, XS_12)
+                                    .offset(ColumnOffset.SM_3, ColumnOffset.XS_0)
                                     .add(new ParagraphBuilder()
                                             .add(new ImageBuilder()
                                                     .url(BOOT_BUNDLE.pin().getSafeUri())
@@ -66,7 +78,7 @@ public class Grids extends Presenter<View, Grids.Proxy> {
                                             .addStyleName("h4")
                                             .paragraph)
                                     .column)
-                            .add(new ColumnBuilder(ColumnSize.SM_4, ColumnSize.XS_12)
+                            .add(new ColumnBuilder(SM_5, XS_12)
                                     .add(new ParagraphBuilder()
                                             .add(new ImageBuilder()
                                                     .url(BOOT_BUNDLE.pin().getSafeUri())
