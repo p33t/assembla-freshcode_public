8ef9644fa83db8888d3e65357ff6703dc7a3a064
pwl <1179301+p33t@users.noreply.github.com>
2015-08-28 06:10:51 +0000

    [gwt-bootstrap] Updated builders.

---
+++ learn/gwt-bootstrap/src/main/java/biz/freshcode/learn/gwt_bootstrap/client/builder/Construct.java
@@ -0,0 +1,17 @@
+package biz.freshcode.learn.gwt_bootstrap.client.builder;
+
+public abstract class Construct<T> {
+    public T builder;
+
+    public abstract void run();
+
+    public static class Parent<T> {
+        public T construct(Construct<T> c) {
+            @SuppressWarnings("unchecked")
+            T t = (T) this;
+            c.builder = t;
+            c.run();
+            return t;
+        }
+    }
+}
