0fafbf0b5cb6654be30869bec50d0edfe862f129
pwl <1179301+p33t@users.noreply.github.com>
2015-10-14 06:02:03 +0000

    [gwt-bootstrap] Use NavigationEvent instead of History.valueChange event for navigation maintenance.

--- learn/gwt-bootstrap/src/main/java/biz/freshcode/learn/gwt_bootstrap/client/boot/RootPresenter.java
+++ learn/gwt-bootstrap/src/main/java/biz/freshcode/learn/gwt_bootstrap/client/boot/RootPresenter.java
@@ -4,8 +4,6 @@ import biz.freshcode.learn.gwt_bootstrap.client.builder.org.gwtbootstrap3.client
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.Window;
@@ -18,6 +16,8 @@ import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.ViewImpl;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
 import com.gwtplatform.mvp.client.presenter.slots.NestedSlot;
+import com.gwtplatform.mvp.client.proxy.NavigationEvent;
+import com.gwtplatform.mvp.client.proxy.NavigationHandler;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
 import org.gwtbootstrap3.client.ui.Button;
@@ -45,6 +45,7 @@ public class RootPresenter extends Presenter<View, RootPresenter.Proxy> {
             @Override
             public void onClick(ClickEvent event) {
                 // changes place, sets history (does NOT fire history change event)
+                // is OK now because don't use History.valueChange event.  Now use NavigationEvent.
                 placeManager.revealPlace(new PlaceRequest.Builder()
                         .nameToken(TOK_HOME)
                         .build());
@@ -72,7 +73,7 @@ public class RootPresenter extends Presenter<View, RootPresenter.Proxy> {
         private NavbarCollapse navbarCollapse;
 
         @Inject
-        public View() {
+        public View(EventBus bus) {
             bindSlot(SLOT, slotPanel);
 
             NavbarCollapseButton collapseButton;
@@ -165,14 +166,15 @@ public class RootPresenter extends Presenter<View, RootPresenter.Proxy> {
             // setup responsive nav bar
             collapseButton.setDataTargetWidget(navbarCollapse);
 
-            // Making the window scroll to top on every page change
-            History.addValueChangeHandler(new ValueChangeHandler<String>() {
+            bus.addHandler(NavigationEvent.getType(), new NavigationHandler() {
                 @Override
-                public void onValueChange(ValueChangeEvent<String> event) {
+                public void onNavigation(NavigationEvent navigationEvent) {
                     Scheduler.get().scheduleDeferred(new Command() {
                         @Override
                         public void execute() {
+                            // Making the window scroll to top on every page change
                             Window.scrollTo(0, 0);
+                            // and collapse any nav menus
                             hideNavbarCollapse();
                         }
                     });
