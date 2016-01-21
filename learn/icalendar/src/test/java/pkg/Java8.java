f501c8ff3873b0e2c18f1e0597fa07c76d0682af
pwl <1179301+p33t@users.noreply.github.com>
2016-01-21 05:57:12 +0000

    [ical] Learning Java8.

---
+++ learn/icalendar/src/test/java/pkg/Java8.java
@@ -0,0 +1,40 @@
+/*
+ Notes:
+ - http://www.oracle.com/webfolder/technetwork/tutorials/obe/java/Lambda-QuickStart/index.html
+ - Interfaces with 1 method are 'functional interfaces' (formerly SAM)
+ - Method reference String::hashCode
+ - java.util.function for various function interfaces
+ - Collection.stream() to facilitate extra ops
+ - Collectors for getting collection back from stream
+ - Parallel streams
+ */
+package pkg;
+
+import junit.framework.TestCase;
+
+import java.io.PrintStream;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class Java8 extends TestCase {
+    public void testLamdaExpression() {
+        Runnable r = () -> System.out.println("Run");
+        r.run();
+    }
+
+    public void testLamdaMethodReference() {
+        List<PrintStream> l = asList(System.out, System.err);
+        l.forEach(PrintStream::println);
+    }
+
+    public void testStreams() {
+        List<String> l = asList("1", "2", "3", "11", "22", "33");
+        int i = l.stream()
+                .filter(s -> s.length() == 1)
+                .mapToInt(String::hashCode)
+                .sum();
+        System.out.println(i);
+    }
+
+}
