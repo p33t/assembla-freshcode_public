17cfdc94be85ed7ed9b35b4dd63b0eff1103c12d
pwl <1179301+p33t@users.noreply.github.com>
2016-02-05 01:48:17 +0000

    [learn-squeryl] Update libs and workaround 'lookup' issue.

--- learn_scala/squeryl/src/test/scala/pkg/db/model/DirectedEntityTest.scala
+++ learn_scala/squeryl/src/test/scala/pkg/db/model/DirectedEntityTest.scala
@@ -11,6 +11,7 @@ import pkg.db.model.Direction.North
 
 @RunWith(classOf[JUnitRunner])
 class DirectedEntityTest extends FunSuite {
+
  test("save & restore") {
     val id = UUID.randomUUID()
     val de = new DirectedEntity(id, North)
@@ -21,7 +22,8 @@ class DirectedEntityTest extends FunSuite {
     }
 
     inTransaction {
-      AppSchema.directedEntity.lookup(id) match {
+      // Not sure how to fix this.  Broken when 0.9.6-RC2 => RC4.  AppSchema.directedEntity.lookup(id) match {
+      AppSchema.directedEntity.where(_.id === id).headOption match {
         case None => fail("Unable to locate entity with id " + id)
         case Some(found) => assertResult(de)(found)
       }
