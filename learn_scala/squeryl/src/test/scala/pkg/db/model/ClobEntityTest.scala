2c2a6b8264474519a69abf6f3d801c70cf290e0c
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2016-02-10 05:27:34 +0000

    [learn-squeryl] EG: Reading and writing CLOB elements.

--- learn_scala/squeryl/src/test/scala/pkg/db/model/ClobEntityTest.scala
+++ learn_scala/squeryl/src/test/scala/pkg/db/model/ClobEntityTest.scala
@@ -1,9 +1,9 @@
 package pkg.db.model
 
 import java.io.StringWriter
-import java.sql.Types
 import java.util.UUID
 
+import org.apache.commons.io.IOUtils
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -24,34 +24,50 @@ class ClobEntityTest extends FunSuite {
 
       // write the contents
       val conn = Session.currentSession.connection
+      val stmt = conn.prepareStatement("update clobentity set data = ? where id = ?")
       try {
-        val stmt = conn.prepareStatement("update clobentity set data = ? where id = ?")
         val data = conn.createClob()
-        val stream = data.setCharacterStream(1)
-        stream.write(contents)
-        stream.close()
-        stmt.setObject(1, data)
-        stmt.setObject(2, id)
-        stmt.execute()
-        data.free()
-        assertResult(1)(stmt.getUpdateCount)
+        try {
+          val stream = data.setCharacterStream(1)
+          stream.write(contents)
+          stream.close()
+          stmt.setObject(1, data)
+          stmt.setObject(2, id)
+          stmt.execute()
+          assertResult(1)(stmt.getUpdateCount)
+        } finally {
+          data.free()
+        }
+      } finally {
+        stmt.close()
       }
     }
 
     val writer = new StringWriter()
     inTransaction {
       // Not sure how to fix this.  Broken when 0.9.6-RC2 => RC4.  AppSchema.directedEntity.lookup(id) match {
-      AppSchema.clobEntity.where(_.id === id).headOption match {
-        case None => fail("Unable to locate entity with id " + id)
-        case Some(ClobEntity(_, Some(data))) =>
-          writer.write(data)
-//          val reader = data.getCharacterStream
-//          var ch = reader.read()
-//          while (ch != -1) {
-//            writer.write(ch)
-//            ch = reader.read()
-//          }
-//          reader.close()
+      //      AppSchema.clobEntity.where(_.id === id).headOption match {
+      //        case None => fail("Unable to locate entity with id " + id)
+      //        case Some(ClobEntity(_, Some(data))) =>
+      //          writer.write(data)
+      //    }
+
+      val conn = Session.currentSession.connection
+      val stmt = conn.prepareStatement("select data from clobentity where id = ?")
+      try {
+        stmt.setObject(1, id)
+        assert(stmt.execute(), "Expected a ResultSet")
+        val rs = stmt.getResultSet
+        assert(rs.next(), "Expected a row")
+        val reader = rs.getCharacterStream(1)
+        try {
+          IOUtils.copy(reader, writer)
+        } finally {
+          reader.close()
+        }
+        assert(!rs.next(), "Expected no more rows")
+      } finally {
+        stmt.close()
       }
     }
     assertResult(contents)(writer.toString)
