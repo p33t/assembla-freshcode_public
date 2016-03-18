6d9d4e178282e37608108b0a922b0715c8df22c0
pwl <1179301+p33t@users.noreply.github.com>
2016-03-18 00:31:59 +0000

    [learn-scala] XML modifying children etc.

--- learn_scala/syntax/src/main/scala/pckg_2_10/XmlModify.scala
+++ learn_scala/syntax/src/main/scala/pckg_2_10/XmlModify.scala
@@ -1,16 +1,32 @@
 package pckg_2_10
 
+import scala.xml.Elem
+
 
 object XmlModify {
+  val before = <root attr1="before" attr2="keep">
+    <child>hello</child>
+  </root>
+  val expAfter = <root attr1="after" attr2="keep">
+    <child>hello</child>
+  </root>
+  val expWorld = <root attr1="before" attr2="keep">
+    <child whom="world">hello</child>
+  </root>
 
   def main(args: Array[String]) {
-    val before = <root attr1="before" attr2="keep">
-      <child>hello</child>
-    </root>
     val after = before.copy(attributes = before.attributes.append(<alt attr1="after"/>.attributes))
-    val exp = <root attr1="after" attr2="keep">
-      <child>hello</child>
-    </root>
-    assert(after == exp)
+    assert(after == expAfter)
+
+
+    val world = before.copy(
+      child = before.child.map {
+        case target: Elem if target.label == "child" && target.attributes.get("whom").isEmpty =>
+          val alt = <x whom="world"/>
+          target.copy(attributes = target.attributes.append(alt.attributes))
+        case other => other
+      }
+    )
+    assert(world == expWorld)
   }
 }
