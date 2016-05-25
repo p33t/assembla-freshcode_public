2f2d5407ac389e04f51ed03285ef00cc3329ba77
pwl <1179301+p33t@users.noreply.github.com>
2016-05-25 04:28:41 +0000

    [learn-scala] Initialisation control with 'lazy'.

--- learn_scala/syntax/src/main/scala/pckg_2_10/EarlyInit.scala
+++ learn_scala/syntax/src/main/scala/pckg_2_10/EarlyInit.scala
@@ -4,6 +4,7 @@ package pckg_2_10
 object EarlyInit {
 
   trait A {
+    // NOTE: This could be a lazy val but if it is used during initialization then 'laziness' is negated.
     def foo: String
 
     val bar = foo + "bar"
@@ -29,10 +30,16 @@ object EarlyInit {
   // does not work; Core initialised last
   class E extends A with Core
 
+  // somehow, 'lazy' changes the initialisation order (?!)
+  class F extends A {
+    lazy val foo = "museli"
+  }
+
   def main(args: Array[String]) {
     println(new B().bar)
     println(new C().bar)
     println(new D().bar)
     println(new E().bar)
+    println(new F().bar)
   }
 }
