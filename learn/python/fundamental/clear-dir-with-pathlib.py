20ce2524776b0144a8264fae17670a7c282998d6
pwl <1179301+p33t@users.noreply.github.com>
2016-06-13 11:24:34 +0000

    [learn-python] pathlib.

---
+++ learn/python/fundamental/clear-dir-with-pathlib.py
@@ -0,0 +1,19 @@
+# pathlib seems to be a later generation, stricter method of working with paths.
+# Unfortunately I can't work out how to convert a 'Path' to a string, which is needed of other libraries like os,zipfile,shutil etc.
+import pathlib
+
+temp_dir = pathlib.Path.cwd().joinpath("temp")
+
+def clear_dir(pth):
+    for child in pth.iterdir():
+        if child.is_file():
+            child.unlink()
+        elif child.is_dir():
+            clear_dir(child)
+            child.rmdir()
+
+# Clean out temp dir
+if temp_dir.exists():
+    clear_dir(temp_dir)
+else:
+    temp_dir.mkdir()
