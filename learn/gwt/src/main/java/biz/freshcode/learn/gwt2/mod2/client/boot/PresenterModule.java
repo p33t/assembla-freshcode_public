b1dbecdbb63f5d2962782f20e0bb414932bbbc01
pwl <1179301+p33t@users.noreply.github.com>
2016-06-15 03:25:17 +0000

    [gwt] Text button set icon stops select event bug.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt2/mod2/client/boot/PresenterModule.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt2/mod2/client/boot/PresenterModule.java
@@ -1,5 +1,6 @@
 package biz.freshcode.learn.gwt2.mod2.client.boot;
 
+import biz.freshcode.learn.gwt2.mod2.client.bug.TextButtonSetIconStopsSelectEvent;
 import biz.freshcode.learn.gwt2.mod2.client.home.Home;
 import biz.freshcode.learn.gwt2.mod2.client.spike.adapterfieldgrid.AdapterFieldGridSpike;
 import biz.freshcode.learn.gwt2.mod2.client.spike.checkboxcellicon.CheckBoxCellIconSpike;
@@ -20,6 +21,9 @@ public class PresenterModule extends AbstractPresenterModule {
     protected void configure() {
         bindPresenter(Root.class, Root.Proxy.class);
         bindPresenter(Home.class, Home.Proxy.class);
+
+        bindPresenter(TextButtonSetIconStopsSelectEvent.class, TextButtonSetIconStopsSelectEvent.Proxy.class);
+
         bindPresenter(ResizeSpike.class, ResizeSpike.Proxy.class);
         bindPresenter(AdapterFieldGridSpike.class, AdapterFieldGridSpike.Proxy.class);
         bindPresenter(DragOrderSpike.class, DragOrderSpike.Proxy.class);
