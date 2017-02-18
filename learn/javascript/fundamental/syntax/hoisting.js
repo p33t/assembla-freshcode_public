b4ee5e4b513222ef7afb4084912b980db53b9be6
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2017-02-18 12:55:02 +0000

    [learn-js] Strings, Functions (WIP).

---
+++ learn/javascript/fundamental/syntax/hoisting.js
@@ -0,0 +1,15 @@
+// hoisting.js
+"use strict";
+
+v1 = "first value"
+
+function f1() {
+    console.log(`v1: ${v1}`)
+    var v1 = "inner value"
+}
+
+f1() // The function declaration is hoisted over the assignment so this prints 'undefined'
+var v1 // hoisted to start of function
+var v2 = v1 // just the declaration is hoisted, not the assignment
+
+console.log(`v2: ${v2}`)
