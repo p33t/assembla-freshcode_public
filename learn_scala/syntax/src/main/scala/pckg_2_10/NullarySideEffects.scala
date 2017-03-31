d7cfa904a20573ac1d2b38c98febc85fce6759f6
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2017-03-31 04:04:39 +0000

    [learn-scala] Exploring 'nullary' warning.

---
+++ learn_scala/syntax/src/main/scala/pckg_2_10/NullarySideEffects.scala
@@ -0,0 +1,26 @@
+package pckg_2_10
+
+/**
+  * Trying to reproduce a "side-effecting nullary methods are discouraged" warning that prevents compilation when -Xfatal-warnings is used.
+  * NOTE: Can use -Xlint:-nullary-unit to suppress the warning.  See https://groups.google.com/d/msg/squeryl/XKm9e1_B-QM/aYGH4E2KFwAJ
+  */
+object NullarySideEffects {
+
+  abstract class AbstractDb {
+    //noinspection UnitMethodIsParameterless
+    def create: Unit = {
+      println("AbstractDb.create")
+    }
+  }
+
+  class AppDb extends AbstractDb {
+    override def create: Unit = {
+      super.create
+      println("AppDb.create")
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    new AppDb().create
+  }
+}
