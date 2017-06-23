9dd3d443e1f4ab5e92c3c8b418665c9e977fb1bd
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2017-06-23 04:08:22 +0000

    [gwt] Tooltip obscures error message fix.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt2/mod2/client/bug/ToolTipVsErrorMessageBug.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt2/mod2/client/bug/ToolTipVsErrorMessageBug.java
@@ -11,6 +11,7 @@ import com.gwtplatform.mvp.client.annotations.ProxyStandard;
 import com.sencha.gxt.widget.core.client.container.FlowLayoutContainer;
 import com.sencha.gxt.widget.core.client.form.TextField;
 import com.sencha.gxt.widget.core.client.form.error.DefaultEditorError;
+import com.sencha.gxt.widget.core.client.tips.ToolTipConfig;
 
 import java.util.Collections;
 
@@ -32,7 +33,10 @@ public class ToolTipVsErrorMessageBug extends Presenter<ToolTipVsErrorMessageBug
         @Inject
         public View() {
             TextField txt = new TextField();
-            txt.setToolTip("A tool tip that gets in the way");
+//            txt.setToolTip("A tool tip that gets in the way");
+            ToolTipConfig config = new ToolTipConfig("A tool tip that gets in the way");
+            config.setShowDelay(100); // want this to appear sooner than validation error message
+            txt.setToolTipConfig(config);
             txt.setAutoValidate(true);
             txt.addValidator((editor, value) -> {
                 if (value != null && value.length() > 0) {
@@ -42,7 +46,8 @@ public class ToolTipVsErrorMessageBug extends Presenter<ToolTipVsErrorMessageBug
             });
 
             FlowLayoutContainer flc = new FlowLayoutContainer();
-            flc.add(new HTML("<p>Any text will trigger an invalid message however the tooltip interferes</p>"));
+            flc.add(new HTML("<p>Any text will trigger an invalid message however the tooltip interferes<br/>" +
+                    "NOTE: Fixed by setting tooltip config 'showDelay' to something small.  The error message shows last and on top.</p>"));
             flc.add(txt);
             initWidget(flc);
         }
