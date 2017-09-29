49adb59c09c9fe780d87349e259263fa72351ef3
pwl <1179301+p33t@users.noreply.github.com>
2017-09-29 06:31:22 +0000

    [gwt] Failed attempt at Rest Command pattern.  GWTP Rest specifically typed methods at this point.

--- learn/gwt/src/main/java/biz/freshcode/learn/gwt2/mod2/client/spike/restcommand/RestCommandSpike.java
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt2/mod2/client/spike/restcommand/RestCommandSpike.java
@@ -4,8 +4,13 @@ import biz.freshcode.learn.gwt2.common.client.builder.gxt.container.BorderLayout
 import biz.freshcode.learn.gwt2.common.client.builder.gxt.container.FlowLayoutContainerBuilder;
 import biz.freshcode.learn.gwt2.common.client.builder.gxt.form.TextAreaBuilder;
 import biz.freshcode.learn.gwt2.mod2.client.boot.Root;
+import biz.freshcode.learn.gwt2.mod2.shared.pkg.rest.ActionDispatch;
+import biz.freshcode.learn.gwt2.mod2.shared.pkg.rest.StringResult;
+import biz.freshcode.learn.gwt2.mod2.shared.pkg.rest.TestAction;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
+import com.gwtplatform.dispatch.rest.delegates.client.ResourceDelegate;
 import com.gwtplatform.mvp.client.Presenter;
 import com.gwtplatform.mvp.client.ViewImpl;
 import com.gwtplatform.mvp.client.annotations.NameToken;
@@ -22,10 +27,30 @@ public class RestCommandSpike extends Presenter<RestCommandSpike.View, RestComma
     public static final String TOKEN = "restCommand";
 
     @Inject
+    private ResourceDelegate<ActionDispatch> actionDisp;
+
+    @Inject
     public RestCommandSpike(EventBus eventBus, View view, Proxy proxy) {
         super(eventBus, view, proxy, Root.SLOT);
 
-        view.getBtnGo().addSelectHandler(evt -> getView().message("Go clicked"));
+        view.getBtnGo().addSelectHandler(evt -> go());
+    }
+
+    private void go() {
+        getView().message("Invoking action...");
+        actionDisp
+                .withCallback(new AsyncCallback<StringResult>() {
+                    @Override
+                    public void onFailure(Throwable caught) {
+                        getView().message("Failure: " + caught.getMessage());
+                    }
+
+                    @Override
+                    public void onSuccess(StringResult result) {
+                        getView().message("Success: " + result.getResult());
+                    }
+                })
+                .post(new TestAction());
     }
 
     @ProxyStandard
---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt2/mod2/shared/pkg/rest/StringResult.java
@@ -0,0 +1,18 @@
+package biz.freshcode.learn.gwt2.mod2.shared.pkg.rest;
+
+import com.gwtplatform.dispatch.rpc.shared.Result;
+
+public class StringResult implements Result {
+    private String result;
+
+    public StringResult(String result) {
+        this.result = result;
+    }
+
+    protected StringResult() {
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
