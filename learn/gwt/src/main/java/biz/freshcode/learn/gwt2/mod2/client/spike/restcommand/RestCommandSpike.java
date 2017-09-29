f095c108ae41367e3b3a77482e35e8b6183662d8
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2017-09-29 06:05:04 +0000

    [gwt] Ready to spike Rest Command.

---
+++ learn/gwt/src/main/java/biz/freshcode/learn/gwt2/mod2/client/spike/restcommand/RestCommandSpike.java
@@ -0,0 +1,61 @@
+package biz.freshcode.learn.gwt2.mod2.client.spike.restcommand;
+
+import biz.freshcode.learn.gwt2.common.client.builder.gxt.container.BorderLayoutContainerBuilder;
+import biz.freshcode.learn.gwt2.common.client.builder.gxt.container.FlowLayoutContainerBuilder;
+import biz.freshcode.learn.gwt2.common.client.builder.gxt.form.TextAreaBuilder;
+import biz.freshcode.learn.gwt2.mod2.client.boot.Root;
+import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
+import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.ViewImpl;
+import com.gwtplatform.mvp.client.annotations.NameToken;
+import com.gwtplatform.mvp.client.annotations.ProxyStandard;
+import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.form.TextArea;
+
+import java.util.Date;
+
+/**
+ * Trying to do command pattern over REST.
+ */
+public class RestCommandSpike extends Presenter<RestCommandSpike.View, RestCommandSpike.Proxy> {
+    public static final String TOKEN = "restCommand";
+
+    @Inject
+    public RestCommandSpike(EventBus eventBus, View view, Proxy proxy) {
+        super(eventBus, view, proxy, Root.SLOT);
+
+        view.getBtnGo().addSelectHandler(evt -> getView().message("Go clicked"));
+    }
+
+    @ProxyStandard
+    @NameToken(TOKEN)
+    public interface Proxy extends com.gwtplatform.mvp.client.proxy.ProxyPlace<RestCommandSpike> {
+    }
+
+    public static class View extends ViewImpl {
+        private TextArea feedback;
+
+        public TextButton getBtnGo() {
+            return btnGo;
+        }
+
+        private TextButton btnGo;
+
+        @Inject
+        public View() {
+            initWidget(new BorderLayoutContainerBuilder()
+                    .northWidget(new FlowLayoutContainerBuilder()
+                            .add(btnGo = new TextButton("Go"))
+                            .flowLayoutContainer)
+                    .centerWidget(feedback = new TextAreaBuilder()
+                            .readOnly(true)
+                            .textArea)
+                    .borderLayoutContainer);
+        }
+
+        public void message(String msg) {
+            feedback.setText(feedback.getText() + "\n " + new Date() + ":" + msg);
+        }
+    }
+}
