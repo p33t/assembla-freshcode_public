68da5a44c24a2a0e357865bad6c28a99358b778e
pwl <1179301+p33t@users.noreply.github.com>
2018-01-08 03:19:48 +0000

    [learn-js] Background on JS language / functions.

--- learn/javascript/fundamental/syntax/hoisting.js
+++ learn/javascript/fundamental/syntax/hoisting.js
@@ -9,7 +9,7 @@ function f1() {
 }
 
 f1() // The function declaration is hoisted over the assignment so this prints 'undefined'
-var v1 // hoisted to start of function
+var v1 // hoisted to start of scope
 var v2 = v1 // just the declaration is hoisted, not the assignment
 
 console.log(`v2: ${v2}`)
