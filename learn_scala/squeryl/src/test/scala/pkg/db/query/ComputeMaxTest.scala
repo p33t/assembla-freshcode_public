15298b89ff35dbc7ab9203d6d2119221cbaef3c7
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2018-03-16 02:02:58 +0000

    [learn-squeryl] Explore use of compute(max(...)).

---
+++ learn_scala/squeryl/src/test/scala/pkg/db/query/ComputeMaxTest.scala
@@ -0,0 +1,39 @@
+package pkg.db.query
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import org.squeryl.dsl.Measures
+import org.squeryl.dsl.ast.TrueLogicalBoolean
+import pkg.db.DbTypes.inTransaction
+import pkg.db.{AppSchema, T1}
+
+@RunWith(classOf[JUnitRunner])
+class ComputeMaxTest extends FunSuite {
+  test("calc max") {
+    AppSchema.init()
+    inTransaction {
+      AppSchema.reset()
+      //      AppSchema.directedEntity.insert(de)
+    }
+
+    def readMax = inTransaction {
+      import pkg.db.DbTypes._
+      val qry: Measures[Option[Int]] = from(AppSchema.t1)(t => where(TrueLogicalBoolean) compute max(t.id)).single
+      qry.measures
+    }
+
+    assert(readMax.isEmpty)
+
+    inTransaction{
+      AppSchema.t1.insert(T1(0, 0, "Bruce Banner", Some("spiderman")))
+    }
+    // not sure why but it appears id is automatically assigned
+    assert(readMax === Some(1))
+
+    inTransaction{
+      AppSchema.t1.insert(T1(0, 0, "Bruce Lee", Some("bruise")))
+    }
+    assert(readMax === Some(2))
+  }
+}
