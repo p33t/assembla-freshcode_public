f8fdf5d8720da5ac9e6e7d4c966ae1d4c7279843
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2018-06-18 05:52:36 +0000

    [kotlin] Reactive first example.

---
+++ learn/kotlin/reactive/src/main/kotlin/ch01/PullVsPush.kt
@@ -0,0 +1,18 @@
+package ch01
+
+object PullVsPush {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        pull()
+    }
+
+    fun pull() {
+        val list:List<Any> = listOf("One", 2, "Three", "Four", 4.5,
+                "Five", 6.0f) // 1
+        val iterator = list.iterator() // 2
+        while (iterator.hasNext()) { // 3
+            println(iterator.next()) // Prints each element 4
+        }
+    }
+}
