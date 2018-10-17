4be984f8ce3476321aa6db189de1efa6880c784c
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2018-10-17 20:51:53 +0000

    [learn-react] Tutorial

--- learn/react/fundamental-ts/src/App.tsx
+++ learn/react/fundamental-ts/src/App.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import './App.css';
 
 import logo from './logo.svg';
+import Description from './tutorial/Description';
+import Header from './tutorial/Header';
 
 class App extends React.Component {
   public render() {
@@ -10,10 +12,14 @@ class App extends React.Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <Header name="REACT" />
+          <Header />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+
+        <Description countBy={3} />
       </div>
     );
   }
