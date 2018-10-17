b6b8f636d3fbe57116848545353baf0752b22d3a
pwl <pwl@e6e56447-3450-4a0b-9e57-bd02873903ef>
2018-10-17 21:09:03 +0000

    [learn-react] Refactor for async safety
    Avoids using this.state (outside of constructor)

--- learn/react/fundamental-ts/src/tutorial/Description.tsx
+++ learn/react/fundamental-ts/src/tutorial/Description.tsx
@@ -1,37 +1,35 @@
 import * as React from 'react';
 
 interface IProps {
-  countBy?: number;
+    countBy?: number;
 }
 
 interface IState {
-  count: number;
+    count: number;
 }
 
 class Description extends React.Component<IProps, IState> {
-  public static defaultProps: Partial<IProps> = {
-    countBy: 1,
-  };
+    public static defaultProps: Partial<IProps> = {
+        countBy: 1,
+    };
 
-  public state: IState = {
-    count: 0,
-  };
+    public state: IState = {
+        count: 0,
+    };
 
-  public increase = () => {
-    const countBy: number = this.props.countBy!;
-    // TODO: Don't use 'this.state'... its not async safe.  Use setState function arg
-    const count = this.state.count + countBy;
-    this.setState({ count });
-  };
+    public increase = () => {
+        // Would be nice to break into a const but not good enough to handle type decl.
+        this.setState((state) => ({ count: state.count + this.props.countBy! }));
+    };
 
-  public render() {
-    return (
-      <div>
-        <p>My favorite number is {this.state.count}</p>
-        <button onClick={this.increase}>Increase</button>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div>
+                <p>My favorite number is {this.state.count}</p>
+                <button onClick={this.increase}>Increase</button>
+            </div>
+        );
+    }
 }
 
 export default Description;
