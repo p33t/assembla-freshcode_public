743ba6961f5ef683677d51eee7fa2f48aafc82d6
pwl <1179301+p33t@users.noreply.github.com>
2018-11-07 19:48:23 +0000

    [bf.l] Improve wording.

--- beatfix.live/www.beatfix.live/src/containers/About.js
+++ beatfix.live/www.beatfix.live/src/containers/About.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import Product from './Product'
+import Provider from "./Provider";
 //
 
 export default () => (
     <div>
         <h1>About</h1>
         <p>
-            I've been an amateur drummer for over 10 years and needed a metronome to help me monitor and fix my tempo
-            during a performance. Very few metronomes can do this and I've never found one I was entirely happy with.
-            (The closest was the Matrix MR600 DLX)
+            Hi, my name is Peter, an amateur drummer for over 10 years. I've never used a 'click track' and instead
+            rely on a visual metronome when onstage to ensure correct tempo, especially to start a song. A performance
+            metronome has a number of different requirements to a conventional metronome. In general, it needs to be
+            passive, flexible, quickly adjustable and work in the dark.
         </p>
         <p>
-            Being <a href="http://www.freshcode.biz">a software developer</a> by trade, I thought I'd come up with my
-            own metronome. <Product/> is the second generation solution.
-            The first was <a href="https://sourceforge.net/projects/multi-phasemetr/">Multi-phase Metronome</a> and it
-            worked reasonably well.
+            In my experience, the <i>Matrix MR600 DLX</i> metronome is very good for performance usage although not
+            perfect. Also, using the MR600, I found I could monitor and fix my tempo during a performance if necessary.
         </p>
+        <p>
+            Being a software developer by trade, I thought I'd come up with my own metronome. <Product/> is the second
+            generation solution. The first was <a href="https://sourceforge.net/projects/multi-phasemetr/">Multi-phase
+            Metronome</a> and it worked reasonably well.
+        </p>
+
+        <h2><Product/> is a <Provider/> innovation</h2>
     </div>
 )
--- beatfix.live/www.beatfix.live/src/containers/Usage.js
+++ beatfix.live/www.beatfix.live/src/containers/Usage.js
@@ -7,20 +7,37 @@ export default withRouteData(() => (
     <div>
         <h1>Usage</h1>
         <p>
-            To use <Product/> you need to maintain the ambient tempo (being played) whilst comparing it with the
-            desired tempo being shown. In doing so, one can keep a performance going even if the tempo is incorrect.
+            First and foremost, avoid the tendancy to focus on the spinning 'rotor' (it is rather hypnotizing).
+            Instead, choose and <u>focus on a single 'indicator'</u> on the circle. This will facilitate the use
+            of <Product/> as a simple metronome.
         </p>
         <p>
-            When comparing the ambient tempo with the display, you'll find the 'one' beat lands at a particular point on
-            the circle. If this point remains in the same place then you're in time. If this point 'slips' then you're
-            out of time. If this point slips clockwise (away from the rotor) then you're slow. If this point slips
-            counter-clockwise (toward the rotor) then you're fast.
+            To use <Product/> for tempo correction you need to maintain the ambient tempo (being played) whilst
+            comparing it with the desired tempo being shown. In doing so, one can keep a performance going even if the
+            tempo is incorrect.
         </p>
         <p>
-            To fix tempo, simply ride the start or end of the beat to gradually fix the 'slipage'.
+            When comparing the ambient tempo with the display, you'll find the 'one' beat lands on a particular
+            indicator on the circle. If this point remains in the same place then you're in time. If this point 'slips'
+            then you're out of time. If this point slips clockwise (away from the rotor) then you're slow. If this point
+            slips counter-clockwise (toward the rotor) then you're fast.
+        </p>
+        <p>
+            To fix tempo, simply ride the start or end of the beat to gradually reduce and eliminate the 'slipage'.
         </p>
         <p>
             Naturally, this should all be practiced before using <Product/> during a performance.
+        </p>
+
+        <h1>Tips</h1>
+        <p>
+            There are a number of + and - buttons that can be used to adjust the tempo. They are designed
+            to minimize the number of clicks.
+        </p>
+        <p>
+            The tempo shown in the center of the circle provides a way to 'train' the metronome to the desired
+            beats-per-minute. Just <u>repeatedly tap on the number at the desired rate</u>. This is handy to gauge the tempo of
+            a song one is listening to.
         </p>
     </div>
 ))
