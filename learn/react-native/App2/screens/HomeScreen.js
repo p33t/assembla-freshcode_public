045ad3f1659672c187546f5803999829d9566af2
pwl <1179301+p33t@users.noreply.github.com>
2019-02-04 20:05:18 +0000

    [learn-react-native] Fix home screen hyperlinks

--- learn/react-native/App2/screens/HomeScreen.js
+++ learn/react-native/App2/screens/HomeScreen.js
@@ -88,12 +88,12 @@ export default class HomeScreen extends React.Component {
   }
 
   _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
   };
 
   _handleHelpPress = () => {
     WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+      'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
     );
   };
 }
