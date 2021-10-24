08b9ae8eb062c156878541e2e564574e41df52e1
pwl <1179301+p33t@users.noreply.github.com>
2021-10-24 19:57:40 +0000

    Make compatible with smaller screens

--- beatfix.live/www/src/index.js
+++ beatfix.live/www/src/index.js
@@ -11,7 +11,7 @@ export default App
 if (typeof document !== 'undefined') {
     const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate || ReactDOM.render;
     const render = Comp => {
-        renderMethod(<Comp/>, document.getElementById('root'))
+        renderMethod(<Comp innerWidth={document.innerWidth}/>, document.getElementById('root'))
     };
 
     // Render!
